f19246a2c5a02a1784cf56458eff2d778016666b
atanych <atanych@gmail.com>
2016-07-14 20:09:21 +0300

    fight with bot

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :administrators, path: 'administration/administrators', as: :devise,
-             name:                  :administrator, singular: :administrator, to: 'User',
-             class_name:            'User'
+             name: :administrator, singular: :administrator, to: 'User',
+             class_name: 'User'
   devise_for :users, path: 'users', as: :devise,
-             name:         :user, singular: :user, to: 'User',
-             class_name:   'User'
+             name: :user, singular: :user, to: 'User',
+             class_name: 'User'
 
   namespace :administration do
     root to: 'home#index'
