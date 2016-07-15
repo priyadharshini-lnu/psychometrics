38fb669f0d70232d3e86692917ad97e2fed11c3a
atanych <atanych@gmail.com>
2016-07-15 15:02:03 +0300

    fight with bot

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -9,9 +9,7 @@ class Dimension < ApplicationRecord
     },
     available_filters: [
        :sorted_by,
-       :search_query,
-       :with_client,
-       :with_role
+       :search_query
      ]
   )
 
