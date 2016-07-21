4ee068c6cb742ca2e88719d017a92a9c6e81b7a3
atanych <atanych@gmail.com>
2016-07-21 19:02:17 +0300

    refactoring

--- lib/imports/xls/norm_import.rb
+++ lib/imports/xls/norm_import.rb
@@ -15,11 +15,12 @@ module Imports
       end
 
       def process
-        @file  = 'ETI_Oman Norms_Mar 2015.xlsx'
-        sheets = RubyXL::Parser.parse(@file)
-        sheets.each do |sheet|
-          @current_sheet = sheet
-          import_data
+        ActiveRecord::Base.transaction do
+          @file  = 'ETI_Oman Norms_Mar 2015.xlsx'
+          sheets = RubyXL::Parser.parse(@file)
+          sheets.each do |sheet|
+            import_by_sheet(sheet)
+          end
         end
         true
       rescue ActiveRecord::RecordInvalid => e
@@ -29,32 +30,37 @@ module Imports
 
       private
 
-      def import_data
-        sheet_name_arr     = @current_sheet.sheet_name.split
-        @current_norm_type = sheet_name_arr.pop.downcase
-        @norm              = Norm.create!(name: sheet_name_arr.join('')) unless @norm
-        @dimension         = Dimension.create!(name: sheet_name_arr.join('')) unless @dimension
+      def import_by_sheet(sheet)
+        @current_sheet = sheet
+        import_by_sheet_name(@current_sheet.sheet_name)
         import_factors
         import_sub_factors
       end
 
+      def import_by_sheet_name(sheet_name)
+        sheet_name_arr     = sheet_name.split
+        @current_norm_type = sheet_name_arr.pop.downcase
+        @norm              = Norm.create!(name: sheet_name_arr.join('')) unless @norm
+        @dimension         = Dimension.create!(name: sheet_name_arr.join('')) unless @dimension
+      end
+
       def import_factors
-        factor_start_ceil = XLS_CONFIG[:factor_start_ceil]-1
-        factor_start_row  = XLS_CONFIG[:factor_start_row]-1
+        factor_start_ceil = XLS_CONFIG[:factor_start_ceil] - 1
+        factor_start_row  = XLS_CONFIG[:factor_start_row] - 1
         (factor_start_row...@current_sheet.count).each do |i|
           factor_name = @current_sheet[i][factor_start_ceil].value
           break unless factor_name
           @cursor_x = factor_start_ceil
           @cursor_y = i
-          @factor   = Factor.where(dimension_id: @dimension.id, name: factor_name).first
-          @factor   = @dimension.factors.create!(name: factor_name) unless @factor
-          import_factor_norms(@factor, i, factor_start_ceil + 1)
+          factor    = Factor.where(dimension_id: @dimension.id, name: factor_name).first
+          factor    = @dimension.factors.create!(name: factor_name) unless factor
+          import_factor_norms(factor, i, factor_start_ceil + 1)
         end
       end
 
       def import_sub_factors
-        factor_start_ceil = XLS_CONFIG[:factor_start_ceil]-1
-        factor_start_row = nil
+        factor_start_ceil = XLS_CONFIG[:factor_start_ceil] - 1
+        factor_start_row  = nil
         (@cursor_y...@current_sheet.count).each do |i|
           if @current_sheet[i][0].value
             factor_start_row = i
@@ -62,25 +68,25 @@ module Imports
           end
         end
         (factor_start_row+1...@current_sheet.count).each do |i|
-          @cursor_x   = 0
-          @cursor_y   = i
-          factor_name = @current_sheet[i][0].value
+          @cursor_x       = 0
+          @cursor_y       = i
+          factor_name     = @current_sheet[i][0].value
           sub_factor_name = @current_sheet[i][1].value
           break unless factor_name
-          @factor = Factor.where(dimension_id: @dimension.id, name: factor_name).first
-          unless @factor
+          factor = Factor.where(dimension_id: @dimension.id, name: factor_name).first
+          unless factor
             raise Errors::ImportError.new(I18n.t('administration.imports.errors.norm.factor_is_not_described',
                 coords: human_coordinates,
                 factor: factor_name))
           end
-          @sub_factor   = Factor.where(dimension_id: @dimension.id, name: sub_factor_name, parent_id: @factor.id).first
-          @sub_factor   = @dimension.factors.create!(name: sub_factor_name, parent_id: @factor.id) unless @sub_factor
-          import_factor_norms(@sub_factor, i, factor_start_ceil + 1)
+          sub_factor = Factor.where(dimension_id: @dimension.id, name: sub_factor_name, parent_id: factor.id).first
+          sub_factor = @dimension.factors.create!(name: sub_factor_name, parent_id: factor.id) unless sub_factor
+          import_factor_norms(sub_factor, i, factor_start_ceil + 1)
         end
       end
 
       def import_factor_norms(factor, row, ceil)
-        (ceil...ceil+10).each_slice(2) do |score_from, score_to|
+        (ceil...ceil+FactorsNorm::LEVELS.size*2).each_slice(2) do |score_from, score_to|
           @cursor_x = score_from
           factor.factors_norms.create!(
               score_from: @current_sheet[row][score_from].value,
