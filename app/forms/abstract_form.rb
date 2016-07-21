c29c4dc8a60dba007cdcf26e8015520dc743de0d
atanych <atanych@gmail.com>
2016-07-21 11:52:53 +0300

    add forms, errors

--- app/forms/abstract_form.rb
+++ app/forms/abstract_form.rb
@@ -5,7 +5,7 @@
 # 1. Implement AbstractForm (for example: MyForm)
 # 2. Use in the controller:
 #
-# data = MyForm.new(params, { param_1: 'my_param' })
+# data = MyForm.new(file: 'my_file')
 # if data.valid?
 #   do success login
 # else
@@ -15,41 +15,7 @@ class AbstractForm
 
   include ActiveModel::Validations
   include ActiveModel::Conversion
+  include ActiveModel::Model
   extend ActiveModel::Naming
-
-  class << self
-    # TODO if it is possible, rewrite without class variables
-    def attr_accessor(*vars)
-      @@attributes ||= []
-      @@attributes.concat vars
-      super(*vars)
-    end
-  end
-
-  #
-  # @param [ActionController::Parameters] attributes
-  #
-  def initialize(attributes = {}, extra_data)
-    @extra_data = extra_data
-    attributes  = form_params(attributes)
-    attributes.each do |name, value|
-      send("#{name}=", value) if respond_to? "#{name}="
-    end
-  end
-
-  #
-  # Convert strong params to hash
-  #
-  # @param [ActionController::Parameters] params
-  #
-  # @return [Hash]
-  #
-  def form_params(params)
-    params.permit(@@attributes)
-  end
-
-  def attributes
-    @@attributes
-  end
 
 end
