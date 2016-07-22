88e2820108aa05c32be853d655840e9c08613ea4
atanych <atanych@gmail.com>
2016-07-22 16:49:29 +0300

    for_bot

--- app/forms/norm_import_form.rb
+++ app/forms/norm_import_form.rb
@@ -2,6 +2,9 @@ class NormImportForm < BaseForm
   attr_accessor :file
 
   validates :file, presence: true,
-      file_size:             { less_than_or_equal_to: 4.megabytes },
-      file_content_type:     { allow: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel'] }
+                   file_size: { less_than_or_equal_to: 4.megabytes },
+                   file_content_type: { allow: [
+                                                   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                                   'application/vnd.ms-excel'
+                                               ] }
 end
