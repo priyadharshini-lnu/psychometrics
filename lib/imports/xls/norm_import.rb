0065590e09874679547362fce270a1d2d32124c3
atanych <atanych@gmail.com>
2016-07-22 16:56:42 +0300

    for_bot

--- lib/imports/xls/norm_import.rb
+++ lib/imports/xls/norm_import.rb
@@ -74,8 +74,8 @@ module Imports
           factor = Factor.where(dimension_id: @dimension.id, name: factor_name).first
           unless factor
             raise Errors::ImportError, I18n.t('administration.imports.errors.norm.factor_is_not_described',
-                                          coords: human_coordinates,
-                                          factor: factor_name)
+                                              coords: human_coordinates,
+                                              factor: factor_name)
           end
           sub_factor = Factor.where(dimension_id: @dimension.id, name: sub_factor_name, parent_id: factor.id).first
           sub_factor = @dimension.factors.create!(name: sub_factor_name, parent_id: factor.id) unless sub_factor
