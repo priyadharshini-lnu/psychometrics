96598432909c19f758775c1f378e7178add5fdcd
atanych <atanych@gmail.com>
2016-07-25 16:05:59 +0300

    some fix

--- app/decorators/factor_decorator.rb
+++ app/decorators/factor_decorator.rb
@@ -1,16 +1,7 @@
 class FactorDecorator < BaseDecorator
-  def xls_factor_score_row(type)
-    result = [nil, object.name]
-    FactorsNorm::LEVELS.each do |level|
-      current_factor_norm = object.factors_norms.where(level: level, type: type).first
-      result << current_factor_norm.try(:score_from).try(:round, 5)
-      result << current_factor_norm.try(:score_to).try(:round, 5)
-    end
-    result
-  end
 
-  def xls_subfactor_score_row(type)
-    result = [object.root.name, object.name]
+  def xls_factor_score_row(type)
+    result = [object.root? ? nil : object.root.name, object.name]
     FactorsNorm::LEVELS.each do |level|
       current_factor_norm = object.factors_norms.where(level: level, type: type).first
       result << current_factor_norm.try(:score_from).try(:round, 5)
