132192984bbaf907ddb91e06ef08a1791e4f1475
atanych <atanych@gmail.com>
2016-07-26 19:17:56 +0300

    integrate inplace edit

--- app/assets/javascripts/administration/norms/editor.js
+++ app/assets/javascripts/administration/norms/editor.js
@@ -1,25 +1,20 @@
 $(function () {
     $.fn.editable.defaults.ajaxOptions = {type: "PUT"};
 
-    $('.editable').editable({
-        'type': 'text',
-        id: 111,
-        //'url': function (e) {
-        //    console.log(111);
-        //    return '/administration/factors_norms/3';
-        //},
-        //url: '/administration/factors_norms/2',
-        'title': I18n.t('administration.norms.editor.inplace_title'),
-        params: function (params) {
+    function init() {
 
-            console.log('params', params);
-            var data = {};
-            data.id = params.pk;
-            data.pk = params.pk;
-            data[params.name] = params.value;
-            return data;
-        }
-    }).on('shown', function (e, editable) {
-        editable.$element.attr('data-url', 'aaaa')
+        $('.editable').editable({
+            'type': 'text',
+            'title': I18n.t('administration.norms.editor.inplace_title'),
+            params: function (params) {
+                var data = {};
+                data[params.name] = params.value;
+                return data;
+            }
+        });
+    }
+    init();
+    $(document).on('initPlugins', function() {
+        init();
     });
 });
