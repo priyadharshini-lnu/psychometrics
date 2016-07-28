3135c57f8180fee5a1b962423e9a2d40764bb3ab
atanych <atanych@gmail.com>
2016-07-28 11:28:27 +0300

    for_bot

--- app/controllers/administration/norms_controller.rb
+++ app/controllers/administration/norms_controller.rb
@@ -89,12 +89,12 @@ class Administration::NormsController < Administration::BaseController
 
   def editor
     @filterrific = initialize_filterrific(
-    FactorsNorm,
-    params[:filterrific],
-    select_options: {
-      by_norm_type: FactorsNorm::NORM_TYPES,
-      by_factor_type: FactorsNorm::FACTOR_TYPES
-    }) || return
+      FactorsNorm,
+      params[:filterrific],
+      select_options: {
+        by_norm_type: FactorsNorm::NORM_TYPES,
+        by_factor_type: FactorsNorm::FACTOR_TYPES
+      }) || return
     @resources = FactorsNorm.structured_hash(
       @filterrific.find.where(norm_id: @resource.id),
       @filterrific.by_factor_type == 'sub_factors'
