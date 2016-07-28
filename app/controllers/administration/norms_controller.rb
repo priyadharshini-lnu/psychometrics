fab6a72d369083fc372d715e096eced88c6c0ed3
atanych <atanych@gmail.com>
2016-07-28 11:29:09 +0300

    for_bot

--- app/controllers/administration/norms_controller.rb
+++ app/controllers/administration/norms_controller.rb
@@ -88,6 +88,8 @@ class Administration::NormsController < Administration::BaseController
   end
 
   def editor
+    add_breadcrumb @resource.name
+    add_breadcrumb I18n.t('administration.breadcrumbs.norms_editor')
     @filterrific = initialize_filterrific(
       FactorsNorm,
       params[:filterrific],
@@ -99,8 +101,6 @@ class Administration::NormsController < Administration::BaseController
       @filterrific.find.where(norm_id: @resource.id),
       @filterrific.by_factor_type == 'sub_factors'
     )
-    add_breadcrumb @resource.name
-    add_breadcrumb I18n.t('administration.breadcrumbs.norms_editor')
     respond_to do |format|
       format.js
       format.html
