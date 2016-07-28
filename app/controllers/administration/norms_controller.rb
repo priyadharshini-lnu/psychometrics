ed138e037a8696708b57be4c3fe9a7f28ae2a62f
atanych <atanych@gmail.com>
2016-07-28 11:26:53 +0300

    for_bot

--- app/controllers/administration/norms_controller.rb
+++ app/controllers/administration/norms_controller.rb
@@ -95,7 +95,7 @@ class Administration::NormsController < Administration::BaseController
         by_norm_type: FactorsNorm::NORM_TYPES,
         by_factor_type: FactorsNorm::FACTOR_TYPES
       }) || return
-    @resources   = FactorsNorm.structured_hash(
+    @resources = FactorsNorm.structured_hash(
       @filterrific.find.where(norm_id: @resource.id),
       @filterrific.by_factor_type == 'sub_factors'
     )
