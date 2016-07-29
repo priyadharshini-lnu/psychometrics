e914d95e42fa15b88a452e3f7961851d7546a00d
atanych <atanych@gmail.com>
2016-07-29 17:52:14 +0300

    for_bot

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -4,7 +4,6 @@ Warden::Manager.after_set_user do |user,auth,opts|
   auth.cookies.signed["#{scope}.expires_at"] = 30.minutes.from_now
 end
 
-
 Warden::Manager.before_logout do |_user, auth, opts|
   scope = opts[:scope]
   auth.cookies.signed["#{scope}.id"] = nil
