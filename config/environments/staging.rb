2b17f37ca4d45fa2216f97f63df05564fe506c55
Vladimir Atanov <atanych@gmail.com>
2016-09-07 18:03:16 +0300

    change_log_level

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.log_level = :debug
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
