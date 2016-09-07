9ce8ae1e7b6c7d2dc8c203dc01d3ad9058788f05
Vladimir Atanov <atanych@gmail.com>
2016-09-07 16:42:36 +0300

    for_bot

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,8 +1,3 @@
-# ----------------------------------------------------------------------------
-# Frozen-string-literal: true
-# Copyright: 2015 - 2016 Jordon Bedwell - Apache v2.0 License
-# Encoding: utf-8
-# ----------------------------------------------------------------------------
 class String
   def valid_float?
     true if Float self
