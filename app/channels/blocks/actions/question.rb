107c354a1538b5abe535fdfffd58196a60ba1794
Vladimir Atanov <atanych@gmail.com>
2016-09-14 15:37:03 +0300

    create_question

--- app/channels/blocks/actions/question.rb
+++ app/channels/blocks/actions/question.rb
@@ -6,13 +6,17 @@ module Blocks
       action :update do |data|
         id = data.delete('id')
         ::Question.update(id, data)
-        FactorsScoring.where(question_id: id).update_all(props: [])
         nil
       end
 
       action :rename do |data|
         ::Question.find(data['id']).update(name: data['name'])
         nil
+      end
+
+      action :create do |data, _, block|
+        question = block.questions.create!(data)
+        QuestionSerializer.new(question).to_hash
       end
     end
   end
