e958c16fe0088e9e5bf736412344173f8acda613
Vladimir Atanov <atanych@gmail.com>
2016-09-22 13:20:38 +0300

    add_factors

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -4,7 +4,6 @@ class ReportSerializer < ActiveModel::Serializer
   has_many :pages, serializer: Reports::PageSerializer
   has_one :assessment, serializer: AssessmentSerializer
 
-
   has_many :factors, serializer: Factors::WithoutSubFactorsSerializer do
     Factor.where(dimension_id: object.assessment.dimension_id).order(name: :asc)
   end
