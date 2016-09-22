133e9f131439d8864c2072415a379fde2ef2a161
Fedor <tarassovf@gmail.com>
2016-09-22 13:03:42 +0300

    serialize assesment with report

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -2,4 +2,5 @@ class ReportSerializer < ActiveModel::Serializer
   attributes :id, :name, :disabled, :created_at
 
   has_many :pages, serializer: Reports::PageSerializer
+  has_one :assessment, serializer: AssessmentSerializer
 end
