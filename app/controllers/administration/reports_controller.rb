8a3f93dd40f3511280c322f8475a1a57507ea3b6
Vladimir Atanov <atanych@gmail.com>
2016-10-22 13:59:21 +0300

    weird_skip_logic

--- app/controllers/administration/reports_controller.rb
+++ app/controllers/administration/reports_controller.rb
@@ -9,7 +9,7 @@ module Administration
     # TODO: Implement token auth for preview
     # skip_before_action :authenticate, only: [:preview]
     # skip_before_action :authenticate_user!, only: [:preview]
-    # skip_after_action :verify_authorized, only: [:preview]
+    skip_after_action :verify_authorized, only: [:preview]
     # GET /administration/resources
     def index
       @filterrific = initialize_filterrific(
