4a3f33e5e83a45f930d4d52dafe0156efd85a241
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2016-10-27 16:05:35 +0300

    Fix I18n

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -22,7 +22,7 @@ class Assign < ApplicationRecord
   belongs_to :assessment
   belongs_to :client
 
-  validates_uniqueness_of :client_id, scope: [:assessment_id, :user_id], message: 'Assign has already been taken'
+  validates_uniqueness_of :client_id, scope: [:assessment_id, :user_id], message: :not_uniqueness
 
   enum status: [:not_started, :in_progress, :completed]
   enum role: [:member, :manager, :admin]
