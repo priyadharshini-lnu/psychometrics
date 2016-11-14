a4e7a3082bc676fec53f3691d66864b50ed34ba7
Vladimir Atanov <atanych@gmail.com>
2016-11-14 17:26:47 +0300

    fix assigns

--- app/policies/assessment_policy.rb
+++ app/policies/assessment_policy.rb
@@ -10,7 +10,7 @@ class AssessmentPolicy < BasePolicy
   class Scope < Scope
     def resolve
       current_membership_id = @user[:current_membership].id
-      scope.joins(:assigns).where.has { assigns.membership_id == current_membership_id }.references(:assigns)
+      scope.includes(:assigns).where.has { assigns.membership_id == current_membership_id }.references(:assigns)
     end
   end
 end
