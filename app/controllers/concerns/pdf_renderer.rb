3946e1fa8f328636be01e56e1d026555596138bb
Fedor <tarassovf@gmail.com>
2016-11-17 19:44:58 +0300

    add pdf rendering with unminificated files

--- app/controllers/concerns/pdf_renderer.rb
+++ app/controllers/concerns/pdf_renderer.rb
@@ -6,21 +6,21 @@ class PdfRenderer
   end
 
   def render(opts = {})
-    # tmp_folder = Rails.root.join('tmp/reports')
-    # output = "#{tmp_folder}#{@resource.id}_#{Time.now.to_f}.pdf"
-    #
-    # args = {
-    #   url: @context.url_for({ action: :preview, id: @resource, export: true, user_token: @user_token }),
-    #   output: output,
-    #   pageWidth: 850,
-    #   pageHeight: 1100
-    # }.merge(opts).to_a.map { |key, value| "#{key}='#{value}'" }.join(' ')
-    #
-    # Dir.mkdir(tmp_folder) unless Dir.exist?(tmp_folder)
+    tmp_folder = Rails.root.join('tmp/reports')
+    output = "#{tmp_folder}#{@resource.id}_#{Time.now.to_f}.pdf"
 
-    # system("phantomjs #{Rails.root.join('lib/raster.js')} #{args}")
+    args = {
+      url: @context.url_for({ action: :preview, id: @resource, export: true, user_token: @user_token }),
+      output: output,
+      pageWidth: 850,
+      pageHeight: 1100
+    }.merge(opts).to_a.map { |key, value| "#{key}='#{value}'" }.join(' ')
+
+    Dir.mkdir(tmp_folder) unless Dir.exist?(tmp_folder)
+
+    system("phantomjs #{Rails.root.join('lib/raster.js')} #{args}")
 
     # output
-    Rails.root.join('public', 'reports1_1477837028.264626.pdf')
+    Rails.root.join('public', output)
   end
 end
