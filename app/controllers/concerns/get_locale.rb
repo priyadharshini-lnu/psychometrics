c0fbc6352858d98229ebcf2830d620c68e50d1a7
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2016-11-23 17:35:00 +0300

    Fix translations. Set translation by lang parameter

--- app/controllers/concerns/get_locale.rb
+++ app/controllers/concerns/get_locale.rb
@@ -7,6 +7,7 @@ module GetLocale
 
   def user_locale
     probably_locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    probably_locale = params[:lang] if params[:lang] && Settings.languages.include?(params[:lang])
     Settings.languages.include?(probably_locale) ? probably_locale : 'en'
   end
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -15,7 +15,8 @@ class Translation < ApplicationRecord
   def self.to_hash_for_assessment(assessment_id, locale)
     results = {}
     for_assessment(assessment_id).where(locale: locale).find_each do |t|
-      results[t.translateable_type.underscore] = Hash[t.translateable_id, t.props]
+      results[t.translateable_type.underscore] ||= {}
+      results[t.translateable_type.underscore][t.translateable_id] ||= t.props
     end
     results
   end
@@ -23,7 +24,8 @@ class Translation < ApplicationRecord
   def self.to_hash_for_report(report_id, assessment_id, locale)
     results = {}
     for_report(report_id).or(for_assessment(assessment_id)).where(locale: locale).find_each do |t|
-      results[t.translateable_type.underscore] = Hash[t.translateable_id, t.props]
+      results[t.translateable_type.underscore] ||= {}
+      results[t.translateable_type.underscore][t.translateable_id] ||= t.props
     end
     results
   end
