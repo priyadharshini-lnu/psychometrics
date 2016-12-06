4f58bcfc0cd04dd05e993ad12e0a8fad29f9b488
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2016-12-06 12:55:19 +0300

    Fixes

--- app/serializers/reports/user_serializer.rb
+++ app/serializers/reports/user_serializer.rb
@@ -2,7 +2,7 @@ module Reports
   class UserSerializer < ActiveModel::Serializer
     attributes :id, :first_name, :last_name, :email, :status, :completed_at
 
-    def user_id
+    def id
       object.user_id
     end
 
