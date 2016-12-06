e7dfdee4a24b2257618995a3fa213d1b8685e1fa
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2016-12-06 11:13:12 +0300

    Fix locale import

--- lib/imports/translations/report_import.rb
+++ lib/imports/translations/report_import.rb
@@ -61,6 +61,7 @@ module Imports
           # Initialize collections of translation
           # Nested hash if not initilized return blank hash insted nil
           data.each do |locale, translation|
+            next if locale == 'en' # Default locale
             collect_translations[translateable_type][translateable_id][locale] ||= {}
             collect_translations[translateable_type][translateable_id][locale][key] = translation
           end
