5d6017fa0118be89cdd63b354416a97cfb724c57
Yuri Zubov <y.zubov@sumatosoft.com>
2016-12-30 22:47:34 +0300

    added production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,6 @@
+require 'syslog/logger'
+require 'uglifier'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -22,7 +25,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new output: { comments: :none }
+  config.logger = Syslog::Logger.new 'psychometrics'
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
