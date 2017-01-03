d607981c7cd20a83dd685646176085cc0156d662
Vladimir Atanov <atanych@gmail.com>
2017-01-03 19:26:27 +0300

    add auto collapse after reload page, if it is necessary

--- app/controllers/managers/tasks_controller.rb
+++ app/controllers/managers/tasks_controller.rb
@@ -48,6 +48,8 @@ module Managers
       @resource.owner = @current_membership
       respond_to do |format|
         if @resource.save
+          # TODO: This is huck, used to collapse tasks (I need to fast solution)
+          flash[:task_id_collapse] = @resource.parent_id if @resource.parent_id
           format.js
         else
           format.js { render :new }
