31e93edaa276ab722625c60cfc008391a1ebe158
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2017-01-08 12:38:29 +0300

    Fix

--- app/policies/administration/assessment_client_policy.rb
+++ app/policies/administration/assessment_client_policy.rb
@@ -3,5 +3,13 @@ module Administration
     def index?
       @user.is?(:superadmin, :admin)
     end
+
+    def export_results?
+      @user.is?(:superadmin)
+    end
+
+    def import_results?
+      @user.is?(:superadmin)
+    end
   end
 end
