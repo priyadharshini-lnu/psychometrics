ebe59195d66abfbe41ba39fb1e4d64c4aec76141
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2017-01-17 12:53:02 +0300

    Add to piped text norm used

--- app/serializers/reports/user_serializer.rb
+++ app/serializers/reports/user_serializer.rb
@@ -1,16 +1,25 @@
 module Reports
   class UserSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :email, :status, :completed_at
+    attributes :id, :first_name, :last_name, :email, :status, :completed_at, :norm_used
     def id
       object.user_id
     end
 
+    def status
+      @instance_options[:assign].status
+    end
+
+    # Using for Piped Text
     def completed_at
       @instance_options[:assign]&.completed_at&.strftime('%d %b %Y')
     end
 
-    def status
-      @instance_options[:assign].status
+    # Using for Piped Text
+    def norm_used
+      norm_data = @instance_options[:assign]&.norm_data
+      return nil if norm_data.blank?
+      norm = Norm.find_by(id: norm_data.try(:[], 'id'))
+      norm&.decorate&.display_name
     end
   end
 end
