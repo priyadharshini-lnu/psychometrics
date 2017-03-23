c74d5759ef90ffab27528c85736996dae7d35aa7
atanych <atanych@gmail.com>
2017-03-23 19:06:02 +0300

    Fix invitation

--- config/routes.rb
+++ config/routes.rb
@@ -286,16 +286,16 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users,
-             path: 'users',
-             as: :devise,
-             name: :user,
-             singular: :user,
-             to: 'User',
-             class_name: 'User',
-             controllers: { registrations: 'users/registrations' }
 
   constraints(subdomain: /^(?!(www|#{Settings.subdomain})$)(.+)$/i) do
+    devise_for :users,
+               path: 'users',
+               as: :devise,
+               name: :user,
+               singular: :user,
+               to: 'User',
+               class_name: 'User',
+               controllers: { registrations: 'users/registrations', invitations: 'users/invitations' }
     # Manager's panel
     #
     namespace :managers do
