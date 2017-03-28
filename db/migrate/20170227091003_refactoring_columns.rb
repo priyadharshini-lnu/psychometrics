dbcf4431c6f2a7ee1f6bd86e54651ad2bd7dcaa8
atanych <atanych@gmail.com>
2017-03-28 17:20:32 +0300

    Fix migrations

--- db/migrate/20170227091003_refactoring_columns.rb
+++ db/migrate/20170227091003_refactoring_columns.rb
@@ -2,6 +2,7 @@ class RefactoringColumns < ActiveRecord::Migration[5.0]
   def change
     rename_column :clients, :memberships_count, :users_count
     change_column_default(:clients, :children_count, from: nil, to: 0)
+    Client.find_each { |client| Client.reset_counters(client.id, :memberships) }
     Client.reset_column_information
     Client.find_each { |client| Client.reset_counters(client.id, :children) }
   end
