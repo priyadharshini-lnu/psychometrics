fff9f29252a5272a7c54deb5208719897c1eb087
atanych <atanych@gmail.com>
2017-03-30 14:10:15 +0300

    4 client level permissions for Client Admin

--- app/policies/administration/client_policy.rb
+++ app/policies/administration/client_policy.rb
@@ -4,6 +4,10 @@ module Administration
       super || @user.has_grant?(:clients, :view)
     end
 
+    def manage_first_level?
+      @user.is?(:superadmin)
+    end
+
     def create?
       super || @user.has_grant?(:clients, :manage)
     end
@@ -28,11 +32,24 @@ module Administration
       @user.is?(:superadmin)
     end
 
+    def view_additional_fields?
+      @user.is?(:superadmin)
+    end
+
+    def edit_additional_fields?
+      view_additional_fields?
+    end
+
     class Scope < Scope
       def resolve
         return scope if @user.is?(:superadmin)
-        parent_ids = @user.admin_clients.not_retails.enabled.pluck(:id)
-        scope.where.has { (id.in parent_ids) | (parent_id.in parent_ids) }
+        clients = @user.admin_clients.not_retails.enabled
+        parent_ids = clients.map { |c| c.client.id }
+        if scope.is_a? Client
+          return parent_ids.include?(scope.id) ? scope : nil
+        else
+          scope.where.has { (id.in parent_ids) | (parent_id.in parent_ids) }
+        end
       end
     end
   end
