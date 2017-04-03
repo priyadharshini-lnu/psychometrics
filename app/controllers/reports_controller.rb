1362fe93992e5cb9e08031cd614cdbb4ac0350f3
atanych <atanych@gmail.com>
2017-04-03 14:21:18 +0300

    Fix export PDF for user side

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -29,7 +29,7 @@ class ReportsController < ApplicationController
         render('_show', layout: 'pdf') if params[:export]
       end
       format.pdf do
-        pdf_file = Exports::Reports::Pdf::ReportExport.export(@current_user, @resource, @current_user, @current_client)
+        pdf_file = Exports::Reports::Pdf::ReportExport.export(@current_user, @resource, @current_user, @current_project)
         send_file pdf_file, type: 'application/pdf'
       end
     end
