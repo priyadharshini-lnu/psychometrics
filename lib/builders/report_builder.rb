933058a6a4c855edc89b7a3021dfecaecbb01903
simon <s.shchukin@sumatosoft.com>
2017-06-06 17:50:57 +0300

    fix typo

--- lib/builders/report_builder.rb
+++ lib/builders/report_builder.rb
@@ -25,12 +25,12 @@ module Builders
             modules = page_params.delete(:modules)
             page = @report.pages.find_or_initialize_by(id: id)
 
-            page.destoy && next if page_params.delete(:removed)
+            page.destroy && next if page_params.delete(:removed)
             page.update(page_params)
 
             modules.each do |module_params|
               mod = page.modules.find_or_initialize_by(id: module_params.delete(:id))
-              mod.destoy && next if module_params.delete(:removed)
+              mod.destroy && next if module_params.delete(:removed)
               mod.update(module_params)
             end
           end
