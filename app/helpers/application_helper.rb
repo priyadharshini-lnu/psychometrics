319d7be136a0a046e6567448705528de37be49f8
simon <s.shchukin@sumatosoft.com>
2017-07-05 19:26:07 +0300

    apply project logo and background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def device_html_style
+    if @current_project&.background&.url
+      "background-image: url('#{@current_project.background.url}');"
+    elsif @current_project&.background_color
+      "background: #{@current_project.background_color};"
+    end
+  end
 end
