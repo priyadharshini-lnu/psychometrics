b44b796717401fea4877729fd806be1e761da882
simon <s.shchukin@sumatosoft.com>
2017-08-01 17:14:05 +0300

    improve priviliges frontend input toggle

--- app/assets/javascripts/administration/users/users.js
+++ app/assets/javascripts/administration/users/users.js
@@ -3,16 +3,47 @@ $(function () {
     var $this = $(this),
         checked = $this.prop('checked');
 
-    if (checked && $this.data('turnOn')) {
-      $.each($this.data('turnOn'), function (i, id) {
-        set_input(id, true)
-      });
-      return false;
+    if (checked) {
+      if ($this.data('turnOn')) {
+        toggle_inputs($this.data('turnOn'), true)
+      }
+
+      if ($this.data('turnOnIf')) {
+        toggle_inputs_if($this.data('turnOnIf'), true)
+      }
+      return;
     }
 
-    if (!checked && $this.data('turnOff')) {
-      $.each($this.data('turnOff'), function (i, id) {
-        set_input(id, false)
+    if ($this.data('turnOff')) {
+      toggle_inputs($this.data('turnOff'), false)
+    }
+
+    if ($this.data('turnOffIf')) {
+      toggle_inputs_if($this.data('turnOffIf'), false)
+    }
+
+    function toggle_inputs_if (arr, val) {
+      $.each(arr, function (i, data) {
+        var disabled = false,
+            enabled = true;
+
+        $.each(data.disabled, function (i, id) {
+          disabled = disabled || $('#' + id).prop('checked')
+        });
+
+        $.each(data.enabled, function (i, id) {
+          enabled = enabled && $('#' + id).prop('checked')
+        });
+
+        if (!disabled && enabled) {
+          toggle_inputs(data.ids, val)
+        }
+      });
+    }
+
+    function toggle_inputs (data, val) {
+      $.each(data, function (i, id) {
+        set_input(id, val)
       });
       return false;
     }
