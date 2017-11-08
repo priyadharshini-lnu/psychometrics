3eb2caff7a8c8209bb802082427d9a34e1fe51c7
Andrey Demidenko <demidenkoandy@gmail.com>
2017-11-08 18:41:44 +0300

    fix(js): move communication js to separate file

--- app/decorators/communication_decorator.rb
+++ app/decorators/communication_decorator.rb
@@ -2,4 +2,12 @@ class CommunicationDecorator < BaseDecorator
   def display_name
     object.subject
   end
+
+  def form_url
+    if object.new_record?
+      helpers.new_form_administration_communications_path
+    else
+      helpers.edit_form_administration_communication_path(object)
+    end
+  end
 end
