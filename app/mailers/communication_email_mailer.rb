c2ab808e68c3be5209ee8003abde56e23e9889f3
Konstantin Klimovich <k.klimovich@sumatosoft.com>
2017-11-28 18:20:52 +0300

    feat(communications): add 'other' communication type. PSYC-778

--- app/mailers/communication_email_mailer.rb
+++ app/mailers/communication_email_mailer.rb
@@ -2,12 +2,12 @@ require 'mustache'
 class CommunicationEmailMailer < ApplicationMailer
   def create(email_id)
     communication_mail = CommunicationEmail.preload(:communication).find(email_id)
-    copy_recipients_emails = Membership.join_user.where(id: communication_mail.communication.copy_membership_ids).pluck(:email)
     @recipient = Membership.join_user.find(communication_mail.membership_id)
-    body = Mustache.render(communication_mail.communication.body, @recipient.slice(:first_name, :last_name, :email).merge!({user_link: accept_invitation_link}))
+    data = @recipient.slice(:first_name, :last_name, :email)
+    data[:user_link] = accept_invitation_link
+    body = Mustache.render(communication_mail.communication.body, data)
     mail(
       to: @recipient.email,
-      cc: copy_recipients_emails,
       subject: communication_mail.communication.subject,
       body: body,
       content_type: 'text/html'
