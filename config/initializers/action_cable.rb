33c3e640d50fbbeb67e48fe131f7d96e492e539a
Andrey Demidenko <demidenkoandy@gmail.com>
2018-04-13 10:52:36 +0300

    feat(config, gems): update gems

---
+++ config/initializers/action_cable.rb
@@ -0,0 +1,10 @@
+require 'action_cable/engine'
+require 'action_cable/subscription_adapter/redis'
+
+module ActionCable
+  module SubscriptionAdapter
+    Redis.class_eval do
+      cattr_accessor(:redis_connector) { ->(config) { ::Redis.new(config.slice(:url, :host, :port, :db, :password, :path)) } }
+    end
+  end
+end
--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,4 +1,4 @@
-Rack::Timeout.service_timeout = Rails.application.secrets.rack_timeout['service_timeout']
-Rack::Timeout.wait_timeout = Rails.application.secrets.rack_timeout['wait_timeout']
-Rack::Timeout.wait_overtime = Rails.application.secrets.rack_timeout['wait_overtime']
-Rack::Timeout.service_past_wait = Rails.application.secrets.rack_timeout['service_past_wait']
+Rack::Timeout.service_timeout = Rails.application.secrets.rack_timeout[:service_timeout]
+Rack::Timeout.wait_timeout = Rails.application.secrets.rack_timeout[:wait_timeout]
+Rack::Timeout.wait_overtime = Rails.application.secrets.rack_timeout[:wait_overtime]
+Rack::Timeout.service_past_wait = Rails.application.secrets.rack_timeout[:service_past_wait]
