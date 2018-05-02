9a89c2b887806723512dabe96aafe455ba6db4d5
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-05-02 13:06:39 +0300

    feat(lib, app, config, spec): completion status export

---
+++ spec/lib/exports/assessments/completion_status_export_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Exports::Assessments::CompletionStatusExport' do
+  let(:client_id) { 0 }
+  let(:exporter) { Exports::Assessments::CompletionStatusExport.new(client_id) }
+
+  describe '#current_level_assigns' do
+    before do
+      expect(Client).to receive(:find).with(client_id).and_return(project)
+      expect(query_object).to receive(:call).with(client_id).and_return(Assign.none)
+    end
+
+    context 'for assessments in project level' do
+      let(:project) { double('project', project?: true) }
+      let(:query_object) { Queries::Assigns::ProjectLevel::ByClient }
+
+      it 'calls Queries::Assigns::ProjectLevel::ByClient' do
+        exporter.current_level_assigns
+      end
+    end
+
+    context 'for assessments in subproject level' do
+      let(:project) { double('project', project?: false) }
+      let(:query_object) { Queries::Assigns::SubProjectLevel::ByClient }
+
+      it 'calls Queries::Assigns::SubProjectLevel::ByClient' do
+        exporter.current_level_assigns
+      end
+    end
+  end
+end
