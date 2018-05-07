3f30615cab316bbc309564ee0fb5940aff185aa0
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-05-07 19:08:56 +0300

    fix(app/controllers, app/views):
    Fix NameError in app/views/application/_lang.html.slim by setting default active locale

--- app/controllers/anonym/assessments_controller.rb
+++ app/controllers/anonym/assessments_controller.rb
@@ -12,8 +12,7 @@ module Anonym
     append_before_action :authenticate_user!
 
     def pass
-      @active_locale = user_locale
-      @translations = ::Translation.to_hash_for_assessment(@resource.id, @active_locale)
+      @translations = ::Translation.to_hash_for_assessment(@resource.id, user_locale)
       @available_translations = ::Translation.available_translation_for_assessment(@resource.id)
       # Find or create assign
       @assign = Assign.find_or_create_by(assessment_id: @resource.id, membership_id: @current_membership.id)
