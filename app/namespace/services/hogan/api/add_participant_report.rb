c401726b05c15525e289faab1b9e4be534b377ec
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-05-31 15:54:54 +0300

    feat(app/controllers, app/namespace):
    Assign report to Hogan participant when assigning a Hogan assessment to a user
    Closes PSYC-956

--- app/namespace/services/hogan/api/add_participant_report.rb
+++ app/namespace/services/hogan/api/add_participant_report.rb
@@ -3,7 +3,8 @@ module Services
     module API
       class AddParticipantReport < Base
         def call
-          response(client.call(:add_participant_reports, message: { inputXML: input_xml }).body)
+          context.body = client.call(:add_participant_reports, message: { inputXML: input_xml }).body
+          context.response = response(context.body)
         end
 
         private
@@ -33,7 +34,7 @@ module Services
         end
 
         def response(body)
-          context.response ||= response_from_xml(body).with_indifferent_access
+          response_from_xml(body).with_indifferent_access
         end
 
         def response_from_xml(body)
