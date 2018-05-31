2a9e3c22b1c76c047419d1943430082ad0fdeb1b
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-05-31 13:52:05 +0300

    feat(app/controllers, app/namespace):
    Add assessment to the Hogan participant when assigning a Hogan assessment to a user
    Closes PSYC-955

--- app/namespace/services/hogan/assign_assessment_and_reports.rb
+++ app/namespace/services/hogan/assign_assessment_and_reports.rb
@@ -6,26 +6,37 @@ module Services
       def call
         context.group = context.assessment_params[:group]
         context.membership = context.assessment_params[:membership]
+        context.assessment = context.assessment_params[:assessment]
 
-        ensure_group_present
-        ensure_participant_added_to_group
+        create_group
+        add_participant_to_group
+        add_participant_assessment
       end
 
       private
 
-      def ensure_group_present
+      def create_group
         result = Services::Hogan::API::GroupDetails.call(group: context.group)
         return if result.success?
 
         Services::Hogan::API::CreateGroup.call!(group: context.group)
       end
 
-      def ensure_participant_added_to_group
+      def add_participant_to_group
         return if context.membership.hogan_credential.present?
 
         password = Devise.friendly_token.first(10)
         result = Services::Hogan::API::AddParticipantToGroup.call!(group: context.group, password: password)
         context.membership.create_hogan_credential(password: password, participant_id: result.participant_id)
+      end
+
+      def add_participant_assessment
+        Services::Hogan::API::AddParticipantAssessment.call!(
+          participant_id: context.membership.hogan_credential.participant_id,
+          group: context.group,
+          assessment_id: context.assessment.hogan_assessment_setting.hogan_assessment_id,
+          form_id: context.assessment.hogan_assessment_setting.hogan_form_id
+        )
       end
     end
   end
