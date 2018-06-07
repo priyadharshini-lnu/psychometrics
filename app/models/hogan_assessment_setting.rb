15adb2ccc43177f42bb77278cbd88aa0ffa264e2
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-06-07 20:20:15 +0300

    feat(controllers, assets, models, views):
    Add ability to select hogan assessment_id when creating a hogan assessment
    Closes PSYC-975

--- app/models/hogan_assessment_setting.rb
+++ app/models/hogan_assessment_setting.rb
@@ -3,4 +3,13 @@ class HoganAssessmentSetting < ApplicationRecord
 
   validates :hogan_form_id, presence: true
   validates :hogan_assessment_id, presence: true
+
+  before_validation :set_hogan_form_id
+
+  private
+
+  def set_hogan_form_id
+    setting = Settings.hogan.find { |i| i[:assessment_id] == self.hogan_assessment_id }
+    self.hogan_form_id = setting.form_id
+  end
 end
