1996bc9b7f0e007fb6c59893f2eab8fed2bd613f
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-06-11 16:30:52 +0300

    fix(controllers):
    Fix filename for hogan assessment results
    Closes PSYC-992

--- app/controllers/administration/clients/assessments_controller.rb
+++ app/controllers/administration/clients/assessments_controller.rb
@@ -31,7 +31,7 @@ module Administration
         @assessment = Assessment.find(params[:assessment_id])
         results = ::Exports::Assessments::HoganResultsExport.new(client.id, @assessment.id, params[:hogan_report_id])
         respond_to do |format|
-          format.xlsx { send_data results.to_xlsx.to_stream.read, filename: 'export_scoring.xlsx' }
+          format.xlsx { send_data results.to_xlsx.to_stream.read, filename: 'hogan_assessment_results.xlsx' }
         end
       end
 
