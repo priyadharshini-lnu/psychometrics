c6a98f90e247b46230747fde02ebeba837a9600b
Uladzislau Dorash <u.dorash@sumatosoft.com>
2018-08-01 13:07:46 +0300

    feat: add 'enable_back' attribute to assessments
    Closes PSYC-1047

--- app/serializers/assessments/assessment_serializer.rb
+++ app/serializers/assessments/assessment_serializer.rb
@@ -1,6 +1,6 @@
 module Assessments
   class AssessmentSerializer < ActiveModel::Serializer
-    attributes :id, :name, :category, :disabled, :created_at, :flow, :norm_rules, :factors
+    attributes :id, :name, :category, :disabled, :created_at, :flow, :norm_rules, :factors, :enable_back
 
     has_many :blocks, serializer: Assessments::BlockSerializer do
       object.blocks.
---
+++ db/migrate/20180731094932_add_enable_back_to_assessments.rb
@@ -0,0 +1,5 @@
+class AddEnableBackToAssessments < ActiveRecord::Migration[5.1]
+  def change
+    add_column :assessments, :enable_back, :boolean, default: false, null: false
+  end
+end
