661e02902a6b282dacadc1381c19d653aa8467f9
Shuja Shabandri <shuja@shabandri.com>
2018-10-16 00:37:20 +0400

    Added case study licensing

--- app/models/assigns_report.rb
+++ app/models/assigns_report.rb
@@ -14,6 +14,7 @@ class AssignsReport < ApplicationRecord
   LICENSES = {
       Assessment::PSYCHOMETRIC => Licenses::AssignReportPsychometrics,
       Assessment::ORGANISATIONAL => Licenses::AssignReportOrgSurvey,
+      Assessment::CASE_STUDY => Licenses::AssignReportCaseStudy,
       Assessment::NUM_360 => Licenses::AssignReport_360_Feedback,
       Assessment::MINDMILL => Licenses::AssignReportMindmill,
       Assessment::HOGAN => Licenses::AssignReportHogan
