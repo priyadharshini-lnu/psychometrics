8ae9ad1a4a0f7284ec41b932f25c8b96ed916035
atanych <atanych@gmail.com>
2018-10-28 22:17:02 +0300

    Replace simple js confirm with bootstrap modal
    https://gitlab.com/tte-lighthouse/psychometrics/issues/41

--- app/assets/javascripts/application/assigns/_assign.js
+++ app/assets/javascripts/application/assigns/_assign.js
@@ -1,37 +1,49 @@
+// TODO (atanych): Im sorry for a bad code. But I dont know any idea, except to move all frontend on React/Redux side
 $(function () {
+
+  var storage = {}
+
   $(document).on('click', '#user #assigns_index .assign a', function (e) {
     e.preventDefault()
     processPrivacyConsent($(this))
   })
 
   function processPrivacyConsent($el) {
-    var textConfirm = $el.data('privacy-consent').confirm_text,
-      isMindMill = $el.hasClass('mindmill_link'),
+    var isMindMill = $el.hasClass('mindmill_link'),
       href = $el.data('privacy-consent').href,
       needConfirm = !!$el.data('privacy-consent').need_confirm
-    if (!needConfirm) { return isMindMill ? processMindMill($el) : location.href = href }
-    const confirmed = confirm(textConfirm)
-    if (!confirmed) { return null }
-    $.ajax({
-      url: "/assigns/accept_privacy",
-      method: 'POST',
-      success: function() {
-        if (isMindMill) {
-          processMindMill($el)
-        } else {
-          location.href = href
-        }
-      }
-    })
+    if (!needConfirm) { return isMindMill ? processMindMill($el.data('mindmill')) : location.href = href }
+    storage = {
+      isMindMill: isMindMill,
+      mindmillData: $el.data('mindmill'),
+      href: href
+    }
+    $('.modal-accept-privacy').modal('show')
+
   }
 
-  function processMindMill($el) {
-    var textConfirm = $el.data('mindmill').confirm_text,
-      href = $el.data('mindmill').href,
-      needConfirm = !!$el.data('mindmill').need_confirm,
+  function processMindMill(data) {
+    var textConfirm = data.confirm_text,
+      href = data.href,
+      needConfirm = !!data.need_confirm,
       confirmed = needConfirm && confirm(textConfirm)
     if(needConfirm === false || confirmed === true) {
       window.open(href, 'windowMindmill', 'width=980,height=700,resizable,scrollbars')
     }
   }
+
+  // Modal handler
+  $('.modal-accept-privacy .btn-success').on('click', function() {
+    $.ajax({
+      url: "/assigns/accept_privacy",
+      method: 'POST',
+      success: function() {
+        if (storage.isMindMill) {
+          processMindMill(storage.mindmillData)
+        } else {
+          location.href = storage.href
+        }
+      }
+    })
+  })
 })
