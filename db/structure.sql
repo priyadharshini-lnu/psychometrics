766eca03d72967c535e890e2501de40cc0c184ad
Shuja Shabandri <shuja@shabandri.com>
2018-11-19 19:41:30 +0400

    change db

--- db/structure.sql
+++ db/structure.sql
@@ -82,7 +82,6 @@ CREATE TABLE public.assessments (
     type character varying,
     mindmill_id integer,
     enable_back boolean DEFAULT false NOT NULL,
-    enable_progress boolean DEFAULT true,
     extra jsonb DEFAULT '{}'::jsonb NOT NULL,
     icon character varying
 );
@@ -162,8 +161,7 @@ CREATE TABLE public.assigns (
     project_assign_id integer,
     mindmill_report character varying,
     selected_locale character varying,
-    mindmill_prefix character varying,
-    external_results json
+    mindmill_prefix character varying
 );
 
 
@@ -321,8 +319,7 @@ CREATE TABLE public.clients (
     ancestry character varying,
     ancestry_depth integer DEFAULT 0,
     end_level boolean DEFAULT false,
-    hogan_group_name character varying,
-    privacy_consent boolean
+    hogan_group_name character varying
 );
 
 
@@ -1252,37 +1249,6 @@ ALTER SEQUENCE public.occupations_id_seq OWNED BY public.occupations.id;
 
 
 --
--- Name: privacy_consents; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.privacy_consents (
-    id bigint NOT NULL,
-    membership_id bigint,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
-);
-
-
---
--- Name: privacy_consents_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.privacy_consents_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: privacy_consents_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.privacy_consents_id_seq OWNED BY public.privacy_consents.id;
-
-
---
 -- Name: product_images; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -2047,13 +2013,6 @@ ALTER TABLE ONLY public.occupations_factors ALTER COLUMN id SET DEFAULT nextval(
 
 
 --
--- Name: privacy_consents id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.privacy_consents ALTER COLUMN id SET DEFAULT nextval('public.privacy_consents_id_seq'::regclass);
-
-
---
 -- Name: product_images id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -2413,14 +2372,6 @@ ALTER TABLE ONLY public.occupations_factors
 
 ALTER TABLE ONLY public.occupations
     ADD CONSTRAINT occupations_pkey PRIMARY KEY (id);
-
-
---
--- Name: privacy_consents privacy_consents_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.privacy_consents
-    ADD CONSTRAINT privacy_consents_pkey PRIMARY KEY (id);
 
 
 --
@@ -3013,13 +2964,6 @@ CREATE INDEX index_occupations_on_dimension_id ON public.occupations USING btree
 
 
 --
--- Name: index_privacy_consents_on_membership_id; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX index_privacy_consents_on_membership_id ON public.privacy_consents USING btree (membership_id);
-
-
---
 -- Name: index_product_images_on_product_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -3392,14 +3336,6 @@ ALTER TABLE ONLY public.clients
 
 ALTER TABLE ONLY public.communications
     ADD CONSTRAINT fk_rails_639c49fe3d FOREIGN KEY (creator_id) REFERENCES public.users(id) ON DELETE SET NULL;
-
-
---
--- Name: privacy_consents fk_rails_6cd91d815a; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.privacy_consents
-    ADD CONSTRAINT fk_rails_6cd91d815a FOREIGN KEY (membership_id) REFERENCES public.memberships(id) ON DELETE CASCADE;
 
 
 --
@@ -3877,11 +3813,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180915101319'),
 ('20181002152730'),
 ('20181010120450'),
-('20181013151355'),
 ('20181022210715'),
-('20181028143714'),
-('20181028180057'),
-('20181112210040'),
 ('20181118154257'),
 ('20181119095817');
 
