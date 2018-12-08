f1c20030ac9b965c22d2ae0d743a2363f2d7e70a
atanych <atanych@gmail.com>
2018-12-08 18:29:15 +0300

    Stub job with fake report

--- app/jobs/build_mindmill_results_job.rb
+++ app/jobs/build_mindmill_results_job.rb
@@ -5,8 +5,9 @@ class BuildMindmillResultsJob < ApplicationJob
   def perform(assign, current_membership, user_locale)
     mindmill = Api::Mindmill.new(assign, current_membership, user_locale)
     mindmill.load_results
-    redirect_back(fallback_location: root_path, error: t('.not_completed')) && return unless mindmill.report
-    report = "data:application/pdf;base64,#{mindmill.report}"
-    @assign.update(mindmill_report: report, status: :completed, completed_at: Time.current)
+    # TODO (atanych): uncomment when MM integration works
+    # report = "data:application/pdf;base64,#{mindmill.report}"
+    report = "data:application/pdf;base64,#{File.read('lib/imports/external/samples/mind_mill_import.xml')}"
+    assign.update(mindmill_report: report, status: :completed, completed_at: Time.current)
   end
 end
