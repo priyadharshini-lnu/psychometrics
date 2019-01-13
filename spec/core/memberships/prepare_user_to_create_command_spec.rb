d32111523e308f781719eca7432a533329716878
atanych <atanych@gmail.com>
2019-01-14 01:10:04 +0300

    Cover prepare command by tests

---
+++ spec/core/memberships/prepare_user_to_create_command_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Memberships::PrepareUserToCreateCommand do
+  # let(:assessment) { create(:assessment) }
+  # let(:client_id) { 0 }
+  # let(:exporter) { Exports::Assessments::AssessmentResultsExport.new(assessment, client_id) }
+
+  describe '.call' do
+    it "returns prepared membership with new user" do
+      events = described_class.call(stub_form({valid?: true, email: 'carpazzi@gmail.com'}), {'assessments' => ['view']})
+
+      expect(events[:ok].grants.data).to eq({'assessments' => ['view']})
+      expect(events[:ok].user.email).to eq('carpazzi@gmail.com')
+    end
+
+    context "returns prepared membership with existing user" do
+      before { create(:client_admin, email: 'vasiliy@gmail.com', first_name: 'Vasiliy', last_name: 'Pupkin') }
+      it {
+        events = described_class.call(stub_form({valid?: true, email: 'vasiliy@gmail.com'}), {'assessments' => ['view']})
+
+        expect(events[:ok].grants.data).to eq({'assessments' => ['view']})
+        expect(events[:ok].user.first_name).to eq('Vasiliy')
+        expect(events[:ok].user.last_name).to eq('Pupkin')
+      }
+    end
+  end
+end
