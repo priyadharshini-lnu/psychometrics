7c4d07afbbf5c69a12dc6231f52b7247b45bee75
atanych <atanych@gmail.com>
2019-03-15 00:11:00 +0300

    Unify `/api/v1/projects/{project_id}/users/{user_id}/campaigns`

--- app/commands/administration/clients/create_user.rb
+++ app/commands/administration/clients/create_user.rb
@@ -32,13 +32,15 @@ module Administration
       def create_membership_and_user(client)
         @membership = client.memberships.new(form.membership_attributes)
         membership.user = User.find_or_initialize_by(email: form.email, project_id: client.project.id)
-        membership.user.assign_attributes(form.user_attributes)
+        if membership.user.new_record?
+          membership.user.assign_attributes(form.user_attributes)
 
-        # Sets invitation data
-        membership.user.tap do |u|
-          u.create_by_invite = true
-          u.created_by_id = current_user.id
-          u.modified_by_id = current_user.id
+          # Sets invitation data
+          membership.user.tap do |u|
+            u.create_by_invite = true
+            u.created_by_id = current_user.id
+            u.modified_by_id = current_user.id
+          end
         end
 
         membership.save!
