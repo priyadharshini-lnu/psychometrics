7cdd62c874377c1c2a6c07d8e87bf98c72bd5cbe
atanych <atanych@gmail.com>
2019-04-26 00:24:41 +0300

    Add saga to project
    https://gitlab.com/tte-lighthouse/psychometrics/issues/137

--- app/frontend/admin/core/threeSixtyCampaign/subjects/index.js
+++ app/frontend/admin/core/threeSixtyCampaign/subjects/index.js
@@ -1,3 +1,5 @@
+import { takeLatest, put } from 'redux-saga/effects'
+
 const FETCH_SUBJECTS = 'threeSixty/subjects/FETCH_SUBJECTS'
 export const CREATE_ALL = 'threeSixty/subjects/CREATE_ALL'
 export const defaultState = []
@@ -11,12 +13,14 @@ export const fetchSubjects = campaignId => ({
 
 export const createAll = (campaignId, subjects) => ({
   type: CREATE_ALL,
+  campaignId,
   request: {
     method: 'post',
     url: `/administration/threesixty_campaigns/${campaignId}/subjects/create_all`,
     body: { subjects },
   },
 })
+
 
 export default function reducer (state = defaultState, action) {
   switch (action.type) {
@@ -26,3 +30,11 @@ export default function reducer (state = defaultState, action) {
       return state
   }
 }
+
+function* refetchSubjects ({ requestAction }) {
+  yield put(fetchSubjects(requestAction.campaignId))
+}
+
+export const watchers = [
+  takeLatest(CREATE_ALL, refetchSubjects),
+]
