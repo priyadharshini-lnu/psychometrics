3746a4193d7cb63642f39b9686bb33ff5bf72e2d
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2019-05-15 11:54:15 +0300

    [198] Fixes

--- app/controllers/users_results_controller.rb
+++ app/controllers/users_results_controller.rb
@@ -3,7 +3,7 @@ class UsersResultsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update
-    users_result = UsersResult.find(params[:id])
+    users_result = UsersResult.find_by(id: params[:id], evaluator_id: current_user.id)
     form = ::UsersResults::UpdatingForm.from_params(params.require(:resource))
     ::UsersResults::UpdateUsersResult.call(form, users_result, current_user)
 
