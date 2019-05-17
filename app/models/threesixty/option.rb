0114e94bd69acc8e0fb25cc9a5fc9ca66a5a3715
atanych <atanych@gmail.com>
2019-05-17 22:51:49 +0300

    Add default participant options

--- app/models/threesixty/option.rb
+++ app/models/threesixty/option.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module Threesixty
   class Option < ApplicationRecord
+    DEFAULT_PARTICIPANTS = { manager: {}, subject: {}, evaluator: {} }.freeze
     belongs_to :campaign
   end
 end
