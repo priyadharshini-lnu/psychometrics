e7a2ab6879a9803d8acb5db40b75d18117bf141b
atanych <atanych@gmail.com>
2019-05-20 22:00:00 +0300

    Fix norm_data

--- app/core/users_results/updating_form.rb
+++ app/core/users_results/updating_form.rb
@@ -2,7 +2,7 @@ module UsersResults
   class UpdatingForm < Rectify::Form
     attribute :status, Integer
     attribute :embedded_data, Hash, default: nil
-    attribute :norm_data, Hash, default: nil
+    attribute :norm_id, Integer, default: nil
     attribute :answers, Hash, default: nil
   end
 end
