eebca421e75a64bc4d3dcc2ee2e3dcd29c3ecaf2
Fedor Tarasov <tarassovf@gmail.com>
2019-05-21 23:00:41 +0300

    add ant overrides to admin side

--- app/frontend/packs/admin/threeSixtyCampaign.js
+++ app/frontend/packs/admin/threeSixtyCampaign.js
@@ -1,7 +1,7 @@
 import ThreeSixtyApp from 'admin/core/threeSixtyCampaign/components/App'
 import ReactDOM from 'react-dom'
 import React from 'react'
-import 'antd/dist/antd.css'
+import 'admin/styles/ant.less'
 import 'admin/assets/scss/common.scss'
 
 ReactDOM.render(<ThreeSixtyApp />, document.getElementById('three-sixty-container'))
