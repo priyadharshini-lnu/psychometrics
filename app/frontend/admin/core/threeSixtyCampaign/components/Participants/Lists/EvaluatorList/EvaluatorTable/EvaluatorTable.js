ab63380aeeb2565c95d7094bcceb15ddef950fe4
atanych <atanych@gmail.com>
2019-05-22 00:04:45 +0300

    Open participant modal by email

--- app/frontend/admin/core/threeSixtyCampaign/components/Participants/Lists/EvaluatorList/EvaluatorTable/EvaluatorTable.js
+++ app/frontend/admin/core/threeSixtyCampaign/components/Participants/Lists/EvaluatorList/EvaluatorTable/EvaluatorTable.js
@@ -10,21 +10,15 @@ export default function EvaluatorTable ({
   evaluators, openModal, onCloseParticipantModal, campaignId,
 }) {
   return (
-    <Table
-      className="mtm"
-      rowKey="id"
-      dataSource={evaluators}
-      pagination={false}
-      onRow={record => ({
-        onClick: (e) => {
-          if (['TR', 'TD'].includes(e.target.tagName)) {
-            openModal('ParticipantModal', { user: record.user, onClose: onCloseParticipantModal })
-          }
-        },
-      })}
-    >
+    <Table className="mtm" rowKey="id" dataSource={evaluators} pagination={false}>
       <Column title="Name" key="fullName" render={({ user }) => userPresenter.getFullName(user)} />
-      <Column title="Email" dataIndex="user.email" key="email" />
+      <Column
+        title="Email"
+        key="user_email"
+        render={({ user }) => (
+          <a onClick={() => openModal('ParticipantModal', { user, onClose: onCloseParticipantModal })}>{user.email}</a>
+        )}
+      />
       <Column title="Evaluations Received" dataIndex="evaluators" key="received_evaluations" />
       <Column title="Evaluations Completed" dataIndex="evaluations" key="completed_evaluations" />
       <Column title="Report Status" dataIndex="reportStatus" key="report_status" />
