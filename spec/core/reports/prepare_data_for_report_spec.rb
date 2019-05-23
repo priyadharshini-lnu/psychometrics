8d49670976d89dc5434cd209ea919eb2999ff674
Fedor Tarasov <tarassovf@gmail.com>
2019-05-24 01:28:45 +0300

    update specs

--- spec/core/reports/prepare_data_for_report_spec.rb
+++ spec/core/reports/prepare_data_for_report_spec.rb
@@ -47,12 +47,12 @@ describe Reports::PrepareDataForReport do
 
     before do
       allow_any_instance_of(Report).to receive(:category_threesixty?).and_return(true)
-      create(:participant, subject: subject.user, evaluator: evaluator_1, relationship: manager)
-      create(:participant, subject: subject.user, evaluator: evaluator_2, relationship: peer)
-      create(:participant, evaluator: evaluator_3, relationship: customer)
-      create(:users_result, status: 'completed', assessment: threesixty_campaign.assessment, subject: subject.user, evaluator: evaluator_1)
-      create(:users_result, status: 'completed', assessment: threesixty_campaign.assessment, subject: subject.user, evaluator: evaluator_2)
-      create(:users_result, status: 'completed', assessment: threesixty_campaign.assessment, subject: subject.user, evaluator: evaluator_3)
+      create(:participant, campaign: threesixty_campaign.campaign, subject: subject.user, evaluator: evaluator_1, relationship: manager)
+      create(:participant, campaign: threesixty_campaign.campaign, subject: subject.user, evaluator: evaluator_2, relationship: peer)
+      create(:participant, campaign: threesixty_campaign.campaign, evaluator: evaluator_3, relationship: customer)
+      create_users_result(threesixty_campaign, subject, evaluator_1)
+      create_users_result(threesixty_campaign, subject, evaluator_2)
+      create_users_result(threesixty_campaign, subject, evaluator_3)
     end
 
     it do
@@ -65,5 +65,14 @@ describe Reports::PrepareDataForReport do
       results = described_class.new(args).serialize_results
       expect(results[threesixty_campaign.assessment.id].map{|r| r[:relationship]}).to match_array %w[manager peer]
     end
+  end
+
+  def create_users_result threesixty_campaign, subject, evaluator
+    create(:users_result,
+      campaign: threesixty_campaign.campaign,
+      status: 'completed',
+      assessment: threesixty_campaign.assessment,
+      subject: subject.user,
+      evaluator: evaluator)
   end
 end
