c9a10d141fe34676c559311ef1182d6b3f1b014e
Shuja Shabandri <shuja@shabandri.com>
2019-05-27 11:40:33 +0000

    Apply suggestion to app/jobs/hogan/assign_and_load_results_job.rb

--- app/jobs/hogan/assign_and_load_results_job.rb
+++ app/jobs/hogan/assign_and_load_results_job.rb
@@ -28,7 +28,7 @@ module Hogan
 
     def load_results
       reports.each do |report|
-        unless report.hogan?
+        if report.hogan?
           Hogan::LoadResults.call!(assign_with_result, report, membership_with_result, project)
         end
       end
