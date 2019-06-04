28bdb3b5f1f906dd4178fbff329311456c20daac
atanych <atanych@gmail.com>
2019-06-04 17:08:24 +0300

    Create self-evaluator when subject is created
    https://gitlab.com/tte-lighthouse/psychometrics/issues/214

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -6,4 +6,8 @@ class Relationship < ApplicationRecord
   belongs_to :campaign
   enum type: { global: 0, campaign: 1 }
   enum assign_type: { manual: 0, automatic: 1 }
+
+  def self.self_relationship
+    Relationship.find_by(name: 'Self')
+  end
 end
--- spec/core/threesixty/subjects/create_all_spec.rb
+++ spec/core/threesixty/subjects/create_all_spec.rb
@@ -10,11 +10,15 @@ describe Threesixty::Subjects::CreateAll do
     user = create(:user, project: project, email: 'fedor@gmail.com')
     create(:campaigns_user, user: user, campaign: campaign)
     create(:threesixty_subject, user: user, campaign: campaign)
+    create(:relationship, name: 'Self')
   end
   describe '.call' do
-    it 'duplicated emails' do
+    it do
       subjects = described_class.call!({ "0": { email: 'dev.atanov@gmail.com' }, "1": { email: 'fedor@gmail.com' } }, threesixty_campaign)
+      participants = Participant.all
       expect(subjects.map { |s| s.user.email }).to match_array(%w[fedor@gmail.com dev.atanov@gmail.com])
+      expect(participants.map { |s| s.evaluator.email }).to match_array(%w[fedor@gmail.com dev.atanov@gmail.com])
+      expect(participants.map { |s| s.subject.email }).to match_array(%w[fedor@gmail.com dev.atanov@gmail.com])
     end
   end
 end
