c817fbc33909444cb99f57ad1d834379ea96ee6c
Fedor Tarasov <tarassovf@gmail.com>
2019-06-10 23:19:38 +0300

    use real campaign nomination requirements

---
+++ app/serializers/threesixty/end_user/nomination_requirement_serializer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+module Threesixty::EndUser
+  class NominationRequirementSerializer < ActiveModel::Serializer
+    attributes :id, :name, :position, :subject_conditions, :conditions
+  end
+end
