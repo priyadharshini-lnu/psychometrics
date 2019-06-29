293a39ab0949569601dd453a248c1ef380de50b6
atanych <atanych@gmail.com>
2019-06-30 01:02:37 +0300

    Adapt get_report_status for new requirements

--- app/core/threesixty/participants/get_report_status.rb
+++ app/core/threesixty/participants/get_report_status.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+#
+# Set to 'On hold' when Admin marks report as on hold from Subject listing page
+# Set to 'Approved' when 'Manager approves reports' is set and Manager have approved report for Subject
+# Set to Available when following conditions are meet
+#
+#
+# If 'Manager approves reports' is not set and Report availability conditions are meet
+# Or if Admin approves the report (even if report availability conditions are not meet)
+# Or if Admin Releases the report (even if report availability conditions are not meet)
+#
 module Threesixty
   module Participants
     class GetReportStatus < BaseCommand
@@ -7,26 +17,32 @@ module Threesixty
       DENIED = 'denied'
       AVAILABLE = 'available'
       APPROVED = 'approved'
+      ON_HOLD = 'on_hold'
 
-      def initialize(subject, option)
+      def initialize(subject, option, subject_evaluator_counters)
         @subject = subject
         @option = option
+        @subject_evaluator_counters = subject_evaluator_counters
       end
 
       def call
-        return broadcast :ok, nil unless subject
-        return broadcast :ok, DENIED if subject.report_denied?
-        return broadcast :ok, INCOMPLETE unless Threesixty::Reports::IsAvailable.call!(subject)
+        return broadcast :ok, ON_HOLD if subject.report_status_on_hold?
+        return broadcast :ok, APPROVED if manager_can_approve_evaluations? && subject.report_approved?
 
-        return broadcast :ok, AVAILABLE unless option.participants.dig('manager', 'can_approves_evaluations')
-        return broadcast :ok, APPROVED if subject.report_approved?
+        return broadcast :ok, AVAILABLE if Threesixty::Reports::IsAvailable.call!(subject, option, subject_evaluator_counters) && !manager_can_approve_evaluations?
+        return broadcast :ok, AVAILABLE if subject.report_approved?
+        return broadcast :ok, AVAILABLE if subject.report_status_released?
 
         broadcast :ok, INCOMPLETE
       end
 
       private
 
-      attr_reader :subject, :option
+      attr_reader :subject, :option, :subject_evaluator_counters
+
+      def manager_can_approve_evaluations?
+        !!option.participants.dig('manager', 'can_approves_evaluations')
+      end
     end
   end
 end
--- app/core/threesixty/reports/is_available.rb
+++ app/core/threesixty/reports/is_available.rb
@@ -2,9 +2,13 @@
 
 module Threesixty
   module Reports
+    # TODO (atanych): should be added:
+    # subject.report_approved? || subject.report_status_released?
     class IsAvailable < BaseCommand
-      def initialize(subject)
+      def initialize(subject, option, subject_evaluator_counters)
         @subject = subject
+        @option = option
+        @subject_evaluator_counters = subject_evaluator_counters
       end
 
       def call; end
