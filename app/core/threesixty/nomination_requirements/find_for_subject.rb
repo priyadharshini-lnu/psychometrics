3daeb4901d6411459df48b5f94b32189e1c19fbd
atanych <atanych@gmail.com>
2019-06-30 00:53:10 +0300

    Avoiding N+1 for the command FindForSubject

--- app/core/threesixty/nomination_requirements/find_for_subject.rb
+++ app/core/threesixty/nomination_requirements/find_for_subject.rb
@@ -3,11 +3,14 @@
 module Threesixty
   module NominationRequirements
     class FindForSubject < BaseCommand
-      attr_reader :subject, :threesixty_campaign, :subject_conditions
+      attr_reader :subject, :threesixty_campaign, :subject_conditions, :datasheet_row_data, :nomination_requirements
 
-      def initialize(subject, threesixty_campaign)
+      # We use datasheet_row_map in order to avoid N+1 queries
+      def initialize(subject, threesixty_campaign, datasheet_row_map = nil, nomination_requirements = nil)
         @subject = subject
         @threesixty_campaign = threesixty_campaign
+        @nomination_requirements = nomination_requirements || fetch_nomination_requirements
+        @datasheet_row_data = datasheet_row_map ? (datasheet_row_map[subject.user.email]&.data || {}) : fetch_datasheet_row_data
       end
 
       def call
@@ -21,25 +24,26 @@ module Threesixty
         field = condition['field']
         value = condition['value']&.downcase
         value_from_datasheet = datasheet_row_data[field].to_s.downcase
-        result = if condition['comparator'] == 'equal'
-          value_from_datasheet == value.downcase
-        else
-          value_from_datasheet != value.downcase
-        end
+        result =
+          if condition['comparator'] == 'equal'
+            value_from_datasheet == value.downcase
+          else
+            value_from_datasheet != value.downcase
+          end
 
-        {operator: condition['operator'], result: result}
+        { operator: condition['operator'], result: result }
       end
 
       private
 
-      def datasheet_row_data
-        @datasheet_row_data ||= threesixty_campaign.datasheet&.
+      def fetch_datasheet_row_data
+        threesixty_campaign.datasheet&.
           rows&.
-          find_by(email: subject.user.email)
-          &.data || {}
+          find_by(email: subject.user.email)&.
+          data || {}
       end
 
-      def nomination_requirements
+      def fetch_nomination_requirements
         threesixty_campaign.nomination_requirements.order(:position)
       end
     end
