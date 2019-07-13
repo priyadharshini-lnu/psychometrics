60578bdc098e44e7c9c915372a038218cdd0d165
Rohan Pujari <rohanpujaris@gmail.com>
2019-07-13 22:11:41 +0530

    Add tests and few fixes

--- app/models/threesixty/email_template.rb
+++ app/models/threesixty/email_template.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-class Threesixty::EmailTemplate < ApplicationRecord
-  enum category: { invitations: 0, reminders: 1, report_ready: 2, approvals: 3 }
+module Threesixty
+  class EmailTemplate < ApplicationRecord
+    belongs_to :threesixty_campaign, class_name: "Threesixty::Campaign"
+
+    enum category: { invitations: 0, reminders: 1, report_ready: 2, approvals: 3 }
+  end
 end
