7f9733b530cb195e65a8bcad6a1c18b435db84d5
Shuja Shabandri <shuja@shabandri.com>
2019-07-18 15:30:40 +0400

    fixes N+1 for factor scoring and uses bulk import

--- lib/builders/scoring_builder.rb
+++ lib/builders/scoring_builder.rb
@@ -19,11 +19,14 @@ module Builders
     def save
       ActiveRecord::Base.transaction do
         begin
+          rows = []
           @scoring_list.each do |scoring|
             factors_scoring_attributes = scoring.slice(:factor_id, :question_id, :props).merge({ assessment_id: @assessment.id })
-            factors_scoring = FactorsScoring.find_or_initialize_by(factor_id: scoring[:factor_id], question_id: scoring[:question_id], assessment_id: @assessment.id)
-            factors_scoring.update(factors_scoring_attributes)
+            rows << factors_scoring_attributes.to_h
           end
+          rows.uniq! {|r| "#{r[:factor_id]}-#{r[:question_id]}-#{r[:assessment_id]}" }
+          FactorsScoring.import rows, on_duplicate_key_update: {conflict_target: [:factor_id, :question_id, :assessment_id], columns: [:props]}
+          FactorsScoring.where("props::text = '[]'").delete_all
         rescue => e
           Rails.logger.info(e)
           return false
