cc53cc7caf8a02378292dce808854e4998e0c9f9
Rohan Pujari <rohanpujaris@gmail.com>
2019-07-20 21:13:37 +0530

    Changes to handle custom message

--- app/core/threesixty/participatable_by_criteria/filter.rb
+++ app/core/threesixty/participatable_by_criteria/filter.rb
@@ -51,35 +51,42 @@ module Threesixty
       ]
 
 
-      def initialize(threesixty_campaign, participatable_type, criteria_list)
+      def initialize(threesixty_campaign, participatable_types, criteria_list)
         @threesixty_campaign = threesixty_campaign
-        @participatable_type = participatable_type
+        @participatable_types = Array.wrap(participatable_types)
         @criteria_list = criteria_list
-        set_participatables
       end
 
       def call
-        CRITERIA_RESOLVER.each do |resolver|
-          valid_criteria = criteria_list.select { |c| resolver[:field_types].include?(c['field']) }
-          next if valid_criteria.empty?
+        participatables = participatable_types.each_with_object([]) do |participatable_type, acc|
+          acc << get_participantables_by_filter(participatable_type)
+        end.flatten.uniq
 
-          @participatables = resolver[:class_name].call!(
-            threesixty_campaign: threesixty_campaign,
-            participatable_type: participatable_type,
-            participatables: @participatables,
-            criteria_list: valid_criteria,
-          )
-        end
-        broadcast :ok, @participatables
+        broadcast :ok, participatables
       end
 
       private
 
+      attr_reader :threesixty_campaign, :participatable_types, :criteria_list
 
-      attr_reader :threesixty_campaign, :participatable_type, :criteria_list
+      def get_participantables_by_filter(participatable_type)
+        participatables = set_participatables(participatable_type)
+        CRITERIA_RESOLVER.each do |resolver|
+          valid_criteria = criteria_list.select { |c| resolver[:field_types].include?(c['field']) }
+          next if valid_criteria.empty?
 
-      def set_participatables
-        @participatables ||= if participatable_type == :subject
+          participatables = resolver[:class_name].call!(
+            threesixty_campaign: threesixty_campaign,
+            participatable_type: participatable_type,
+            participatables: participatables,
+            criteria_list: valid_criteria,
+          )
+        end
+        participatables
+      end
+
+      def set_participatables(participatable_type)
+        if participatable_type == :subject
           threesixty_campaign.subjects
         elsif participatable_type == :evaluator
           threesixty_campaign.evaluators
