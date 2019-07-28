9a7cc3eaa73dfd3331ae61421878fe256fcc07fe
Shuja Shabandri <shuja@shabandri.com>
2019-07-28 18:47:48 +0400

    fix migration error

--- db/migrate/20190721163707_add_self_relationship.rb
+++ db/migrate/20190721163707_add_self_relationship.rb
@@ -1,5 +1,6 @@
 class AddSelfRelationship < ActiveRecord::Migration[5.1]
   def change
+    Relationship.reset_column_information
     Relationship.create(name: 'Self', type: :global, assign_type: :automatic)
   end
 end
