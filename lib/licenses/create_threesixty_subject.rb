aee82f1878242f2afbed345b1c1307b080088632
Rohan Pujari <rohanpujaris@gmail.com>
2019-08-12 23:01:08 +0530

    Uncomment comment code

--- lib/licenses/create_threesixty_subject.rb
+++ lib/licenses/create_threesixty_subject.rb
@@ -11,14 +11,14 @@ module Licenses
     end
 
     def build_license
-      # license = fetch_available_license
-      # unless license
-      #   message = 'There are not enough licenses'.html_safe
-      #   raise Errors::LicenseError.new(client, nil, user, message)
-      # end
+      license = fetch_available_license
+      unless license
+        message = 'There are not enough licenses'.html_safe
+        raise Errors::LicenseError.new(client, nil, user, message)
+      end
 
-      # extras = { subject_name: user.decorate.full_name, subject_email: user.email, campaign_name: campaign.name }
-      # license.license_usages.create!(client: client, user: user, campaign: campaign, extras: extras)
+      extras = { subject_name: user.decorate.full_name, subject_email: user.email, campaign_name: campaign.name }
+      license.license_usages.create!(client: client, user: user, campaign: campaign, extras: extras)
     end
 
     def fetch_available_license
