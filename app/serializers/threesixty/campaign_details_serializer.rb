8f561915b134ca910f8449110291ef829599f815
atanych <atanych@gmail.com>
2019-08-19 17:44:29 +0300

    Add proper participants to results
    https://gitlab.com/tte-lighthouse/psychometrics/issues/385

--- app/serializers/threesixty/campaign_details_serializer.rb
+++ app/serializers/threesixty/campaign_details_serializer.rb
@@ -5,12 +5,19 @@ module Threesixty
     attributes :invited_evaluators, :received_evaluations, :total_evaluators_for_assessment, :options
 
     def invited_evaluators
-      Threesixty::Participant.active.where(campaign_id: object.campaign_id, subject_id: user_id).where.not(evaluator_id: user_id).count
+      Threesixty::Participant.
+        active.
+        where(campaign_id: object.campaign_id, subject_id: user_id).
+        actual_by_options(object.option).
+        count
     end
 
     def received_evaluations
       if object.option.participants.dig('manager', 'can_approves_evaluations')
-        Threesixty::Participant.where(subject_id: user_id, manager_nomination_status: :approved, campaign_id: object.campaign_id).count
+        Threesixty::Participant.
+          where(subject_id: user_id, manager_nomination_status: :approved, campaign_id: object.campaign_id).
+          actual_by_options(object.option).
+          count
       else
         UsersResult.where(subject_id: user_id, status: :completed, assessment_id: object.assessment_id).count
       end
