7292f2b5b2c1be9fc60a0f8a060c90cf6536fd91
atanych <atanych@gmail.com>
2019-08-29 23:41:08 +0300

    Add default recoding values
    https://gitlab.com/tte-lighthouse/psychometrics/issues/387

--- app/core/users_results/expand_answers_by_recoding.rb
+++ app/core/users_results/expand_answers_by_recoding.rb
@@ -13,7 +13,7 @@ module UsersResults
     def call
       result =
         users_result.answers.transform_values do |answer|
-          question_recoding = question_recoding_map[answer['question_id']]
+          question_recoding = question_recoding_map[answer['question_id']] || lookup_default_recoding(answer['question_id'])
           if question_recoding
             {
               'answers' => lookup_type_class(question_recoding)&.call!(answer['answers'], question_recoding) || answer['answers'],
@@ -29,6 +29,25 @@ module UsersResults
     def lookup_type_class(question_recoding)
       class_name = "UsersResults::ExpandAnswersByRecodingTypes::#{question_recoding.question.type}"
       class_name.safe_constantize
+    end
+
+    def lookup_default_values_class(question)
+      return nil unless question
+
+      class_name = "UsersResults::DefaultRecodingValues::#{question.type}"
+      class_name.safe_constantize
+    end
+
+    def lookup_default_recoding(question_id)
+      question = question_map[question_id]
+      props = lookup_default_values_class(question)&.call!(question)
+      return nil unless props
+
+      QuestionRecoding.new(question_id: question_id, props: props)
+    end
+
+    def question_map
+      @question_map ||= Question.where(assessment_id: users_result.assessment_id).index_by(&:id)
     end
 
     private
