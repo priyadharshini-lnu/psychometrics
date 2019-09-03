0d6df2689ab1ba1f541d012e875288123c4fd878
atanych <atanych@gmail.com>
2019-09-03 16:20:57 +0300

    Migrate single filter to multi filters (Custom Pie)

---
+++ db/migrate/20190903131845_migrate_custom_pie_data.rb
@@ -0,0 +1,11 @@
+class MigrateCustomPieData < ActiveRecord::Migration[5.1]
+  def change
+    Reports::Module.where(type: 'Graph').find_each do |m|
+      if m.props['type'] == 'CustomPie'
+        new_filters = m.props['filter'] ? Array.wrap(m.props['filter']) : []
+        m.props = m.props.merge(filter: new_filters)
+        m.save(validate: false)
+      end
+    end
+  end
+end
