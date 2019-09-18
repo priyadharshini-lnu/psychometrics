0028678fbed4dd571ddcbbb1e3643066ebf77e75
Shuja Shabandri <shuja@shabandri.com>
2019-09-18 18:12:30 +0400

    adds terms and conditions and some evaluator list style updates

--- app/frontend/user/core/ThreesixtyCampaign/components/Footer/Footer.js
+++ app/frontend/user/core/ThreesixtyCampaign/components/Footer/Footer.js
@@ -12,6 +12,7 @@ export default function Footer ({ isFrame }) {
       <div className="fluid-container">
         <div className="footer-wrapper">
           <img src={lighthouseLogo} alt="Lighthouse" />
+          <a href="https://thetalententerprise.com/privacy-statement/" target="_blank"> Terms, Conditions and Privacy Statement</a>
           <img src={tteLogo} alt="The Talent Enterprise" />
         </div>
       </div>
