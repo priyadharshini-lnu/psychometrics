8fb72cf055f574ceca83fe2958d8cf3687c07a5d
Rohan Pujari <rohan@thetalententerprise.com>
2019-09-29 11:12:24 +0400

    Fix evaluator invite and evaluator reminder

--- app/core/threesixty/emails/send_single_scheduled_email.rb
+++ app/core/threesixty/emails/send_single_scheduled_email.rb
@@ -22,16 +22,15 @@ module Threesixty
       private
 
       def send_email(recipient)
-        preprocess_email_schedule(recipient)
         recipient_type = get_recipient_type
         context = { recipient: recipient, threesixty_campaign: threesixty_campaign }
         context[recipient_type] = recipient if recipient_type
 
-        other_participator = recipient_type == :subject ? :evaluator : :subject
+        other_participator_type = recipient_type == :subject ? :evaluator : :subject
 
-        if (user_ids = schedule_email.meta["#{other_participator}_ids"].presence)
+        if (user_ids = get_other_participators(recipient, other_participator_type))
           User.where(id: user_ids).each do |user|
-            context[other_participator] = user
+            context[other_participator_type] = user
             Threesixty::ScheduleEmailMailer.send_email(schedule_email, context).deliver_later
           end
         else
@@ -55,10 +54,12 @@ module Threesixty
         reminder_history.save!
       end
 
-      def preprocess_email_schedule(user)
+      def get_other_participators(user, other_participator_type)
+        other_participator_ids = schedule_email.meta["#{other_participator_type}_ids"].presence
+        return other_participator_ids if other_participator_ids
+
         if Threesixty::Emails::Name.evaluator_email?(schedule_email.name) && schedule_email.meta['subject_ids'].blank?
-          schedule_email.update(meta:
-            { subject_ids: Threesixty::Evaluators::GetSubjectIdsWithoutEvaluation.call!(threesixty_campaign, user) })
+          Threesixty::Evaluators::GetSubjectIdsWithoutEvaluation.call!(threesixty_campaign, user)
         end
       end
     end
