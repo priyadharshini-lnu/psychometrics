44e1b30ec75287896fc4284364dd4aca29220473
Syed Aslam <aslam@thetalententerprise.com>
2019-10-01 12:07:59 +0400

    frozen literal magic comment

--- db/migrate/20191001075231_rename_second_factor_enabled_in_clients.rb
+++ db/migrate/20191001075231_rename_second_factor_enabled_in_clients.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameSecondFactorEnabledInClients < ActiveRecord::Migration[5.1]
   def change
     rename_column :clients, :second_factor_enabled, :two_factor_enabled
