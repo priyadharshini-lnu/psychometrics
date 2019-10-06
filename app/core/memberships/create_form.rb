35f3acd778fd8ccaaeec6abeb70ebf5aeb11f8e9
Rohan Pujari <rohan@thetalententerprise.com>
2019-10-06 18:05:27 +0400

    Use same email regex in all the places

--- app/core/memberships/create_form.rb
+++ app/core/memberships/create_form.rb
@@ -7,6 +7,6 @@ module Memberships
     attribute :parent_id, Integer
 
     validates :email, presence: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, format: { with: Devise.email_regexp }
   end
 end
--- app/forms/administration/users/fetch_user_form.rb
+++ app/forms/administration/users/fetch_user_form.rb
@@ -6,7 +6,7 @@ module Administration
       attribute :email, String
 
       validates :email, presence: true
-      validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+      validates :email, format: { with: Devise.email_regexp }
     end
   end
 end
