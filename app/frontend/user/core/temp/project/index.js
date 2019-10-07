c54df9a7742dfa602609f729ab91c2b8ce2a27f3
Rohan Pujari <rohan@thetalententerprise.com>
2019-10-07 18:37:23 +0400

    Fix lint issues

--- app/frontend/user/core/temp/project/index.js
+++ app/frontend/user/core/temp/project/index.js
@@ -4,9 +4,9 @@ export const defaultState = {
   logo: null,
 }
 
-export const get = (state) => _.get(state, ['threeSixtyCampaign', 'temp', 'project'])
-export const getPrivacyText = (state) => _.get(get(state), ['privacyText'])
-export const privacyPageLink = (state) => _.get(get(state), ['privacyPageLink'])
+export const get = state => _.get(state, ['threeSixtyCampaign', 'temp', 'project'])
+export const getPrivacyText = state => _.get(get(state), ['privacyText'])
+export const privacyPageLink = state => _.get(get(state), ['privacyPageLink'])
 
 export default function reducer (state = defaultState, action) {
   switch (action.type) {
