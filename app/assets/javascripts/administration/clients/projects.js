9ecbddd2fa472b5a9457344d93b315a4f8524fcd
Rohan Pujari <rohan@thetalententerprise.com>
2019-10-09 17:05:51 +0400

    Convert arrow function to normal function

--- app/assets/javascripts/administration/clients/projects.js
+++ app/assets/javascripts/administration/clients/projects.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(document).on('change', '#privacy_link_checkbox', () => {
+  $(document).on('change', '#privacy_link_checkbox', function () {
     $('#privacy_link_details').toggleClass('hidden')
   })
 });
