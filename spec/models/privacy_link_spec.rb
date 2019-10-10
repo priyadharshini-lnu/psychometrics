8c579f5ffd4ce1bdb01656ccacb3b258720b3e3a
Rohan Pujari <rohan@thetalententerprise.com>
2019-10-10 12:19:48 +0000

    Enforce http/https and fix style

--- spec/models/privacy_link_spec.rb
+++ spec/models/privacy_link_spec.rb
@@ -29,19 +29,15 @@ RSpec.describe PrivacyLink, type: :model do
       privacy_link = build(:privacy_link, link: 'invalid')
       expect(privacy_link.valid?).to eq(false)
 
-      expect(privacy_link.errors[:link]).to include('invalid url')
+      privacy_link = build(:privacy_link, link: 'https://cc<>.com')
+      expect(privacy_link.valid?).to eq(false)
     end
 
-    it 'allows url without subdomain' do
+    it 'disallows url without protocol' do
       privacy_link = build(:privacy_link, link: 'cc.com')
+      expect(privacy_link.valid?).to eq(false)
 
-      expect(privacy_link.valid?).to eq(true)
-    end
-
-    it 'allows url with subdomain' do
-      privacy_link = build(:privacy_link, link: 'subdomain.cc.com')
-
-      expect(privacy_link.valid?).to eq(true)
+      expect(privacy_link.errors[:link]).to include('invalid url')
     end
 
     it 'allows url with http/https in url' do
