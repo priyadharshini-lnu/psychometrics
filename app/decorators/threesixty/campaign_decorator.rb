226bc0a00782b35a1c8ce13dcaeb9567e7c53014
Syed Aslam <aslam@thetalententerprise.com>
2019-10-15 11:25:42 +0400

    LH-224, fix confirmation text not being visible

--- app/decorators/threesixty/campaign_decorator.rb
+++ app/decorators/threesixty/campaign_decorator.rb
@@ -5,5 +5,20 @@ module Threesixty
     def name
       object.campaign.name
     end
+
+    # FIXME
+    def depth
+      1
+    end
+
+    def delete_confirmation
+      {
+        title: I18n.t(
+          'administration.clients.projects.threesixty_campaigns.resource.confirmations.delete.title',
+          name: display_name
+        ),
+        body: I18n.t('administration.clients.projects.threesixty_campaigns.resource.confirmations.delete.body')[depth]
+      }.to_json
+    end
   end
 end
