01ffff189fc2a4b733851f4663274aeb20136693
Rohan Pujari <rohan@thetalententerprise.com>
2019-11-17 19:22:04 +0000

    Resolve LH-261 "Hotfix/ download raw data for 360 assessments"

--- app/controllers/administration/clients/assessments_controller.rb
+++ app/controllers/administration/clients/assessments_controller.rb
@@ -26,10 +26,10 @@ module Administration
 
       def export_results
         @assessment = Assessment.find(params[:assessment_id])
-        results = ::Exports::Assessments::AssessmentResultsExport.new(@assessment, client.id, export_results_params)
+        results = ::Exports::Assessments::AssessmentResultsExport.call!(@assessment, client.id, export_results_params)
         filename = params[:scoring] ? 'assessment_scoring_results.xlsx' : 'assessment_raw_results.xlsx'
         respond_to do |format|
-          format.xlsx { send_data results.to_xlsx.to_stream.read, filename: filename }
+          format.xlsx { send_data results.to_stream.read, filename: filename }
         end
       end
 
