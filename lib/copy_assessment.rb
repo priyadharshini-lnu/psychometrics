74a8851f9a40336c22e64dca7812727b74cedd27
Syed Aslam <aslam@thetalententerprise.com>
2019-11-19 15:17:12 +0530

    copy assessments

--- lib/copy_assessment.rb
+++ lib/copy_assessment.rb
@@ -4,7 +4,6 @@
 # alone with blocks, questions, question_recodings, factor_scorings
 # and translations
 class CopyAssessment
-  # rubocop:disable Metrics/AbcSize
   def self.process!(assessment_id)
     assessment = Assessment.includes(blocks: {
       questions: %i[factors_scorings question_recodings translations]
@@ -14,44 +13,35 @@ class CopyAssessment
     flow = (assessment.flow || {}).to_json
     norm_rules = (assessment.norm_rules || {}).to_json
 
-    # rubocop:disable Metrics/BlockLength
     new_assessment = Assessment.transaction do
       new_assessment = assessment.clone
-      new_assessment.save
+      new_assessment.save!
+
       # Loop blocks to save for the new assessment
       assessment.blocks.each do |block|
-        new_block = block.clone(false)
-        new_assessment.blocks << new_block
+        new_block = make_copy(block, new_assessment)
+        new_block.save!
 
         # Replace original Block Id to New Block Id
         flow.gsub!(/\"current\":\"#{block.id}\"/, "\"current\":\"#{new_block.id}\"")
 
         # Loop questions to save for the new block
         block.questions.each do |question|
-          new_question = question.clone(false)
-          new_question.assessment_id = new_assessment.id
-          new_block.questions << new_question
+          new_question = make_copy(question, new_assessment)
+          new_question.block_id = new_block.id
+          new_question.save!
 
           # Replace original Question Id to New Question Id
           flow.gsub!(/\"subject\":#{question.id}/, "\"subject\":#{new_question.id}")
           norm_rules.gsub!(/\"subject\":#{question.id}/, "\"subject\":#{new_question.id}")
 
-          question.factors_scorings.each do |factors_scoring|
-            new_factors_scoring = factors_scoring.clone(false)
-            new_factors_scoring.assessment_id = new_assessment.id
-            new_question.factors_scorings << new_factors_scoring
-          end
-
-          question.question_recodings.each do |recoding|
-            new_recoding = recoding.clone(false)
-            new_recoding.assessment_id = new_assessment_id
-            new_question.question_recodings << new_recoding
+          %w[factors_scorings question_recodings].map do |name|
+            copy_association(name, question, new_question, new_assessment)
           end
 
           question.translations.each do |translation|
-            new_translation = translation.clone(false)
+            new_translation = make_copy(translation, new_assessment, 'resource_id')
             new_translation.translateable_id = new_question.id
-            new_translation.resource_id = new_assessment.id
             new_question.translations << new_translation
           end
         end
@@ -59,8 +49,21 @@ class CopyAssessment
       new_assessment.update_attributes(flow: JSON.parse(flow), norm_rules: JSON.parse(norm_rules, quirks_mode: true))
       new_assessment
     end
-    # rubocop:enable Metrics/BlockLength
     new_assessment
   end
-  # rubocop:enable Metrics/AbcSize
+
+  def self.make_copy(object, resource, resource_key = 'assessment_id')
+    copy = object.clone(false)
+    copy[resource_key] = resource.id
+    copy
+  end
+
+  def self.copy_association(name, of_question, into_question, assessment)
+    of_question[name].to_a.each do |item|
+      new_item = make_copy(item, assessment)
+      new_item.question_id = into_question.id
+
+      new_item.save!
+    end
+  end
 end
