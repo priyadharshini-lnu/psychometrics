511f85b4658d0d096bf2dd125da3ef33b368e997
Rohan Pujari <rohan@thetalententerprise.com>
2019-11-24 08:12:13 +0000

    Resolve LH-186 "Parent MR for all email consolidation work"

---
+++ app/core/threesixty/piped_text/branches/subject_table_branch.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Threesixty
+  module PipedText
+    module Branches
+      class SubjectTableBranch < BaseBranch
+        def call
+          class_name = "Threesixty::PipedText::Branches::SubjectTable::#{path.first}".safe_constantize
+
+          broadcast :ok, class_name&.call!(path, params, context)
+        end
+      end
+    end
+  end
+end
