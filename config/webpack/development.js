bac51f6cbcde490d375cf91530d0529af9444f2e
Shuja Shabandri <shuja@shabandri.com>
2020-03-01 16:54:13 +0400

    makes https optional for development

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,13 +5,27 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-environment.config.merge({
-  devServer: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../../support/dev-ssl/dev.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../../support/dev-ssl/dev-cert.pem')),
+function getCerts (host) {
+  const keyPath = path.resolve(__dirname, `../../support/dev-ssl/${host}.key`)
+  const certPath = path.resolve(__dirname, `../../support/dev-ssl/${host}.pem`)
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    return {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    }
+  }
+}
+
+const { config } = environment
+const sslCert = getCerts(config.devServer.host)
+if (config.devServer.https === true && sslCert) {
+  config.merge({
+    devServer: {
+      https: {
+        ...sslCert,
+      },
     },
-  },
-})
+  })
+}
 
 module.exports = environment.toWebpackConfig()
