ddda1f20f6ad4c3ba5d4d8cf54e1315ef63ac0d6
Davidson mohanty <davidson@thetalententerprise.com>
2020-03-09 17:11:50 +0530

    Add the missing translations
     * by-product of work on LH-531

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -109,8 +109,8 @@ class ApplicationController < ::BaseController
 
   def user_not_authorized
     respond_to do |format|
-      format.html { render plain: 'You does not have access to this page', status: 403 }
-      format.json { render json: { error: ['You do not have permissions to this action'] }, status: 403 }
+      format.html { render plain: I18n.t('errors.forbidden'), status: 403 }
+      format.json { render json: { error: [I18n.t('errors.forbidden_action')] }, status: 403 }
     end
   end
 
