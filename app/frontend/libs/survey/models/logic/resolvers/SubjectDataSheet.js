d4340b93c5b9b4bac978516d8d0c9115c376f326
Rohan Pujari <rohan@thetalententerprise.com>
2020-03-17 14:11:11 +0530

    Lint

--- app/frontend/libs/survey/models/logic/resolvers/SubjectDataSheet.js
+++ app/frontend/libs/survey/models/logic/resolvers/SubjectDataSheet.js
@@ -1,8 +1,8 @@
-import DataSheet from './DataSheet'
 import store from 'store/AssessmentPreviewStore'
+import DataSheet from './DataSheet'
 
 export default class SubjectDataSheetResolver extends DataSheet {
-  constructor(condition) {
+  constructor (condition) {
     super(condition)
     this.datasheet = store.subjectDataSheet
   }
