374afb2562b9b3e7a4b2746e92a2e06169b9a7e5
Davidson mohanty <davidson@thetalententerprise.com>
2020-03-22 15:05:13 +0530

    LH-538 : Mindmill and Hogan results action refactor

--- app/controllers/mindmill/assigns_controller.rb
+++ app/controllers/mindmill/assigns_controller.rb
@@ -15,7 +15,6 @@ class Mindmill::AssignsController < ApplicationController
     @ssourl = "#{mindmill.ssourl}&URL=#{request.base_url + results_mindmill_assign_path(@assign)}"
 
     @assign.in_progress!
-    BuildMindmillResultsJob.set(wait: 1.hour).perform_later(@assign, @current_membership, user_locale)
     # Set Not Started for all Mindmill assigns, except current
     # Cause only one Mindmill can has In Progress status
     Assign.
@@ -30,19 +29,20 @@ class Mindmill::AssignsController < ApplicationController
   def results
     redirect_to(root_path, success: t('.successfully')) && return if @assign.completed?
     mindmill = Api::Mindmill.new(@assign, @current_membership, user_locale)
-    mindmill.load_scores
-    mindmill.load_results
-    redirect_to(root_path, error: t('.not_completed')) && return unless mindmill.report && mindmill.scores
-    report = "data:application/pdf;base64,#{mindmill.report}"
-    normalised_scores = Imports::External::BaseExternalImport.build(:mindmill).process(mindmill.scores, @assign)
-    @assign.update(
-      mindmill_report: report,
-      external_results: normalised_scores,
-      status: :completed,
-      completed_at: Time.current
-    )
-    Assigns::GenerateReport.call(@assign, current_user)
-    redirect_to(root_path, success: t('.successfully'))
+    mindmill.assign_user
+    redirect_back(fallback_location: root_path, error: t('errors.error_500')) && return unless mindmill.ssourl
+    @ssourl = "#{mindmill.ssourl}&URL=#{request.base_url + results_mindmill_assign_path(@assign)}"
+
+    @assign.in_progress!
+    BuildMindmillResultsJob.set(wait: 1.hour).perform_later(@assign, @current_membership, user_locale)
+    Assign.
+      in_progress.
+      mindmill.
+      where(membership_id: @current_membership.id).
+      where.not(id: @assign.id).
+      update_all(status: :not_started)
+
+    redirect_to @ssourl
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -615,7 +615,6 @@ Rails.application.routes.draw do
       resources :assigns, only: [] do
         member do
           get :redirect
-          get :results
           put :pass
         end
       end
