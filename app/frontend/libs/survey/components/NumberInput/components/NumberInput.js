65ea6c59507e07d3cc7bb7173a879eae11da1269
Syed Aslam <aslam@thetalententerprise.com>
2020-04-01 22:54:59 +0530

    floating point arithmetic

--- app/frontend/libs/survey/components/NumberInput/components/NumberInput.js
+++ app/frontend/libs/survey/components/NumberInput/components/NumberInput.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import cs from 'classnames'
 import styles from './NumberInput.scss'
 
 const MIN_VALUE = 0.0
@@ -68,17 +67,17 @@ class NumberInput extends Component {
 
   render () {
     const { value, label } = this.props
-    const val = this.isNumber(value) ? value : this.getMinValue()
+    const val = this.isNumber(value) ? parseFloat(value).toPrecision(1) : this.getMinValue()
 
     return (
       <div className={styles.inputContainer}>
         <div className={styles.label}>{label}</div>
         <div className={styles.numberInput}>
-          <a className={cs(styles.control, { disabled: value === MIN_VALUE })} onClick={this.decrement}>
+          <a className={styles.control} onClick={this.decrement}>
             <span className={`fa fa-minus ${styles.icon}`} />
           </a>
           <input onChange={this.change} value={val} className={`from-control ${styles.input}`} />
-          <a className={cs(styles.control, { disabled: value === MAX_VALUE })} onClick={this.increment}>
+          <a className={styles.control} onClick={this.increment}>
             <span className={`fa fa-plus ${styles.icon}`} />
           </a>
         </div>
