f66eed4b1f3be5d6c46ec8f6b17068869f7604f4
Rohan Pujari <rohanpujaris@gmail.com>
2020-04-13 17:31:42 +0530

    Fix display logic on report (#104)
    Fix unfinite loop on clicking next button on assessment
    
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>

--- app/frontend/libs/reports/models/Results/SetDataSheet.ts
+++ app/frontend/libs/reports/models/Results/SetDataSheet.ts
@@ -2,5 +2,8 @@ import _ from 'lodash'
 import RawResult from './interfaces/RawResult'
 
 export default {
-  run: (rawResults: RawResult[]): object => (_.last(rawResults) || {}).data_sheet,
+  run: (rawResults: RawResult[]): object => {
+    const result = (_.last(rawResults) || {})
+    return result.subject_datasheet || result.data_sheet
+  }
 }
