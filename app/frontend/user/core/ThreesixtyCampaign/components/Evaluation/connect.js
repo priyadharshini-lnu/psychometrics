616ea1d16cbe39d682271b19633c7fc4f16a604b
Shuja Shabandri <shuja@shabandri.com>
2020-04-27 22:18:00 +0400

    style fixes

--- app/frontend/user/core/ThreesixtyCampaign/components/Evaluation/connect.js
+++ app/frontend/user/core/ThreesixtyCampaign/components/Evaluation/connect.js
@@ -4,10 +4,12 @@ import {
   updateStatus,
 } from 'user/core/ThreesixtyCampaign/evaluation'
 import { saveResults } from 'core/preview/FlowProcessor/actions'
+import { getProgress } from 'core/preview/FlowProcessor/selectors'
 
 const mapStateToProps = state => ({
   evaluation: state.threeSixtyCampaign.evaluation,
   preview: state.preview,
+  progress: state.preview.initialized && getProgress(state.preview),
 })
 
 const mapDispatchToProps = {
