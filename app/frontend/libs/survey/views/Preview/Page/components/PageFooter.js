95c60e73219b153b70623ca9e6cf3dfdc25d746b
Fedor Tarasov <tarassovf@gmail.com>
2020-04-29 16:01:49 +0300

    use scrollintoview instead anchor

--- app/frontend/libs/survey/views/Preview/Page/components/PageFooter.js
+++ app/frontend/libs/survey/views/Preview/Page/components/PageFooter.js
@@ -11,11 +11,13 @@ class PageFooter extends Component {
 
   prev = () => {
     const { prevPage, preview } = this.props
+    document.body.scrollIntoView({ behavior: 'smooth' })
     prevPage(preview)
   }
 
   next = () => {
     const { nextPage } = this.props
+    document.body.scrollIntoView({ behavior: 'smooth' })
     nextPage()
   }
 
@@ -24,19 +26,15 @@ class PageFooter extends Component {
     return (
       <div className={cs(styles.footer)}>
         {enableBack && hasPrevPage && (
-          <a href="#">
-            <button type="button" className={cs('btn-default', styles.btn, styles.btnDefault)} onClick={this.prev}>
-              <span className="mrs mls fa fa-chevron-left rtl-flip" />
-              { page.prevBtn || Watchman.I18n().t('assessments.page.back') }
-            </button>
-          </a>
+        <button type="button" className={cs('btn-default', styles.btn, styles.btnDefault)} onClick={this.prev}>
+          <span className="mrs mls fa fa-chevron-left rtl-flip" />
+          { page.prevBtn || Watchman.I18n().t('assessments.page.back') }
+        </button>
         )}
-        <a href="#">
-          <button type="button" className={cs('btn-default', styles.btn, styles.btnPrimary)} onClick={this.next}>
-            { page.nextBtn || Watchman.I18n().t('assessments.page.next') }
-            <span className="mls mrs fa fa-chevron-right rtl-flip" />
-          </button>
-        </a>
+        <button type="button" className={cs('btn-default', styles.btn, styles.btnPrimary)} onClick={this.next}>
+          { page.nextBtn || Watchman.I18n().t('assessments.page.next') }
+          <span className="mls mrs fa fa-chevron-right rtl-flip" />
+        </button>
       </div>
     )
   }
