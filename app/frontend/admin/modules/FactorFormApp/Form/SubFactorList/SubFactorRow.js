32955fbd00fb9767af62a240b4879dbeb991ee83
atanych <atanych@gmail.com>
2020-05-19 09:24:40 +0300

    Fix factors_subfactors (#237)
    https://tte.atlassian.net/browse/LH-734

--- app/frontend/admin/modules/FactorFormApp/Form/SubFactorList/SubFactorRow.js
+++ app/frontend/admin/modules/FactorFormApp/Form/SubFactorList/SubFactorRow.js
@@ -3,23 +3,34 @@ import { DnDElement } from 'components/DnD'
 import styles from './styles.scss'
 
 const SubFactorRow = ({
-  list, scoringStrategy, moveRow, subFactor, columns, ...restProps
-}) => (scoringStrategy === 'sub_factors_conditional_average'
-  ? (
-    <DnDElement
-      element={subFactor}
-      onDrag={moveRow}
-      list={list}
-      uniqField="sub_factor_id"
-      strategy="position"
-      wrapper="tr"
-      iconWrapper="td"
-      iconClass={styles.td}
+  list, scoringStrategy, moveRow, subFactor, columns,
+}) => {
+  const renderTdList = () => columns.filter(c => c.render).map(c => (
+    <td
+      className={styles.td}
+      key={c.dataIndex}
     >
-      {columns.filter(c => c.render).map(c => <td className={styles.td} key={c.dataIndex}>{c.render(subFactor)}</td>)}
-    </DnDElement>
-  )
-  : <tr {...restProps} />)
+      {c.render(subFactor)}
+    </td>
+  ))
+
+  return (scoringStrategy === 'sub_factors_conditional_average'
+    ? (
+      <DnDElement
+        element={subFactor}
+        onDrag={moveRow}
+        list={list}
+        uniqField="sub_factor_id"
+        strategy="position"
+        wrapper="tr"
+        iconWrapper="td"
+        iconClass={styles.td}
+      >
+        {renderTdList()}
+      </DnDElement>
+    )
+    : <tr>{renderTdList()}</tr>)
+}
 
 
 export default SubFactorRow
