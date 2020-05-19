95f4775f2dfe21840dc1ca00c7a48d23a45299f8
Shuja Shabandri <shuja@shabandri.com>
2020-05-19 13:12:58 +0400

    typo fix

--- app/frontend/components/hocs/withCopyProtection.tsx
+++ app/frontend/components/hocs/withCopyProtection.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 function withCopyProtection (WrappedComponent) {
   return class extends React.Component {
-
     componentDidMount () {
-      const disableEvnet = (e: Event) => {
+      const disableEvent = (e: Event) => {
         e.preventDefault()
         return false
       }
       ['cut', 'copy', 'contextmenu'].forEach((ev) => {
-        (this.ref as HTMLElement).addEventListener(ev, disableEvnet)
+        (this.ref as HTMLElement).addEventListener(ev, disableEvent)
       })
     }
 
