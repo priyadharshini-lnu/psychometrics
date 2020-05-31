9f0fbcb8c950263c0384004b6f29f844fb85a7f5
Shuja Shabandri <shuja@shabandri.com>
2020-05-31 16:20:02 +0400

    adds audioplayer to questions

--- app/frontend/libs/survey/hooks/useAudioPlayer/useAudioPlayer.ts
+++ app/frontend/libs/survey/hooks/useAudioPlayer/useAudioPlayer.ts
@@ -1,33 +1,31 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 
 import PlayerSkins from './skins'
-import { AudioPlayerSkin } from './interfaces'
 
-export function useAudioPlayer (ref: React.MutableRefObject<HTMLElement>): void {
-  const skins = useRef<AudioPlayerSkin[]>([])
-  useEffect(() => {
-    console.log(ref.current)
-    const tags = ref.current.querySelectorAll('.fr-audio:not(.fr-audio-initialized)') as NodeListOf<HTMLElement>
-    tags.forEach((tag) => {
-      const audio = tag.querySelector('audio')
-      if (!audio) return
-      tag.removeAttribute('contenteditable')
-      tag.removeAttribute('draggable')
-      const { autoplay, skin } = tag.dataset
-      tag.classList.add(`audio-skin-${skin}`)
-      tag.classList.add('fr-audio-initialized')
-      audio.removeAttribute('controls')
-      if (autoplay) {
-        audio.setAttribute('autoplay', 'autoplay')
-      }
-      if (skin && PlayerSkins[skin]) {
-        PlayerSkins[skin].render(tag)
-        skins.current.push(PlayerSkins[skin])
-      }
-    })
-    return () => {
-      skins.current.forEach(s => s.cleanup && s.cleanup())
-      skins.current = []
+export function initAudioPlayer (el: HTMLElement): void {
+  const tags = el.querySelectorAll('.fr-audio:not(.fr-audio-initialized)') as NodeListOf<HTMLElement>
+
+  tags.forEach((tag) => {
+    const audio = tag.querySelector('audio')
+    if (!audio) return
+    tag.removeAttribute('contenteditable')
+    tag.removeAttribute('draggable')
+    const { autoplay, skin } = tag.dataset
+    tag.classList.add(`audio-skin-${skin}`)
+    tag.classList.add('fr-audio-initialized')
+    audio.removeAttribute('controls')
+    if (autoplay) {
+      audio.setAttribute('autoplay', 'autoplay')
     }
+    if (skin && PlayerSkins[skin]) {
+      PlayerSkins[skin].render(tag)
+    }
+  })
+}
+
+export function useAudioPlayer (ref: React.MutableRefObject<null | HTMLElement>): void {
+  useEffect(() => {
+    if (!ref.current) return
+    initAudioPlayer(ref.current)
   }, [ref.current])
 }
