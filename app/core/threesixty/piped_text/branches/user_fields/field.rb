344ab44945dda98dc9bf5ed3cfa58e17482ee663
Syed Aslam <aslam@thetalententerprise.com>
2020-06-07 16:44:08 +0530

    add fields

--- app/core/threesixty/piped_text/branches/user_fields/field.rb
+++ app/core/threesixty/piped_text/branches/user_fields/field.rb
@@ -15,6 +15,28 @@ module Threesixty
           protected
 
           def possible_fields
+            {
+              'Name' => :full_name,
+              'Email' => :email,
+              'FirstName' => :first_name,
+              'LastName' => :last_name
+            }
+          end
+
+          def full_name
+            user.decorate.full_name
+          end
+
+          def email
+            user.email
+          end
+
+          def first_name
+            user.first_name
+          end
+
+          def last_name
+            user.last_name
           end
         end
       end
