a22874681149d926abf7f6af7dbbaa8cf9d6e487
Rohan Pujari <rohanpujaris@gmail.com>
2020-06-08 20:02:02 +0530

    Fix 360 report preview (#296)
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>

--- app/frontend/libs/reports/containers/ReportContainer.js
+++ app/frontend/libs/reports/containers/ReportContainer.js
@@ -1,11 +1,15 @@
 /* eslint-disable react/no-find-dom-node */
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 import _ from 'lodash'
 import Preview from 'rb/views/Preview'
 import I18nStore from 'rb/store/I18nStore'
 import store from 'rb/store/PreviewStore'
 import 'rb/styles/core.scss'
-
+import { normalize } from 'normalizr'
+import rstore from '../store'
+import { init } from '../core/builder/actions'
+import schema from '../store/schema'
 
 class ReportContainer extends Component {
   state = {
@@ -20,15 +24,20 @@ class ReportContainer extends Component {
       I18nStore.setLocale(_.get(selectedLocale, 'code', document.body.dataset.locale))
       I18nStore.locales = locales
     }
+
+    const normalizedData = normalize(data, schema)
     store.init(data, results, user, campaign)
+    rstore.dispatch(init(normalizedData))
     this.setState({ selectedLocale })
   }
 
   render () {
     return (
-      <div className="row">
-        <Preview localeDirection={_.get(this.state, 'selectedLocale.direction', 'ltr')} />
-      </div>
+      <Provider store={rstore}>
+        <div className="row">
+          <Preview localeDirection={_.get(this.state, 'selectedLocale.direction', 'ltr')} />
+        </div>
+      </Provider>
     )
   }
 }
