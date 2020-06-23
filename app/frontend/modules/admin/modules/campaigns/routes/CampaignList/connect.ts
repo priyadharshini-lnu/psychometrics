53c170b3263a942399d06c859142420e8650a35b
Fedor Tarasov <tarassovf@gmail.com>
2020-06-23 16:19:50 +0300

    move campaigns to modules (#331)

--- app/frontend/modules/admin/modules/campaigns/routes/CampaignList/connect.ts
+++ app/frontend/modules/admin/modules/campaigns/routes/CampaignList/connect.ts
@@ -2,9 +2,9 @@ import { connect } from 'react-redux'
 import {
   fetch,
   get as getCampaign,
-} from 'modules/admin/campaigns/core/list'
+} from 'modules/admin/modules/campaigns/core/list'
 import { openModal } from 'modules/admin/core/temp/modals'
-import { get as getTotal } from 'modules/admin/campaigns/core/total'
+import { get as getTotal } from 'modules/admin/modules/campaigns/core/total'
 
 export default connect(
   state => ({
--- app/frontend/packs/admin/campaign.js
+++ app/frontend/packs/admin/campaign.js
@@ -1,10 +1,10 @@
-import CampaignList from 'modules/admin/campaigns/App'
+import CampaignsApp from 'modules/admin/modules/campaigns/App'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import 'modules/admin/styles/ant.less'
 import 'modules/admin/assets/scss/common.scss'
 
-ReactDOM.render(<CampaignList />, document.getElementById('campaigns-container'))
+ReactDOM.render(<CampaignsApp />, document.getElementById('campaigns-container'))
 
 if (__DEV__) {
   if (module.hot) {
