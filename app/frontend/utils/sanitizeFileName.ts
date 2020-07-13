0dcdd186cfbf3b5b94bd68ac1e1a8ca5da1a0280
namrataukirde <namrata.ukirde@thetalententerprise.com>
2020-07-14 01:14:46 +0530

    trim file name and replacement

--- app/frontend/utils/sanitizeFileName.ts
+++ app/frontend/utils/sanitizeFileName.ts
@@ -4,21 +4,18 @@ import _ from 'lodash'
 const ILLEGAL_CHARS_REGEX = /[(#<$+%>!`&*'|{?"=}/:\\\s@)]+/g
 const CONTROL_CHARS_RANGE = /[\x00-\x1f\x80-\x9f]+/g
 const BLANK_FILE_NAME = /^[\s_-]*$/
+const DEFAULT_FILE_NAME = 'file'
 
 const sanitize = (fileName: string, replacement = '_'): string => {
-  replacement = replacement.replace(ILLEGAL_CHARS_REGEX, '_')
+  replacement = _.trim(replacement).replace(ILLEGAL_CHARS_REGEX, '_')
 
-  fileName = _.trim(fileName)
-
-  let sanitizedFileName = fileName.replace(ILLEGAL_CHARS_REGEX, replacement)
+  let sanitizedFileName = _.trim(fileName).replace(ILLEGAL_CHARS_REGEX, replacement)
     .replace(CONTROL_CHARS_RANGE, replacement)
 
   const index = sanitizedFileName.lastIndexOf('.')
-  let fileNameWithoutExtension = sanitizedFileName.substr(0, index)
 
-  if (BLANK_FILE_NAME.test(fileNameWithoutExtension)) {
-    fileNameWithoutExtension = 'file'
-    sanitizedFileName = fileNameWithoutExtension + sanitizedFileName.substr(index)
+  if (BLANK_FILE_NAME.test(sanitizedFileName.substr(0, index))) {
+    sanitizedFileName = DEFAULT_FILE_NAME + sanitizedFileName.substr(index)
   }
   return sanitizedFileName
 }
