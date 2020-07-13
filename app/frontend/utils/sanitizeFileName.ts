90cde503bf3d394564f4eb9cd7e6d46b40ac62a6
namrataukirde <namrata.ukirde@thetalententerprise.com>
2020-07-13 20:04:15 +0530

    remove windows file name range

--- app/frontend/utils/sanitizeFileName.ts
+++ app/frontend/utils/sanitizeFileName.ts
@@ -1,17 +1,9 @@
 /* eslint-disable no-control-regex */
 
-const ILLEGAL_CHARS_REGEX = /[(#<$+%>!`&*'|{?"=}/:\\@)]+/g
+const ILLEGAL_CHARS_REGEX = /[(#<$+%>!`&*'|{?"=}/:\\\s@)]+/g
 const CONTROL_CHARS_RANGE = /[\x00-\x1f\x80-\x9f]+/g
-const SPACE_REGEX = /\s/g
-const WINDOWS_RESERVED_RANGE = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i
-const RESERVED_FILE_NAMES_FOR_UNIX = /^\.+$/
-const RESERVED_FILE_NAMES_FOR_WINDOWS = /[. ]+$/
 
 export const sanitize = (fileName: string): string => (
-  fileName.replace(SPACE_REGEX, '')
-    .replace(ILLEGAL_CHARS_REGEX, '_')
+  fileName.replace(ILLEGAL_CHARS_REGEX, '_')
     .replace(CONTROL_CHARS_RANGE, '_')
-    .replace(WINDOWS_RESERVED_RANGE, '_')
-    .replace(RESERVED_FILE_NAMES_FOR_UNIX, '_')
-    .replace(RESERVED_FILE_NAMES_FOR_WINDOWS, '_')
 )
