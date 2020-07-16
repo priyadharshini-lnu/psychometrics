9a4f8bf3f5640f4601c7e89a6574225fd551d6c2
Syed Aslam <aslam@thetalententerprise.com>
2020-07-16 17:25:53 +0530

    ztable values

---
+++ app/core/assigns/agile_scoring.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Assigns
+  class AgileScoring < BaseCommand
+    private_attr_accessor :assign
+
+    def initialize(asssign)
+      @assign = assign
+    end
+
+    def call
+      # TODO
+    end
+  end
+end
