d1db585a89d5af7da44614436a11ba532238db43
Syed Aslam <aslam@thetalententerprise.com>
2020-07-27 01:05:56 +0530

    spec comman CalculateAgileScoring

--- spec/core/assigns/calculate_agile_scoring_spec.rb
+++ spec/core/assigns/calculate_agile_scoring_spec.rb
@@ -2,37 +2,107 @@
 
 require 'rails_helper'
 
-describe Assigns::CalculateInnovationStyles do
-  let(:assign) { create(:assign) }
-  let(:dimension) { assign.assessment.dimension }
-  let(:factor1) { create(:factor, dimension: dimension) }
-  let(:factor2) { create(:factor, dimension: dimension) }
-  let(:factor3) { create(:factor, dimension: dimension) }
-  let(:agile) { create(:agile, assessment: assign.assessment) }
+describe Assigns::CalculateAgileScoring do
+  before(:all) do
+    @assign = FactoryBot.create(:assign)
+    @agile = FactoryBot.create(:agile, assessment: @assign.assessment)
+    @norm = FactoryBot.create(:norm, :percentile)
 
-  # let(:config) {
-  #   {}
-  # }
+    config = {
+      'normId': @norm.id,
+      'groups': [
+        {
+          'id': 'ed-1-group',
+          'scenes': [
+            {
+              'id': 'ed-1-ass',
+              'type': 'AssessmentScene',
+              'data': {
+                'blocks': [
+                  {
+                    'id': 'ed-1-ass-block-1',
+                    'scoring': [
+                      {
+                        'factorId': @norm.factors.first.id,
+                        'itemScore': 1
+                      }
+                    ],
+                    'questions': [
+                      {
+                        'id': 'edg-1',
+                        'answers': [
+                          [
+                            '2'
+                          ]
+                        ]
+                      },
+                      {
+                        'id': 'edg-2',
+                        'answers': [
+                          [
+                            '2'
+                          ]
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    }
+
+    results = [
+      {
+        'answers' => {
+          'edg-1' => {
+            'id' => 'edg-1',
+            'answers' => ['2'],
+            'group_id' => 'ed-1-group'
+          },
+          'edg-2' => {
+            'id' => 'edg-2',
+            'answers' => ['2'],
+            'group_id' => 'ed-1-group'
+          }
+        },
+        'group_id' => 'ed-1-group'
+      }
+    ]
+
+    @agile.update(config: config)
+    @assign.update(results: results, norm_data: { id: @norm.id })
+    @assign.complete!
+  end
+
+  before(:context) { described_class.call!(@assign) }
 
   context '.call' do
-    xit 'calculates agile score' do
-      # TODO
+    it 'calculates and saves agile score' do
+      expect(@assign.scoring).to be
     end
 
-    xit 'adds factor score' do
-      # TODO
+    it 'add scoring for all factors' do
+      scoring = @assign.scoring
+      factors = @norm.factors.map(&:id)
+      factors.each { |id| expect(scoring).to include(id.to_s) }
     end
 
-    xit 'adds zscore' do
-      # TODO
+    it 'adds factor score' do
+      factor_score = @assign.scoring.first.last
+      expect(factor_score).to include('factor_score')
     end
 
-    xit 'adds percentile' do
-      # TODO
+    it 'adds zscore' do
+      factor_score = @assign.scoring.first.last
+      expect(factor_score).to include('zscore')
     end
 
-    xit 'adds normed score' do
-      # TODO
+    it 'adds normed score' do
+      factor_score = @assign.scoring.first.last
+      expect(factor_score).to include('normed_score')
     end
   end
 end
