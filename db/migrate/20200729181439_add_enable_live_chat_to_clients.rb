3b16b0cecc8077de6032ccb3e6e69214fa74b17a
Syed Aslam <aslam@thetalententerprise.com>
2020-07-30 00:07:20 +0530

    ...

--- db/migrate/20200729181439_add_enable_live_chat_to_clients.rb
+++ db/migrate/20200729181439_add_enable_live_chat_to_clients.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEnableLiveChatToClients < ActiveRecord::Migration[5.1]
   def change
     add_column :clients, :enable_live_chat, :boolean, null: false, default: false
