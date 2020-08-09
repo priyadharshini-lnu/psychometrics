0ff1a399c0eeecd34d45ff7a3428d78abc66151b
Syed Aslam <aslam@thetalententerprise.com>
2020-08-10 03:45:19 +0530

    calculate scoring by strategy

--- app/core/assigns/calculate_agile_scoring.rb
+++ app/core/assigns/calculate_agile_scoring.rb
@@ -24,7 +24,6 @@ module Assigns
 
       calculate
       broadcast(:ok, assign.update(scoring: scoring))
-      # broadcast(:ok, @scoring)
     end
 
     private
@@ -39,7 +38,7 @@ module Assigns
 
     def get_factors_sub_factor(factor_id, sub_factor_id)
       factor = get_factor(factor_id)
-      factor.factors_sub_factors.find { |f| f.id == sub_factor_id }
+      factor.factors_sub_factors.find { |f| f.sub_factor_id == sub_factor_id }
     end
 
     def calculate
@@ -74,64 +73,20 @@ module Assigns
       scoring.tap do |original_scoring|
         original_scoring.each do |factor_id, data|
           factor = get_factor(factor_id)
-          original_scoring[factor_id]['score'] = calculate_factor_score(factor, data)
-        end
-      end
-    end
-
-    def prepare_sub_factors(factor, sub_factor_ids)
-      # { factor_id => <score>, ... }
-      sub_factor_scores = Hash[sub_factor_ids.zip(sub_factor_ids.map { |id| { 'score': scoring[id]['score'] } })]
-      sub_factor_scores.tap do |sub_factors|
-        sub_factors.each do |sub_factor_id, _|
-          sub_factors[sub_factor_id]['weight'] = get_factors_sub_factor(factor.id, sub_factor_id).weight
+          data['score'] = calculate_factor_score(factor, data)
         end
       end
     end
 
     def calculate_factor_score(factor, data)
+      return data['score'] if data.key?('score')
+
       score = if factor.questions_strategy?
-                extend_blocks_with_scores(data[:blocks])
-
-                item_score = data['itemScore'] || 1
-                block_scores = data['blocks'].map { |block| block['score'] }
-                block_scores.sum { |block_score| block_score * item_score }
+                by_questions_strategy(data)
               elsif factor.sub_factors_average_strategy?
-                sub_factor_ids = data.dig('sub_factors')
-
-                nil if sub_factor_ids.blank?
-
-                # { factor_id => <score>, ... }
-                sub_factor_scores = prepare_sub_factors(factor, sub_factor_ids)
-
-                sub_factor_scores.select { |_, sub_factor_score| sub_factor_score.nil? }.tap do |unscored_factors|
-                  unscored_factors.each do |factor_id, _|
-                    factor = get_factor(factor_id)
-                    factor_score = calculate_factor_score(factor, data)
-                    scoring[factor_id]['score'] = factor_score
-                    unscored_factors[factor_id]['score'] = factor_score
-                  end
-                end
-
-                score_data = sub_factor_scores.each_with_object(sum: 0, count: 0) do |(_, config), memo|
-                  weight = config['weight'] || 1
-
-                  memo['sum'] += config['score'] * weight
-                  memo['count'] += weight
-                end
-
-                score_data['sum'] / score_data['count'].to_f
+                by_sub_factors_average_strategy(factor, data)
               end
       score
-    end
-
-    def extend_blocks_with_scores(blocks)
-      blocks.tap do |original_blocks|
-        original_blocks.each do |block|
-          questions = block.dig('questions')
-          block['score'] = count_correct_answers(questions)
-        end
-      end
     end
 
     def extend_with_norm_score
@@ -168,6 +123,69 @@ module Assigns
 
         (result & answer).any?
       end
+    end
+
+    def by_questions_strategy(data)
+      extend_blocks_with_scores(data['blocks'])
+
+      item_score = data['itemScore'] || 1
+      block_scores = data['blocks'].map { |block| block['score'] }
+      block_scores.sum { |block_score| block_score * item_score }
+    end
+
+    def extend_blocks_with_scores(blocks)
+      blocks.tap do |original_blocks|
+        original_blocks.each do |block|
+          questions = block.dig('questions')
+          block['score'] = count_correct_answers(questions)
+        end
+      end
+    end
+
+    def by_sub_factors_average_strategy(factor, data)
+      sub_factor_ids = data.dig('sub_factors')
+
+      nil if sub_factor_ids.blank?
+
+      # { factor_id => <score>, ... }
+      sub_factor_scores = prepare_sub_factors(factor, sub_factor_ids)
+      calculate_average(calculate_sub_factor_scores(sub_factor_scores))
+    end
+
+    def prepare_sub_factors(factor, sub_factor_ids)
+      # { factor_id => <score>, ... }
+      sub_factor_scores = Hash[sub_factor_ids.zip(sub_factor_ids.map { |id| { 'score': scoring[id]['score'] } })]
+      sub_factor_scores.tap do |sub_factors|
+        sub_factors.each do |sub_factor_id, score_hash|
+          score_hash[:weight] = get_factors_sub_factor(factor.id, sub_factor_id).weight
+        end
+      end
+
+      sub_factor_scores
+    end
+
+    def calculate_sub_factor_scores(sub_factor_scores)
+      sub_factor_scores.select { |_, sub_factor_score| sub_factor_score[:score].nil? }.tap do |unscored_factors|
+        unscored_factors.each do |factor_id, score_hash|
+          factor = get_factor(factor_id)
+          factor_score = calculate_factor_score(factor, scoring[factor_id])
+          score_hash[:score] = factor_score
+          scoring[factor_id]['score'] = factor_score
+        end
+      end
+
+      sub_factor_scores
+    end
+
+    def calculate_average(scores)
+      total = scores.each_with_object(sum: 0, count: 0) do |(_, config), memo|
+        weight = config[:weight] || 1
+
+        memo[:sum] += config[:score] * weight
+        memo[:count] += weight
+      end
+
+      total[:sum] / total[:count].to_f
     end
   end
 end
--- spec/core/assigns/calculate_agile_scoring_spec.rb
+++ spec/core/assigns/calculate_agile_scoring_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe Assigns::CalculateAgileScoring do
   def setup_data(complete = false)
-    @norm = FactoryBot.create(:norm, :percentile)
+    @norm = create(:norm, :percentile)
     @factor_id = @norm.factors.first.id
 
     config = {
@@ -80,6 +80,72 @@ describe Assigns::CalculateAgileScoring do
     end
   end
 
+  def get_questions(scope, num = 1)
+    Array.new(num) do |index|
+      { 'id': "q-#{scope}-#{index}", 'answers': [['answer']] }
+    end
+  end
+
+  def get_scoring(factor_id, item_score = 1)
+    [{ 'factorId': factor_id, 'itemScore': item_score }]
+  end
+
+  def get_block(scoring_factor, num_questions = 2)
+    {
+      'id': "block-of-#{scoring_factor}",
+      'scoring': get_scoring(scoring_factor),
+      'questions': get_questions(num_questions, scoring_factor)
+    }
+  end
+
+  def prepare_scene(scoring_factors)
+    blocks = scoring_factors.map { |scoring_factor| get_block(scoring_factor) }
+    { 'id': 'scene-1', 'type': 'AssessmentScene', 'data': { blocks: blocks } }
+  end
+
+  def prepare_answers(scoring_factors)
+    results = { 'answers' => {}, 'group_id' => 'group-1' }
+    scoring_factors.each do |scoring_factor|
+      2.times do |index|
+        question_id = "q-#{scoring_factor}-#{index}"
+        results['answers'][question_id] = {
+          'id' => question_id,
+          'answers' => ['answer'],
+          'group_id' => 'group-1'
+        }
+      end
+    end
+
+    results
+  end
+
+  def setup_data_for_average_strategy
+    @norm = create(:norm, :percentile)
+    @factor_ids = @norm.factors.pluck(:id).sort
+
+    factor1 = @norm.factors.find_by(id: @factor_ids.first)
+    factor1.scoring_strategy = :sub_factors_average
+    factor1.save
+
+    create(:factors_sub_factor, factor: factor1, sub_factor_id: @factor_ids.second, weight: 1)
+    create(:factors_sub_factor, factor: factor1, sub_factor_id: @factor_ids.third, weight: 2)
+    create(:factors_sub_factor, factor: factor1, sub_factor_id: @factor_ids.fourth, weight: 3)
+    create(:factors_sub_factor, factor: factor1, sub_factor_id: @factor_ids.fifth, weight: 4)
+
+    config = {
+      'normId': @norm.id,
+      'groups': [{ 'id': 'group-1', 'scenes': [prepare_scene(@norm.factors.pluck(:id))] }]
+    }
+
+    results = [prepare_answers(@norm.factors.pluck(:id))]
+
+    @assign = create(:assign, results: results)
+    @agile = create(:agile, assessment: @assign.assessment, config: config)
+
+    norm_data = { id: @norm.id, type: 'percentile' }
+    @assign.update_columns(status: :completed, completed_at: Time.now, norm_data: norm_data)
+  end
+
   describe 'it broadcasts :invalid when assign is incomplete' do
     before(:all) do
       setup_data
@@ -114,7 +180,7 @@ describe Assigns::CalculateAgileScoring do
       expect(@assign.scoring).to be
     end
 
-    it 'add scoring for all factors' do
+    it 'adds scoring for all factors' do
       scoring = @assign.scoring
       factors = @norm.factors.pluck(:id)
       factors.each { |id| expect(scoring).to include(id.to_s) }
@@ -141,6 +207,40 @@ describe Assigns::CalculateAgileScoring do
     it 'adds normed score' do
       factor_score = @assign.scoring.first.last
       expect(factor_score).to include('norm_score')
+    end
+  end
+
+  describe 'calculations by strategy' do
+    before(:context) do
+      setup_data_for_average_strategy
+    end
+
+    subject { described_class.call(@assign) }
+
+    it { expect { subject }.to broadcast(:ok) }
+
+    it 'calculates and saves agile score' do
+      expect(@assign.scoring).to be
+    end
+
+    it 'calculates score by sub-factors average strategy' do
+      scoring = @assign.scoring
+
+      # sub_factor_scores:
+      # {
+      #   sub_factor_id => { :score=>2, :weight=>1.0 },
+      #   sub_factor_id => { :score=>2, :weight=>2.0 },
+      #   sub_factor_id => { :score=>2, :weight=>3.0 },
+      #   sub_factor_id => { :score=>2, :weight=>4.0 }
+      # }
+
+      # (1.0 * 2 + 2.0 * 2 + 3.0 * 2 + 4.0 * 2) / (1.0 + 2.0 + 3.0 + 4.0) = 2
+      expect(scoring[@factor_ids.first.to_s]['score']).to eq(2)
+
+      expect(scoring[@factor_ids.second.to_s]['score']).to eq(2)
+      expect(scoring[@factor_ids.third.to_s]['score']).to eq(2)
+      expect(scoring[@factor_ids.fourth.to_s]['score']).to eq(2)
+      expect(scoring[@factor_ids.fifth.to_s]['score']).to eq(2)
     end
   end
 end
