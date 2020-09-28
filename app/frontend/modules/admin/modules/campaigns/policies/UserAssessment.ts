d0d3fd319580872ea07db67b8140672153a8d2d8
namrataukirde <namrata.ukirde@thetalententerprise.com>
2020-09-28 13:59:00 +0530

    move function to check superadmin and hasgrant to currentUser file

--- app/frontend/modules/admin/modules/campaigns/policies/UserAssessment.ts
+++ app/frontend/modules/admin/modules/campaigns/policies/UserAssessment.ts
@@ -1,14 +1,11 @@
-import { getIn } from 'utils/immutable'
+import { isSuperAdmin, hasGrant } from 'core/currentUser'
 import IUserAssessment from '../interfaces/UserAssessment'
 import User from '../interfaces/User'
-
-const isSuperAdmin = ({ role }: User) => role === 'Users::SuperAdmin'
-const hasGrant = ({ grants }: User, [scope, action]: string[]) => getIn(grants, scope, []).includes(action)
 
 const UserAssessment = {
   updateAdditionalTime: (u: User, record: IUserAssessment) => record.status === 'completed' && record.isExpired,
   resetResults: (currentUser: User, record: IUserAssessment) => {
-    const superAdminOrHasGrant = isSuperAdmin(currentUser) || hasGrant(currentUser, ['assessments', 'view'])
+    const superAdminOrHasGrant = isSuperAdmin(currentUser) || hasGrant(currentUser, 'assessments', 'view')
     return superAdminOrHasGrant && record.status !== 'not_started' && !record.isExternal
   },
 }
