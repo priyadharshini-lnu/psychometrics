1d6c340285ac955dfd3d21bfd213aae0ad0601b8
Shuja Shabandri <shuja@shabandri.com>
2020-10-07 19:00:26 +0400

    Feature/multiple fixes (#552)
    * fixes LH-1044: Footer fixed to page bottom
    
    * fixes: LH-694 AssessmentProperties converted to tsx and using react-select for dropdown
    
    * fixes LH-794: Color picker issues
    
    * add back debounce

--- app/frontend/modules/reports/views/PropertyPanel/components/ColorPickerModal/ColorPickerModal.tsx
+++ app/frontend/modules/reports/views/PropertyPanel/components/ColorPickerModal/ColorPickerModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import _ from 'lodash'
-import { SketchPicker } from 'react-color'
+import { ColorState, SketchPicker } from 'react-color'
 import styles from './styles.scss'
 import { Close, ChangeColor, Color } from '../../../../core/temp/colorPicker'
 
@@ -9,27 +9,28 @@ interface Props {
   closePicker: Close
   changeColor: ChangeColor
   color: Color
-  onComplete: null | undefined | ((color: Color) => void)
-  onChange: null | undefined | ((color: Color) => void)
+  onComplete: null | undefined | ((color: ColorState) => void)
+  onChange: null | undefined | ((color: ColorState) => void)
 }
 
 const ColorPickerModal: React.FC<Props> = ({
   isOpen, closePicker, changeColor, onComplete, onChange, color,
 }) => {
-  const handleChange = _.debounce((newColor: Color) => {
-    // sometimes SketchPicker triggers the event twice
-    if (color === newColor) return
-
+  const handleChange = _.debounce((newColor: ColorState) => {
     changeColor(newColor)
     onChange && onChange(newColor)
   }, 200)
-
   if (!isOpen) return null
 
   return (
     <div className={styles.popover}>
       <div className={styles.cover} onClick={closePicker} />
-      <SketchPicker className={styles.picker} onChangeComplete={onComplete} color={color} onChange={handleChange} />
+      <SketchPicker
+        className={styles.picker}
+        onChangeComplete={(color: ColorState) => onComplete && onComplete(color)}
+        color={color}
+        onChange={handleChange}
+      />
     </div>
   )
 }
