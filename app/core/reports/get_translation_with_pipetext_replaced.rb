8b2f3f6baacc2bd70bb50cae10ac2d5d17ab5a33
Rohan Pujari <rohanpujaris@gmail.com>
2020-10-08 11:20:42 +0530

    Replace pipetext for translation (#555)
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>

---
+++ app/core/reports/get_translation_with_pipetext_replaced.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Reports
+  class GetTranslationWithPipetextReplaced < BaseCommand
+    private_attr_reader :report, :piped_text_context, :locale
+
+    def initialize(report, options)
+      @report = report
+      @piped_text_context = options[:piped_text_context]
+      @locale = options[:locale]
+    end
+
+    def call
+      translations = Translation.to_hash_for_report(report.id, report.assessment_ids, locale)
+      broadcast :ok, {} if translations.empty?
+
+      replace_pipetext_for_question(translations)
+      replace_pipetext_for_report_modules(translations)
+
+      broadcast :ok, translations
+    end
+
+    private
+
+    def replace_pipetext_for_question(translations)
+      return unless translations['question']
+
+      translations['question'] = translations['question'].each_with_object({}) do |(question_id, question_details), acc|
+        question_text = question_details['questionText']
+        question_text = Threesixty::PipedText::Perform.call!(question_text, piped_text_context)
+        acc[question_id] = question_details.merge('questionText' => question_text)
+      end
+    end
+
+    def replace_pipetext_for_report_modules(translations)
+      return unless translations['reports/module']
+
+      translations['reports/module'] = translations['reports/module'].
+                                       each_with_object({}) do |(module_id, module_details), acc|
+        module_text = module_details['text']
+        module_text = Threesixty::PipedText::Perform.call!(module_text, piped_text_context)
+        acc[module_id] = module_details.merge('text' => module_text)
+      end
+    end
+  end
+end
--- spec/core/user_reports/generate_and_save_pdf_spec.rb
+++ spec/core/user_reports/generate_and_save_pdf_spec.rb
@@ -8,12 +8,21 @@ describe UserReports::GenerateAndSavePdf do
   let(:report) { user_report.report }
   let(:current_user) { create(:superadmin) }
 
-  it 'save pdf in user_report' do
+  it 'save pdf in user_report if report can be generated' do
     expect(UserReports::GeneratePdf).to receive(:call!).and_return('spec/fixtures/files/reports/test.pdf')
+    expect(user_report).to receive(:generatable?).and_return(true)
 
     described_class.call!(user_report, current_user)
 
     expect(user_report.pdf.url).to be_present
     expect(user_report.read_attribute(:pdf)).to eq('test.pdf')
+  end
+
+  it "doesn't saeve pdf in user_report if report can't be generated" do
+    expect(user_report).to receive(:generatable?).and_return(false)
+
+    described_class.call!(user_report, current_user)
+
+    expect(user_report.pdf.url).to be_nil
   end
 end
