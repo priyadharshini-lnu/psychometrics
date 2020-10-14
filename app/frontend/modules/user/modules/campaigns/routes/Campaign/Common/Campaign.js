80ff9aab1488905ebf9bd2e344cbef5da60f72a3
Fedor Tarasov <tarassovf@gmail.com>
2020-10-14 13:12:09 +0300

    show ungrouped assessment cards (#580)
    * show ungrouped assessment cards

--- app/frontend/modules/user/modules/campaigns/routes/Campaign/Common/Campaign.js
+++ app/frontend/modules/user/modules/campaigns/routes/Campaign/Common/Campaign.js
@@ -45,7 +45,7 @@ export default function Campaign ({
   const counters = _.countBy(campaign.userAssessments, 'status')
   const allAssessmentsComplete = counters.completed === campaign.userAssessments.length
   let prevGroup
-  const ungrouped = _.compact(
+  let ungrouped = _.compact(
     campaign.ungroupedAssessmentsIds.map(id => _.find(campaign.userAssessments, { assessmentId: id })),
   )
   const isMD = useMedia('max-md')
@@ -58,6 +58,15 @@ export default function Campaign ({
   const onTimerFinish = () => {
     fetchCampaign(match.url)
   }
+
+  const allCampaignLevelAsssementIds = _.flatten(
+    [...groups.map(g => g.campaignAssessmentIds), campaign.ungroupedAssessmentsIds],
+  )
+
+  const ungroupedAssessments = campaign.userAssessments.filter(
+    ua => !_.includes(allCampaignLevelAsssementIds, ua.assessmentId),
+  )
+  ungrouped = [...ungrouped, ...ungroupedAssessments]
 
   return (
     <Layout>
@@ -154,7 +163,6 @@ export default function Campaign ({
                         <Col xs={24} sm={24} lg={24} xl={24}>
                           <div className={cs('group')}>
                             <div className="group-title">{I18n.t('campaign.ungrouped')}</div>
-
                             <Row type="flex" gutter={[16, 16]} className="cards">
                               {ungrouped.map((userAssessment) => {
                                 const Assessment = Assessments[userAssessment.type]
