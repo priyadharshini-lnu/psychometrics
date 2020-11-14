7a036d1eaedfb536c168b80b64c32dcf5a183cf7
Fedor Tarasov <tarassovf@gmail.com>
2020-11-15 01:25:34 +0300

    add ts fork checker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,7 @@ const { resolve } = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const cores = require('os').cpus().length
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const less = require('./loaders/less')
 const tsLoader = require('./loaders/ts-loader')
 // uncomment it in order to use bundle analyzer
@@ -31,12 +31,19 @@ environment.plugins.insert(
     RecordRTC: 'recordrtc',
   }),
 )
-
-// environment.plugins.insert('TsForkChecker', new ForkTsCheckerWebpackPlugin({
-//   eslint: {
-//     files: './app/frontend/**/*.{ts,tsx,js,jsx}',
-//   },
-// }))
+if (__DEV__) {
+  environment.plugins.insert('TsForkChecker', new ForkTsCheckerWebpackPlugin({
+    eslint: {
+      files: './app/frontend/**/*.{ts,tsx,js,jsx}',
+    },
+    typescript: {
+      diagnosticOptions: {
+        semantic: true,
+        syntactic: true,
+      },
+    },
+  }))
+}
 
 //   uncomment it in order to use bundle analyzer
 // environment.plugins.insert(
@@ -54,15 +61,17 @@ const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === '
 environment.loaders.get('babel').use.unshift({
   loader: 'thread-loader',
   options: {
-    workers: cores / 2,
+    workers: cores - 2,
+    poolTimeout: Infinity,
   },
 })
+environment.loaders.get('babel').use.unshift({ loader: 'cache-loader' })
+
 
 CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
 environment.loaders.append('less', less)
 environment.loaders.append('typescript', tsLoader)
 
-console.log(environment.loaders.get('typescript'))
 loaders.nodeModules.use[0].options.sourceMaps = true
 
 const vendors = [
--- config/webpack/loaders/ts-loader.js
+++ config/webpack/loaders/ts-loader.js
@@ -3,15 +3,19 @@ const cores = require('os').cpus().length
 module.exports = {
   test: /.(ts|tsx)$/,
   use: [
+    { loader: 'cache-loader' },
     {
       loader: 'thread-loader',
       options: {
-        workers: cores / 2,
+        workers: cores - 2,
+        poolTimeout: Infinity,
       },
     },
     {
       loader: 'ts-loader',
       options: {
+        happyPackMode: true,
+        transpileOnly: true,
       },
     },
   ],
