fe6104651ecc36de8aea326d5c3ce2cc60b8685a
atanych <atanych@gmail.com>
2020-12-17 09:12:20 +0300

    Fix value  in select (#748)

--- app/frontend/modules/reports/presenters/ReactSelectPresenter.js
+++ app/frontend/modules/reports/presenters/ReactSelectPresenter.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import _ from 'lodash'
 
-export const getValue = (options, value) => {
+export const getValue = (options, value, key = 'value') => {
   if (_.isArray(value)) {
     return value.map(val => (_.isObject(val) ? val : _.find(options, { value: val })))
   }
-  return _.isObject(value) ? value : _.find(options, { value })
+  return _.isObject(value) ? value : _.find(options, { [key]: value })
 }
