edbcbd723f8a797651497c8ab3ff159da8df3078
Rohan Pujari <rohanpujaris@gmail.com>
2020-12-27 15:01:59 +0530

    Allow createReducer to support AnyAction (#776)
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>

--- app/frontend/interfaces/ApiActionResponse.ts
+++ app/frontend/interfaces/ApiActionResponse.ts
@@ -5,6 +5,8 @@ import { Request } from './ApiAction'
 interface IApiActionResponse<T> extends AnyAction {
   request: Request<T>
   response: T
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type: any
 }
 
 export type ApiActionResponse<T> = IApiActionResponse<T>
--- app/frontend/utils/redux.ts
+++ app/frontend/utils/redux.ts
@@ -1,19 +1,25 @@
-import { Action } from 'redux'
+import { Action, AnyAction } from 'redux'
 
 export const DEPRECATED_createReducer = (handlers, defaultState) => (state = defaultState, action) => {
   const handler = handlers[action.type]
   return handler ? handler(state, action) : state
 }
-type Handlers<State, Types extends string, Actions extends Action<Types>> = {
-  readonly [Type in Types]: (state: State, action: Actions) => State
+type Handlers<State, Types extends string> = {
+  readonly [Type in Types]: (state: State, action: AnyAction) => State
 }
 
 export const createReducer = <State, Types extends string, Actions extends Action<Types>>(
-  handlers: Handlers<State, Types, Actions>,
+  handlers: Handlers<State, Types>,
   initialState: State,
 ) => (state = initialState, action: Actions) => (
   // eslint-disable-next-line no-prototype-builtins
     handlers.hasOwnProperty(action.type) ? handlers[action.type as Types](state, action) : state
   )
 
-export type Payload<T> = { payload: T }
+// eslint-disable-next-line @typescript-eslint/interface-name-prefix
+interface IPayload<P, T = any> extends AnyAction { // eslint-disable-line @typescript-eslint/no-explicit-any
+  payload: P
+  type: T
+}
+
+export type Payload<P, T = any> = IPayload<P, T> // eslint-disable-line @typescript-eslint/no-explicit-any
