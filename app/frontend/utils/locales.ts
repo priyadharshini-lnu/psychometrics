7b700d4c24ee4ee7dd9dd2cf73f645819810b611
Shuja Shabandri <shuja@shabandri.com>
2020-12-30 16:33:02 +0400

    removed unused function

--- app/frontend/utils/locales.ts
+++ app/frontend/utils/locales.ts
@@ -1,5 +1,3 @@
-const { I18n, $ } = window
-
 const LOCALES = {
   ar: 'rtl',
 }
@@ -7,10 +5,4 @@ const LOCALES = {
 export const isRtl = (code: string): boolean => {
   const locale = code.substr(0, 2)
   return LOCALES[locale] === 'rtl'
-}
-
-export const setLocale = (code: string): void => {
-  I18n.locale = code
-  $('body').data('locale', code)
-  $('body').toggleClass('rtl', isRtl(code))
 }
