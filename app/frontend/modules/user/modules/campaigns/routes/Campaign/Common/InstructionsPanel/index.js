b6dbf878a4d36796bcc9b6f4d5e722540f07292f
Shuja Shabandri <shuja@shabandri.com>
2021-01-18 00:24:02 +0400

    disables instructions collapse by default if campaign is started.

--- app/frontend/modules/user/modules/campaigns/routes/Campaign/Common/InstructionsPanel/index.js
+++ app/frontend/modules/user/modules/campaigns/routes/Campaign/Common/InstructionsPanel/index.js
@@ -11,9 +11,8 @@ export default function InstructionsPanel ({
   instructionsEnabled, instructions, showBegin, showContinue, onBegin, onContinue,
 }) {
   const showActions = showBegin || showContinue
-  const showInstructions = instructionsEnabled && showActions
   const activePanels = [
-    ...(showInstructions ? '1' : []),
+    ...(instructionsEnabled ? '1' : []),
     ...(showActions ? '2' : []),
   ]
 
@@ -28,7 +27,6 @@ export default function InstructionsPanel ({
           <Panel
             header={I18n.t('campaign.instructions.heading')}
             key="1"
-            showArrow={false}
             className={styles.customPanel}
           >
             {<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(instructions, { ADD_TAGS: ['iframe'] }) }} />}
