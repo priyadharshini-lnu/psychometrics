e3efa0d1cab1cfc8e0654abd951a4b5adf88d240
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2021-02-02 16:39:07 +0530

    fix/LH-1231 - Priorize custom CSS of app to load later in the App (Part 1) (#872)
    CSS import order fixed for
    - admin/datasheetManagement

--- app/frontend/packs/admin/datasheetManagement.js
+++ app/frontend/packs/admin/datasheetManagement.js
@@ -1,12 +1,16 @@
-import DatasheetManagementApp from 'modules/admin/modules/DatasheetManagement/App'
-import ReactDOM from 'react-dom'
 import React from 'react'
+import ReactDOM from 'react-dom'
+
 import 'modules/admin/styles/ant.less'
 import 'modules/admin/styles/common.scss'
 import 'styles/utils.scss'
+
 import initSentry from 'libs/initSentry'
 
+import DatasheetManagementApp from 'modules/admin/modules/DatasheetManagement/App'
+
 initSentry()
+
 ReactDOM.render(
   <DatasheetManagementApp />,
   document.getElementById('datasheet-management-container'),
