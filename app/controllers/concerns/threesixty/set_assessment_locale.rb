07e106c6412434fdc5b03414f69594c4e8c52c68
Shuja Shabandri <shuja@shabandri.com>
2021-02-11 19:33:50 +0400

    fixes wrong default locale for threesixty evaluation (#957)
    * fixes wrong default locale for threesixty evaluation
    
    * changes after review

--- app/controllers/concerns/threesixty/set_assessment_locale.rb
+++ app/controllers/concerns/threesixty/set_assessment_locale.rb
@@ -3,11 +3,19 @@
 module Threesixty::SetAssessmentLocale
   extend ActiveSupport::Concern
 
-  def set_locale_for_assessment(assessment_id)
-    @available_translations = ::Translation.available_translation_for_assessment(assessment_id)
-    if params[:lang] && (@available_translations + [I18n.default_locale.to_s]).include?(params[:lang])
+  def set_locale_for_users_result(users_result)
+    fallback_locale = @users_result.selected_locale || @current_user.locale || user_locale
+    @available_translations = ::Translation.available_translation_for_assessment(users_result.assessment_id)
+    valid_locales = @available_translations + [I18n.default_locale.to_s]
+
+    if params[:lang] && valid_locales.include?(params[:lang])
       @selected_locale = params[:lang]
+      @users_result.update(selected_locale: @selected_locale) if @users_result.selected_locale != selected_locale
+
+    elsif valid_locales.include?(fallback_locale)
+      @selected_locale = fallback_locale
     end
-    @selected_locale ||= user_locale # rubocop:disable Naming/MemoizedInstanceVariableName
+
+    @selected_locale ||= I18n.default_locale.to_s # rubocop:disable Naming/MemoizedInstanceVariableName
   end
 end
