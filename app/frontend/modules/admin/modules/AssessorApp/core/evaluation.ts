fe95f037d7d5c9fb767d9d07977f69a677373fca
Fedor Tarasov <tarassovf@gmail.com>
2021-03-07 16:27:08 +0300

    hide overview tab (#1024)

--- app/frontend/modules/admin/modules/AssessorApp/core/evaluation.ts
+++ app/frontend/modules/admin/modules/AssessorApp/core/evaluation.ts
@@ -105,6 +105,7 @@ const HANDLERS = {
     userInfo: response.user_info,
     assessorAssessments: response.assessor_assessments,
     subjectAssessments: response.subject_assessments,
+    currentAssessorFormId: _.get(response, ['assessor_assessments', 0, 'id']),
     currentAssessmentId: _.get(response, ['subject_assessments', 0, 'id']),
     loaded: true,
   }),
