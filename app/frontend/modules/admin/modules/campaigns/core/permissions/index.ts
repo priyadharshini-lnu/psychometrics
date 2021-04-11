3ed540b6ebcc54c065480f251d658c5e85ca7bfc
namrataukirde <namrata.ukirde@thetalententerprise.com>
2021-04-11 13:21:12 +0530

    add type for api action response

--- app/frontend/modules/admin/modules/campaigns/core/permissions/index.ts
+++ app/frontend/modules/admin/modules/campaigns/core/permissions/index.ts
@@ -3,8 +3,19 @@ import { createReducer } from 'utils/redux'
 import { ApiActionResponse } from 'interfaces/ApiActionResponse'
 import { FETCH } from '../list'
 
-const defaultState = {}
-type FetchType = ApiActionResponse<{permissions: {}}>
+const defaultState: State = {
+  permissions: {
+    create: false,
+  },
+}
+
+export interface State {
+  permissions: {
+    create: boolean,
+  },
+}
+
+type FetchType = ApiActionResponse<State>
 
 export const get = (state): {} => _.get(state, ['campaigns', 'permissions'])
 
