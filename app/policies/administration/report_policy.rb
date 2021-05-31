450077ead18dd7491c477e2a2bc79a420189190b
namrataukirde <namrata.ukirde@thetalententerprise.com>
2021-05-31 13:34:00 +0530

    fix error on report listing

--- app/policies/administration/report_policy.rb
+++ app/policies/administration/report_policy.rb
@@ -16,38 +16,28 @@ module Administration
     end
 
     def create?
-      permit = @user.has_grant?(:reports, :manage)
-      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
-      permit &&= ttes_ids.include?(record.owner_id) if record.is_a? ::Report
-      super || permit
+      @user.is?(:superadmin) || permit_manage?
     end
 
     def copy?
-      permit = @user.has_grant?(:reports, :manage)
-      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
-      permit &&= ttes_ids.include?(record.owner_id) if record.is_a? ::Report
-      super || permit
+      @user.is?(:superadmin) || permit_manage?
     end
 
     def edit?
-      permit = @user.has_grant?(:reports, :manage)
-      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
-      permit &&= ttes_ids.include?(record.owner_id) if record.is_a? ::Report
-      super || permit
+      @user.is?(:superadmin) || permit_manage?
     end
 
     def hogan_reports?
-      permit = @user.has_grant?(:reports, :manage)
-      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
-      permit &&= ttes_ids.include?(record.owner_id) if record.is_a? ::Report
-      super || permit
+      @user.is?(:superadmin) || permit_manage?
     end
 
     def upload_data_sheet?
-      permit = @user.has_grant?(:reports, :manage)
-      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
-      permit &&= ttes_ids.include?(record.owner_id) if record.is_a? ::Report
-      super || permit
+      @user.is?(:superadmin) || permit_manage?
+    end
+
+    # Can archive/unarchive Assessment
+    def toggle_archive?
+      @user.is?(:superadmin) || permit_manage?
     end
 
     # Can open Websocket Channel for build Report (Reports, Modules and etc.)
@@ -82,14 +72,6 @@ module Administration
       edit?
     end
 
-    # Can archive/unarchive Assessment
-    def toggle_archive?
-      permit = @user.has_grant?(:reports, :manage)
-      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
-      permit &&= ttes_ids.include?(record.owner_id) if record.is_a? ::Report
-      super || permit
-    end
-
     # Can regenerate reports if Superadmin
     #   and record is not external
     #
@@ -107,6 +89,14 @@ module Administration
 
     def restore?
       @user.is?(:superadmin) || @user.has_grant?(:reports, :manage)
+    end
+
+    private
+
+    def permit_manage?
+      permit = @user.has_grant?(:reports, :manage)
+      ttes_ids = @user.is?(:client_admin) ? @user.client_admin_clients_tte_ids : @user.project_admin_clients_tte_ids
+      permit && ttes_ids.include?(record.owner_id) if record.is_a? ::Report
     end
 
     class Scope < Scope
