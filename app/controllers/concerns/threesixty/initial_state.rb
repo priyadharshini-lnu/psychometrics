61c8ed9cdcf68fb2949716b09143a9f703213b85
Shuja Shabandri <shuja@shabandri.com>
2021-06-10 20:51:55 +0400

    adds project specific live chat

--- app/controllers/concerns/threesixty/initial_state.rb
+++ app/controllers/concerns/threesixty/initial_state.rb
@@ -30,7 +30,7 @@ module Threesixty::InitialState
       currentUser: serialized_current_user,
       liveChat: {
         url: Settings.live_chat.base_url,
-        token: Settings.live_chat.token,
+        token: live_chat_token,
         enabled: @current_project.enable_live_chat
       },
       examus: {
@@ -38,6 +38,10 @@ module Threesixty::InitialState
         integrationName: Rails.application.secrets.examus[:integration_name]
       }
     }
+  end
+
+  def live_chat_token
+    @current_project.live_chat_token.blank? ? Settings.live_chat.token : @current_project.live_chat_token
   end
 
   def serialized_current_user
