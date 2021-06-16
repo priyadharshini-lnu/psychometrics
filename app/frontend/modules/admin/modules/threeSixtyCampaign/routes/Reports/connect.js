8c1b9e7cde93208c24422d5db00cd0500f99489f
namrataukirde <namrata.ukirde@thetalententerprise.com>
2021-06-16 15:52:33 +0530

    show/hide edit subject report based on permission for 360

--- app/frontend/modules/admin/modules/threeSixtyCampaign/routes/Reports/connect.js
+++ app/frontend/modules/admin/modules/threeSixtyCampaign/routes/Reports/connect.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux'
 import {
-  getCurrentReportId,
+  getCurrentReportId, getCampaignReportPermissions,
 } from 'modules/admin/modules/threeSixtyCampaign/core/campaignDetails'
 
 export default connect(
   state => ({
     reportId: getCurrentReportId(state),
+    campaignReportPermissions: getCampaignReportPermissions(state),
   }),
   null,
 )
