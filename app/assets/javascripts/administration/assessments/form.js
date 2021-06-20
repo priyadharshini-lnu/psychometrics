6bf63e367f7aabb464b07ca4575fa4b7b45298ec
Rohan Pujari <rohanpujaris@gmail.com>
2021-06-20 20:19:09 +0530

    Remove js interpolation (#1406)
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>

--- app/assets/javascripts/administration/assessments/form.js
+++ app/assets/javascripts/administration/assessments/form.js
@@ -23,7 +23,7 @@ function AssessmentsForm () {
     if (value == "Assessments::Common") {
       $('#assessments_form').find('.common').removeClass('hidden').find(":input").removeAttr('disabled', true)
       integrations.forEach(function(integrationName) {
-        $('#assessments_form').find(`.${integrationName}:not(.common)`).addClass('hidden').find(":input").attr('disabled', true)
+        $('#assessments_form').find('.' + integrationName + ':not(.common)').addClass('hidden').find(":input").attr('disabled', true)
       })
       return
     }
@@ -34,11 +34,11 @@ function AssessmentsForm () {
   this.showHideFieldsForExternalIntegrations = function (integrationType) {
     var [_, className] = integrationType.toLowerCase().split('::')
 
-    $('#assessments_form').find(`.common:not(.${className})`).addClass('hidden').find(":input").attr('disabled', true)
+    $('#assessments_form').find('.common:not(.' + className + ')').addClass('hidden').find(":input").attr('disabled', true)
     integrations.forEach(function (integrationName) {
-      $('#assessments_form').find(`.${integrationName}:not(.${className})`).addClass('hidden').find(":input").attr('disabled', true)
+      $('#assessments_form').find('.' + integrationName + ':not(.' + className + ')').addClass('hidden').find(":input").attr('disabled', true)
     })
-    $('#assessments_form').find(`.${className}:not(.common)`).removeClass('hidden').find(":input").removeAttr('disabled')
+    $('#assessments_form').find('.' + className + ':not(.common)').removeClass('hidden').find(":input").removeAttr('disabled')
   }
 }
 
