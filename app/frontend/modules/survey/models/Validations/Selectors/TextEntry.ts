8e00bb338970572bf7d7d08ca0e266791b3041d1
Fedor Tarasov <tarassovf@gmail.com>
2021-06-24 12:40:32 +0300

    fix select validation (#1431)

--- app/frontend/modules/survey/models/Validations/Selectors/TextEntry.ts
+++ app/frontend/modules/survey/models/Validations/Selectors/TextEntry.ts
@@ -3,11 +3,13 @@ import _ from 'lodash'
 interface Condition {
   answer: {}
   result: { answers: []}
+  value: string
+  type: string
 }
 
 function find (condition: Condition) {
   const answers = _.get(condition, ['result', 'answers'], [])
-  return _.find(answers, { index: +condition.answer })
+  return _.find(answers, { index: +condition.answer, value: condition.type === 'bool' ? true : condition.value })
 }
 
 export default {
