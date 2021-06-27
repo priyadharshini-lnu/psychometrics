056b6639cbab88de954398922a5524ea94de6fb1
Rohan Pujari <rohanpujaris@gmail.com>
2021-06-27 18:52:28 +0530

    Add all saville assessment (#1459) (#1460)
    * Add all saville assessment
    
    * Fix specs
    
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>
    
    Co-authored-by: Rohan Pujari <rohan@thetalententerprise.com>

--- spec/models/saville_assessment_setting_spec.rb
+++ spec/models/saville_assessment_setting_spec.rb
@@ -4,21 +4,25 @@ require 'rails_helper'
 
 RSpec.describe SavilleAssessmentSetting, type: :model do
   let(:saville_assessment_setting) do
-    create(:saville_assessment_setting, saville_assessment_id: 'A830E4AB-BC66-4238-92E0-6E6FD3FD1EDF')
+    create(:saville_assessment_setting, saville_assessment_id: '73F44184-EED3-400D-B4BB-14925E4502D3')
   end
 
   it 'sets saville_norm_id to default_norm_id' do
-    expect(saville_assessment_setting.saville_norm_id).to eq('05EDB032-2AB3-4B9E-8CCC-F5BCB7FE4337')
+    expect(saville_assessment_setting.saville_norm_id).to eq('DB13563C-4029-4473-848A-AFDF2C59DFB8')
   end
 
   describe '#saville_norms' do
     it 'returns all saville_norms for particula assessment' do
       expect(saville_assessment_setting.saville_norms).to eq(
         [
-          { id: '05EDB032-2AB3-4B9E-8CCC-F5BCB7FE4337',
-            name: 'Wave Focus Styles V4 - Graduates - All (INT, IA, 2021)' },
-          { id: 'A0F63805-6099-47EB-9037-187929E80021',
-            name: 'Wave Focus Styles V4 - Graduates - Recent (INT, IA, 2021)' }
+          {
+            id: 'DB13563C-4029-4473-848A-AFDF2C59DFB8',
+            name: 'Spatial Reasoning Aptitude & Pace (Rx) - Apprentices (INT, IA, 2018)'
+          },
+          {
+            id: 'FFDAD963-8912-44B5-80EA-8E6503B92382',
+            name: 'Spatial Reasoning Aptitude & Pace (Rx) - Technical Occupations (INT, IA, 2018)'
+          }
         ]
       )
     end
