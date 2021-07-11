6de8ea2869bafac23dab4ca2cde8e3e9aa6dd53c
Fedor Tarasov <tarassovf@gmail.com>
2021-07-11 17:04:44 +0300

    add user uploaded type for image module (#1511)
    * add user uploaded type for image module
    
    * fix specs
    
    * review updates
    
    * minor update

---
+++ app/frontend/modules/reports/components/modules/Image/components/QuestionsSelect.tsx
@@ -0,0 +1,64 @@
+import _ from 'lodash'
+import React from 'react'
+import Select from 'react-select'
+import AssessmentProperties from 'modules/reports/components/modules/CommonProperties/AssessmentProperties'
+import panelStyles from 'modules/reports/views/PropertyPanel/components/PropertyPanel.scss'
+import { getValue } from 'modules/reports/presenters/ReactSelectPresenter'
+
+interface Question {
+  id: number
+  name: string
+  type: string
+}
+
+interface Module {
+  assessment_id: number
+  props: {
+    sourceQuestion: number
+  }
+}
+
+interface Props {
+  model: Module
+  questions: Question[]
+  onSelect: () => void
+}
+
+export const QuestionSelect: React.FC<Props> = ({ model, questions, onSelect }) => {
+  const select = () => {
+    onSelect()
+  }
+
+  const getQuestions = () => _.filter(questions, q => q.type === 'FileUpload')
+    .map(q => ({ label: q.name, value: q.id }))
+
+  const changeAssessment = (assessmentId) => {
+    model.assessment_id = assessmentId
+    select()
+  }
+
+  const changeSourceQuestion = (value) => {
+    model.props.sourceQuestion = value.value
+    select()
+  }
+
+  return (
+    <div>
+      <hr className={panelStyles.divider} />
+      <div>
+        <AssessmentProperties assessmentId={model.assessment_id} changeAssessment={changeAssessment} />
+        <span>Question</span>
+        <Select
+          name="form-field-name"
+          value={getValue(getQuestions(), _.result(model, 'props.sourceQuestion', 'Choose question'))}
+          options={getQuestions()}
+          isClearable={false}
+          autoFocus={false}
+          onChange={changeSourceQuestion}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default QuestionSelect
