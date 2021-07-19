70d0aa12b46a98202e3aa7bd296bc1ca704c9737
namrataukirde <namrata.ukirde@thetalententerprise.com>
2021-07-19 19:26:28 +0530

    assessment policy changes for left sidebar

--- app/controllers/administration/assessments_controller.rb
+++ app/controllers/administration/assessments_controller.rb
@@ -151,6 +151,14 @@ class Administration::AssessmentsController < Administration::BaseController
 
   private
 
+  def pundit_authorize
+    authorize(
+      resource || resource_class,
+      nil,
+      project_id: resource&.owner_id
+    )
+  end
+
   def init_breadcrumbs
     add_breadcrumb I18n.t('administration.breadcrumbs.home'), %i[administration root]
     add_breadcrumb I18n.t("administration.breadcrumbs.#{resource_class.model_name.plural}"), action: :index
