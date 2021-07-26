338fc98f590d812f621f2f3ee32b9290c2b1204e
namrataukirde <namrata.ukirde@thetalententerprise.com>
2021-07-26 13:16:31 +0530

    fixed review comments: dont show owner dropdown for client admin of single client

--- app/controllers/administration/assessments_controller.rb
+++ app/controllers/administration/assessments_controller.rb
@@ -33,6 +33,10 @@ class Administration::AssessmentsController < Administration::BaseController
   def create
     @_resource = resource_class.new(resource_params)
 
+    if current_user.is?(:client_admin) && resource_params[:owner_id].blank?
+      resource.owner_id = current_user.client_admin_client_ids.first
+    end
+
     respond_to do |format|
       if resource.save
         format.js
