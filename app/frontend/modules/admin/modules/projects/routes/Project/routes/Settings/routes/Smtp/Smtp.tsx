4ea91b52d03a2d9457b070b3392779dac9e84c43
Rohan Pujari <rohanpujaris@gmail.com>
2021-08-23 17:49:10 +0530

    Smtp intial form

--- app/frontend/modules/admin/modules/projects/routes/Project/routes/Settings/routes/Smtp/Smtp.tsx
+++ app/frontend/modules/admin/modules/projects/routes/Project/routes/Settings/routes/Smtp/Smtp.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Row, Col, Form, Input, Button, Space, message,
+} from 'antd'
+import { FieldData } from 'rc-field-form/lib/interface'
+import { RouteComponentProps, useParams } from 'react-router-dom'
+import ResourceForm from 'components/ResourceForm'
 
-export const Smtp: React.FC = () => (
-  <div>
-    hi
-  </div>
-)
+interface Params {
+  campaignId: string
+}
+
+type Props = RouteComponentProps
+
+const { I18n } = window
+
+export const Smtp: React.FC<Props> = ({ }) => {
+  const [form] = Form.useForm()
+  const [fields, setFields] = useState<FieldData[] | []>([])
+  const { projectId } = useParams<{ projectId: string }>()
+  
+  return (
+    <Row justify="space-between" className="pm">
+      <Col sm={24} md={8} >
+        <ResourceForm
+          resourceName="smtpSetting"
+          requestScope="campaigns"
+          resourceBaseUrl={`/administration/projects/${projectId}/users`}
+          resource={{
+            id: 10
+          }}
+          storeManager={{ form, fields, setFields }}
+          showSuccessMessages
+        >
+          {({ form }) => (
+            <>
+              <Form.Item
+                name="host"
+                label={I18n.t('campaign_report.column.report_bundle')}
+                rules={[{ required: true }]}
+                >
+                  <Input />
+              </Form.Item>
+              <Button type='primary' htmlType='submit'>Save</Button>
+            </>
+          )}
+        </ResourceForm>
+      </Col>
+    </Row>
+  )
+}
