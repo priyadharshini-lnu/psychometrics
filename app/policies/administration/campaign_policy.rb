5c1ed90e2bbee42586748f27a3e5716054ac3c95
Namrata Ukirde <namrataukirde@gmail.com>
2021-09-17 18:21:57 +0530

    bug/lh-1843 remove design grant from client and add it to project (#1728)

--- app/policies/administration/campaign_policy.rb
+++ app/policies/administration/campaign_policy.rb
@@ -80,7 +80,7 @@ module Administration
     end
 
     def design?
-      @user.is?(:superadmin) || @user.has_permission?(:clients, :design, project_id: project_id)
+      @user.is?(:superadmin) || @user.has_permission?(:projects, :design, project_id: project_id)
     end
 
     def export?
