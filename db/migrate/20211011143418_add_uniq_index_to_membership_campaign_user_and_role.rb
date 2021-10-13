058561376a1749b4bd59bbb90feb3e1d9fbd6615
Namrata Ukirde <namrataukirde@gmail.com>
2021-10-13 11:27:05 +0400

    fix review deployment for uniq index error (#1809)

--- db/migrate/20211011143418_add_uniq_index_to_membership_campaign_user_and_role.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddUniqIndexToMembershipCampaignUserAndRole < ActiveRecord::Migration[5.2]
-  def change
-    add_index :memberships, %i[client_id role campaign_id], unique: true
-  end
-end
