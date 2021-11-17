01f0f6477917b2e837d186ac0bde65dcb76d06a5
Fedor Tarasov <tarassovf@gmail.com>
2021-11-17 09:44:09 +0300

    update evaluation status after begin (#1919)
    * update evaluation status after begin
    
    * fix specs

--- spec/core/user_assessments/pass_spec.rb
+++ spec/core/user_assessments/pass_spec.rb
@@ -89,7 +89,7 @@ describe UserAssessments::Pass do
       user_assessment.status = 'not_started'
       described_class.call!(user_assessment, 'ru')
 
-      expect(user_assessment.status).to eq('in_progress')
+      expect(user_assessment.status).to eq('not_started')
       expect(user_assessment.users_result.selected_locale).to eq('ru')
       expect(user_assessment.users_result.expiry_date).to eq(nil)
     end
@@ -98,9 +98,9 @@ describe UserAssessments::Pass do
       user_assessment.status = 'interrupted'
       described_class.call!(user_assessment, nil)
 
-      expect(user_assessment.status).to eq('in_progress')
+      expect(user_assessment.status).to eq('interrupted')
       expect(user_assessment.users_result.selected_locale).to be_nil
-      expect(user_assessment.users_result.expiry_date).to eq(20.seconds.from_now)
+      expect(user_assessment.users_result.expiry_date).to eq(nil)
     end
 
     it 'status = completed' do
