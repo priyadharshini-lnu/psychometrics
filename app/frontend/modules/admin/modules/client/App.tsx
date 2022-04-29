4f63dee5725b20ad76d15c5c9c7cfec64f78c0b5
Rohan Pujari <rohanpujaris@gmail.com>
2022-04-29 18:22:46 +0530

    Intial useResources hook

--- app/frontend/modules/admin/modules/client/App.tsx
+++ app/frontend/modules/admin/modules/client/App.tsx
@@ -5,12 +5,38 @@ import { Provider } from 'react-redux'
 import HTML5Backend from 'react-dnd-html5-backend'
 import { DndProvider } from 'react-dnd'
 import store, { history } from 'modules/admin/store'
-// import { Client } from './routes/Client'
+import { ApiClient, ApiProvider } from 'jsonapi-react'
 
 import RouteList from 'components/RouteList'
 
 import { routes } from './routes'
 import settings from './settings'
+
+const client = new ApiClient({
+  url: 'https://ttedev.me:3030/api/v2/administration',
+  schema: {
+    clients: {
+      type: 'clients',
+      fields: {
+        name: 'string',
+      },
+      relationships: {
+        account_manager: {
+          type: 'users',
+        },
+        project_manager: {
+          type: 'users',
+        }
+      }
+    },
+    users: {
+      type: 'users',
+      fields: {
+        name: 'string',
+      }
+    },
+  }
+})
 
 const App: React.FC<void> = () => (
   <div className="ms-2" style={{ background: 'white' }}>
@@ -19,7 +45,9 @@ const App: React.FC<void> = () => (
       <DndProvider backend={HTML5Backend}>
         <Router>
           <ConnectedRouter history={history}>
-            <RouteList routes={routes} urlPrefix={settings.urlPrefix} />
+            <ApiProvider client={client}>
+                <RouteList routes={routes} urlPrefix={settings.urlPrefix} />
+            </ApiProvider>
             {/* <Client /> */}
           </ConnectedRouter>
         </Router>
--- app/frontend/modules/admin/modules/client/routes/ClientList/ClientList.tsx
+++ app/frontend/modules/admin/modules/client/routes/ClientList/ClientList.tsx
@@ -1,17 +1,101 @@
-import React, { FC } from 'react'
-import { useParams, useHistory, useLocation } from 'react-router-dom'
-import { Menu } from 'antd'
-import {
-  ShopOutlined,
-} from '@ant-design/icons'
-
-import Breadcrumb from 'modules/admin/modules/campaigns/components/Breadcrumb'
-import settings from 'modules/admin/modules/client/settings'
-
-const { I18n } = window
+import React, { FC, useEffect, useState } from 'react'
+import { useQuery, useMutation, useClient } from 'jsonapi-react'
+import * as t from 'io-ts'
 
 type Props = {}
 
+
+function useResources<R extends {id: string, type: string }>(resourceName, config = {}) {
+  const client = useClient(config)
+  const [data, setData] = useState<R[]>([])
+  const [requests, setRequests] = useState({})
+
+  return {
+    fetch: async (args = {}) => {
+      setRequests({ ...requests, fetch: { state: 'loading'} })
+      const { data: newData, error, errors } = await client.fetch(resourceName, args)
+      const errorData = errors ? errors : (error ? [error] : null)
+      const state = errorData ? 'failed' : 'completed'
+      setRequests({ ...requests, fetch: { state: state, errors: errorData } })
+
+      if (state === 'completed') setData(newData)
+    },
+    addResource: async (details: Partial<R>) => {
+      setRequests({ ...requests, add: { state: 'loading'} })
+      const { data: response, error, errors } = await client.mutate(resourceName, details)
+      const errorData = errors ? errors : (error ? [error] : null)
+      const state = errorData ? 'failed' : 'completed'
+      setRequests({ ...requests, add: { state: state, errors: errorData } })
+
+      if (state === 'completed')  setData([response, ...data])
+    },
+    updateResource:async (details: Partial<R>) => {
+      const { id, ...attributes } = details
+      const requestKey = `update@${id}`
+      setRequests({ ...requests, [requestKey]: { state: 'loading'} })
+      const { data: response, error, errors } = await client.mutate([resourceName, id], attributes)
+      const errorData = errors ? errors : (error ? [error] : null)
+      const state = errorData ? 'failed' : 'completed'
+      setRequests({ ...requests, [requestKey]: { state: state, errors: errorData } })
+
+      if (state === 'completed') setData(data.map(r => r.id === response.id ? response : r))
+    },
+    removeResource: async (id: string) => {
+      const requestKey = `delete@${id}`
+      setRequests({ ...requests, [requestKey]: { state: 'loading'} })
+      const { error,  errors } = await client.delete([resourceName, id])
+      const errorData = errors ? errors : (error ? [error] : null)
+      const state = errorData ? 'failed' : 'completed'
+      setRequests({ ...requests, [requestKey]: { state: state, errors: errorData } })
+
+      if (state === 'completed')  setData(data.filter(r => r.id !== id))
+    },
+    isLoading(action: string, resource_id: null | string = null): boolean {
+      const request = resource_id ? requests[`${action}@${resource_id}`] : requests[action]
+
+      return request ? request.state === 'loading' : false
+    },
+    getErrors(action, resource_id = null) {
+      const request = resource_id ? requests[`${action}@${resource_id}`] : requests[action]
+
+      return request ? request.errors : null
+    },
+    data,
+    setData,
+    requests,
+  }
+}
+
+const ResourceIdentifierTR = t.type({
+  id: t.string,
+  type: t.string,
+})
+
+const ClientTR = t.intersection([
+  ResourceIdentifierTR,
+  t.type({
+    name: t.string,
+    type: t.string,
+    year: t.number,
+    country: t.string,
+  })
+])
+type Client = t.TypeOf<typeof ClientTR>
+
 export const ClientList: FC<Props> = () => {
-  return <div>HI</div>
+  const { data, fetch, updateResource, isLoading, requests } = useResources<Client>('clients')
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
+  return (
+    <div>
+      Hi
+      <button onClick={() => {
+        updateResource({ id: '100', name:  Math.random().toString(36).substring(2, 15) })
+      }}>Update</button>
+      {data?.map((client) => (<div>{client.name}</div>))}
+    </div>
+  )
 }
