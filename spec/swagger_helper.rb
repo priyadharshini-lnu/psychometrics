65cbff7115170c6495e963edc16dfb47fa3d7d82
Rohan Pujari <rohanpujaris@gmail.com>
2022-05-11 10:41:35 +0400

    Fix constant name for swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -17,7 +17,7 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
-    'v1/swagger.json' => Swagger::V1DEFINITION,
-    'v2/swagger.json' => Swagger::V2DEFINITION
+    'v1/swagger.json' => Swagger::V1::DEFINITION,
+    'v2/swagger.json' => Swagger::V2::DEFINITION
   }
 end
