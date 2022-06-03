9bbfaa5bc7a608d3b69f476dc2f2db3d0bad016e
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2022-06-03 14:53:39 +0400

    Storybook config (#2281)
    * Add storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const appConfig = require('../config/webpack/environment')
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+const lessRule = appConfig.loaders.find(loader=> loader.key==='less').value
+const lessGlobalsRule = appConfig.loaders.find(loader=> loader.key==='lessGlobals').value
+  // Make whatever fine-grained changes you need
+  config.module.rules.push(lessRule,lessGlobalsRule)
+
+  // Return the altered config
+  return config
+}
---
+++ stories/Button.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Button as AntButton } from 'antd'
+import '../app/frontend/styles/ant.js'
+
+export const Button = () => <AntButton type="primary">Click me!</AntButton>
