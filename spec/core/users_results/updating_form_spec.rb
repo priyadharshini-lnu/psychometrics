6bab9c3d0753e3aa0f8d2e6d4a0cb5cc67def510
Fedor Tarasov <tarassovf@gmail.com>
2022-06-08 11:54:27 +0400

    Revert "update user results only for in progress (#2264)"
    This reverts commit c037dcc18dc18313353df478a27ed92107f9c479.

--- spec/core/users_results/updating_form_spec.rb
+++ spec/core/users_results/updating_form_spec.rb
@@ -7,22 +7,4 @@ describe ::UsersResults::UpdatingForm do
   it do
     is_expected.to respond_to(:status, :answers, :embedded_data, :norm_id)
   end
-
-  describe 'with user result' do
-    let(:user_result) { create(:users_result) }
-    let(:form) { described_class.new.with_context(user_result: user_result) }
-
-    it 'should be invalid' do
-      expect(form).to be_invalid
-    end
-  end
-
-  describe 'with user result' do
-    let(:user_result) { create(:users_result, status: :in_progress) }
-    let(:form) { described_class.new.with_context(user_result: user_result) }
-
-    it 'should be valid for in progress' do
-      expect(form).to be_valid
-    end
-  end
 end
