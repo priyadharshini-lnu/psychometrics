a5234236392ea14c0f2db11d6e94c165ca12d24e
Fedor Tarasov <tarassovf@gmail.com>
2022-06-14 10:40:36 +0400

    fix user errors on updating profile (#2308)

--- app/core/users/profile_form.rb
+++ app/core/users/profile_form.rb
@@ -7,21 +7,5 @@ module Users
     attribute :password, String
 
     validates :first_name, :last_name, presence: true
-    validates :password, strong_password: true, if: :enable_strong_password?
-    validate :password_length
-
-    def password_length
-      return unless password
-
-      if context.user.password_length.exclude?(password.size)
-        errors.add(:password, :too_short, { count: context.user.password_length.min })
-      end
-    end
-
-    private
-
-    def enable_strong_password?
-      context.user.project.strong_password_enabled || context.user.enforce_strong_password?
-    end
   end
 end
