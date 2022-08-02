8da337c1eaf24cbf77e4a34e2d5aa2dee4089bcc
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2022-08-02 13:01:54 +0400

    fix 360 route for new enduser UI (#2460)

--- app/frontend/modules/endUser/modules/campaigns/routes/CampaignList/Campaigns/Threesixty/Threesixty.tsx
+++ app/frontend/modules/endUser/modules/campaigns/routes/CampaignList/Campaigns/Threesixty/Threesixty.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Col } from 'antd'
 import { Link } from 'react-router-dom'
 
-export const Threesixty = ({ campaign }) => {
+export const Threesixty = ({ campaign }) => (
   <Col className="card" xs={24} sm={12} md={8} lg={6} xl={4}>
     <Link to={`/threesixty_campaigns/${campaign.id}`}>
       This is threesixty campaign
     </Link>
   </Col>
-}
+)
