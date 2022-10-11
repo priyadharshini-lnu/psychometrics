5fd43b950aec317388ca31b46199306572b8cbce
Rohan <rohanpujaris@gmail.com>
2022-10-11 18:28:39 +0400

    Add scaling option for external pdf

--- app/frontend/components/PDFViewer/PDFViewer.tsx
+++ app/frontend/components/PDFViewer/PDFViewer.tsx
@@ -1,3 +1,4 @@
+import { Button, InputNumber } from 'antd'
 import React, { useState, FC } from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 import styles from './styles.less'
@@ -7,10 +8,14 @@ interface Props {
   onLoadingComplete?: () => void
 }
 
+const SCALE_STEP = 0.1
 export const PDFViewer: FC<Props> = ({ fileUrl, onLoadingComplete }) => {
   const [numPages, setNumPages] = useState(1)
+  const [scale, setScale] = useState(1)
+  const [loaded, setLoaded] = useState(false)
 
   const onDocumentLoadSuccess = ({ numPages }) => {
+    setLoaded(true)
     setNumPages(numPages)
   }
 
@@ -18,11 +23,29 @@ export const PDFViewer: FC<Props> = ({ fileUrl, onLoadingComplete }) => {
     if (onLoadingComplete && data.loaded === data.total) onLoadingComplete()
   }
 
+  const incrementScale = () => {
+    setScale(scale => scale + SCALE_STEP)
+  }
+
+  const decrementScale = () => {
+    setScale(scale => scale - SCALE_STEP)
+  }
+
   return (
-    <div>
+    <div className={styles.reportContainer}>
+      <InputNumber
+        value={`${Math.round(scale * 100)} %`}
+        readOnly
+        disabled={!loaded}
+        width={40}
+        className={styles.scaleChanger}
+        addonBefore={<Button type="text" size="small" disabled={scale <= 0.7} onClick={decrementScale}>-</Button>}
+        addonAfter={<Button type="text" size="small" disabled={scale >= 2} onClick={incrementScale}>+</Button>}
+      />
+
       <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess} onLoadProgress={handleLoading} loading="">
         {Array.from(new Array(numPages), (_, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} loading="" className={styles.page} />
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} loading="" className={styles.page} scale={scale} />
         ))}
       </Document>
     </div>
