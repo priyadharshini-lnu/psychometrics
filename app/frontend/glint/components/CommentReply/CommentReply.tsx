ba37c83a590ddb4fd03921840671b76674142cf4
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2022-11-30 20:01:16 +0530

    feat/LH-2474: Add ReplyComponent to glint (#2737)

---
+++ app/frontend/glint/components/CommentReply/CommentReply.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useState } from 'react'
+import {
+  Input, Avatar, Row, Col,
+} from 'antd'
+import { UserOutlined, SendOutlined, LoadingOutlined } from '@ant-design/icons'
+import cs from 'classnames'
+
+import styles from './CommentReply.less'
+
+type Props = {
+avatarUrl?: string,
+onSendReply: (replyText: string) => void,
+inputLoading: boolean,
+}
+
+export const CommentReply: FC<Props> = ({
+  avatarUrl,
+  onSendReply,
+  inputLoading,
+}) => {
+  const [commentText, setCommentText] = useState('')
+  const [hasValidText, setHasValidText] = useState(commentText.trim())
+
+  const avatarIcon = avatarUrl ? null : <UserOutlined />
+
+  const handleTextChange = ({ target }) => {
+    setCommentText(target.value)
+    setHasValidText(target.value.trim())
+  }
+
+  const handleSend = () => {
+    hasValidText && onSendReply(commentText)
+  }
+
+  return (
+    <div>
+      <Row wrap={false}>
+        <Col>
+          <Avatar size={40} icon={avatarIcon} className={styles.avatar} />
+        </Col>
+        <Input
+          onPressEnter={handleSend}
+          value={commentText}
+          onChange={handleTextChange}
+          suffix={inputLoading ? <LoadingOutlined />
+            : (
+              <SendOutlined
+                className={cs({ [styles.sendIcon]: true, [styles.disable]: !hasValidText })}
+                onClick={handleSend}
+              />
+            )}
+          disabled={inputLoading}
+        />
+      </Row>
+    </div>
+  )
+}
---
+++ app/frontend/glint/components/CommentReply/index.tsx
@@ -0,0 +1,1 @@
+export { CommentReply } from './CommentReply'
