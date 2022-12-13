652802ccb26cea69793b149e2adfd9b2d70a5070
Rohan <rohanpujaris@gmail.com>
2022-12-13 17:35:02 +0400

    Fix duplicate user in user search api

--- app/models/concerns/user_scopes.rb
+++ app/models/concerns/user_scopes.rb
@@ -46,13 +46,13 @@ module UserScopes
     scope :with_access_to_campaign, lambda { |campaign_id|
       campaign = Campaign.find(campaign_id)
       client_ids = [campaign.client.id, campaign.project_id]
-      left_joins(:memberships).where(
+      joins(:memberships).where(
         %{
           (memberships.client_id IN (:client_ids) AND memberships.campaign_id IS NULL)
           OR memberships.campaign_id = :campaign_id OR users.role = :role
         },
         client_ids: client_ids, campaign_id: campaign.id, role: User::SUPER_ADMIN_ROLE
-      )
+      ).distinct
     }
 
     # Fileter by role
