c3212da27271278bbf6c27bcb8595466be0e21e7
Fedor Tarasov <tarassovf@gmail.com>
2023-01-16 13:18:06 +0400

    Add admin devise layout (#2844)

--- app/controllers/administration/administrator/invitations_controller.rb
+++ app/controllers/administration/administrator/invitations_controller.rb
@@ -4,6 +4,7 @@ module Administration
   module Administrator
     class InvitationsController < Devise::InvitationsController
       helper_method :resource_name, :devise_mapping
+      layout 'administration/devise'
 
       def resource_name
         :user
