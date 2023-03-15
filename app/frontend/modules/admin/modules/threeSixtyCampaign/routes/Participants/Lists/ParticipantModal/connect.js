2bd99179b459504034870f8efdb88550ea36190b
Namrata Ukirde <namrata.ukirde@thetalententerprise.com>
2023-03-15 17:07:09 +0530

    Bug/LH-2695 fix delete result icon visibility (#3039)

--- app/frontend/modules/admin/modules/threeSixtyCampaign/routes/Participants/Lists/ParticipantModal/connect.js
+++ app/frontend/modules/admin/modules/threeSixtyCampaign/routes/Participants/Lists/ParticipantModal/connect.js
@@ -7,7 +7,7 @@ export default connect(
   state => ({
     ...getData(state).ParticipantModal,
     current: getCurrent(state),
-    permissions: state.currentUser.permissions,
+    permissions: state.threeSixtyCampaign.subjects.permissions,
   }),
   { fetchParticipants, fetchRelationships, closeModal },
 )
