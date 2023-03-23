c1a3e451e665b98570ca97fe8567d00a69e40301
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2023-03-23 14:01:38 +0400

    chore/LH-2707: upgrade froala editor to latest version (#3083)

--- app/frontend/components/EmailEditor/index.tsx
+++ app/frontend/components/EmailEditor/index.tsx
@@ -74,7 +74,7 @@ export const EmailEditor: React.FC<Props> = ({
     saveParams: { type, details },
     heightMin: 250,
     heightMax: 500,
-    key: '7MD3aC3A2C4B4D4A2xROKLJKYHROLDXDRE1b1YYGRi1Bd1C4F4B3H3G3A15A13A12C4C4==',
+    key: 'DUA2yE2C2F1A6A3A2A3qYFd1UQRFQIVb1MSMc2IWPNe1IFg1yD4C3D2C1C4C1H1H4B1D2==',
     attribution: false,
     fontFamily: {
       'Arial,Helvetica,sans-serif': 'Arial',
