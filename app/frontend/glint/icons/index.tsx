230a81370e682f20304d11550119c63d0030ec6e
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2023-05-19 14:04:12 +0530

    feat/LH-2727: Add ColorPicker component to glint (#3203)
    * feat/LH-2727: Add ColorPicker component to glint

--- app/frontend/glint/icons/index.tsx
+++ app/frontend/glint/icons/index.tsx
@@ -1,3 +1,4 @@
 export { CampaignIcon } from './CampaignIcon'
 export { LanguageIcon } from './LanguageIcon'
 export { MenuTriggerIcon } from './MenuTriggerIcon'
+export { EyeDropperIcon } from './EyeDropperIcon'
