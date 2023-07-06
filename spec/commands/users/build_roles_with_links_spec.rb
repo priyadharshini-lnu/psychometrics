fdc8a060588d851f64d97c4398b30b5fcd2a9a51
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2023-07-06 13:03:34 +0530

    fix test

--- spec/commands/users/build_roles_with_links_spec.rb
+++ spec/commands/users/build_roles_with_links_spec.rb
@@ -30,7 +30,7 @@ describe Users::BuildRolesWithLinks do
             { :name => client.name, :value => "/administration/clients/#{client.id}/projects" },
             { :name => project.name, :value => "/administration/projects/#{project.id}/new_campaigns" },
             {
-              :name => campaign.name, :value => "/administration/projects/#{project.id}/new_campaigns/#{campaign.id}"
+              :name => campaign.name, :value => "/administration/projects/#{project.id}/new_campaigns/#{campaign.id}/admins" # rubocop:disable Layout/LineLength
             }
           ] }
       ]
@@ -62,7 +62,7 @@ describe Users::BuildRolesWithLinks do
     it do
       roles = described_class.call!(regular)
 
-      expect(roles.first[:paths].last[:value]).to eq("/administration/clients/#{campaign.project.client.id}/projects/#{campaign.project.id}/threesixty_campaigns/#{threesixty_campaign.id}") # rubocop:disable Layout/LineLength
+      expect(roles.first[:paths].last[:value]).to eq("/administration/clients/#{campaign.project.client.id}/projects/#{campaign.project.id}/threesixty_campaigns/#{threesixty_campaign.id}/admins") # rubocop:disable Layout/LineLength
     end
   end
 
