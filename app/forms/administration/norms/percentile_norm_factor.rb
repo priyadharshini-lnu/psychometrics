9f5bbce0a750a2bb5414f8ccf0ca3ce4737fb6dc
Shuja Shabandri <shuja@shabandri.com>
2023-07-11 19:00:47 +0400

    fix: norm values can be less than 1 (#3375)

--- app/forms/administration/norms/percentile_norm_factor.rb
+++ app/forms/administration/norms/percentile_norm_factor.rb
@@ -12,7 +12,7 @@ module Administration
       validates :norm_id, :factor_id, :norm_type, :field_name, presence: true
 
       validates :field_name, inclusion: { in: %w[mean standard_deviation] }
-      validates :field_value, numericality: { greater_than_or_equal_to: 1 }, allow_nil: true
+      validates :field_value, numericality: { greater_than: 0 }, allow_nil: true
     end
   end
 end
