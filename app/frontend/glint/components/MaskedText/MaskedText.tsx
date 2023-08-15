ed62064bb22dddf4691c12ba28014f8ba4938c2a
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2023-08-15 21:32:21 +0530

    LH-2960: Copy issue on view icon click in masked text (#3426)

--- app/frontend/glint/components/MaskedText/MaskedText.tsx
+++ app/frontend/glint/components/MaskedText/MaskedText.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons'
-import { Typography } from 'antd'
+import { Tooltip, Typography } from 'antd'
 
 type MaskedTextProps = {
   text: string,
@@ -12,29 +12,22 @@ export const MaskedText: React.FC<MaskedTextProps> = ({
   text, maskedTextPlaceholder = '************************************',
 }) => {
   const [view, setView] = useState(false)
-  const icon = view ? [
-    <EyeOutlined data-testid="masked-text-view" key="copy-icon" />,
-    <EyeOutlined data-testid="masked-text-hide" key="copied-icon" />,
-  ] : [
-    <EyeInvisibleOutlined data-testid="masked-text-view" key="copy-icon" />,
-    <EyeInvisibleOutlined data-testid="masked-text-hide" key="copied-icon" />,
-  ]
-  const tooltips = view ? [I18n.t('common.tooltips.hide'), I18n.t('common.tooltips.hide')]
-    : [I18n.t('common.tooltips.view'), I18n.t('common.tooltips.view')]
+  const icon = view ? <EyeOutlined data-testid="masked-text-view" key="copy-icon" />
+    : <EyeInvisibleOutlined data-testid="masked-text-view" key="copy-icon" />
+  const tooltips = view ? I18n.t('common.tooltips.hide') : I18n.t('common.tooltips.view')
   return (
-    <Typography.Text
-      copyable={{
-        icon,
-        tooltips,
-        onCopy: () => setView(!view),
-      }}
-    >
+    <Typography.Text onClick={() => setView(!view)}>
       <Typography.Text copyable={{
         text,
       }}
       >
         {view ? text : maskedTextPlaceholder}
       </Typography.Text>
+      <Tooltip title={tooltips}>
+        <span className="ant-typography-copy">
+          {icon}
+        </span>
+      </Tooltip>
     </Typography.Text>
   )
 }
