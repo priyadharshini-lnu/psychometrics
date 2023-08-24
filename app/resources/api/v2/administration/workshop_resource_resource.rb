0a8be7a73af583d4d3359e4275fb990598610319
Eugene Volkovich <eugene@thetalententerprise.com>
2023-08-24 17:37:18 +0300

    LH-2869 Fix WorkshopResource creation (#3489)

--- app/resources/api/v2/administration/workshop_resource_resource.rb
+++ app/resources/api/v2/administration/workshop_resource_resource.rb
@@ -3,6 +3,8 @@
 class Api::V2::Administration::WorkshopResourceResource < Api::V2::Administration::BaseResource
   attributes :name, :url, :workshop_id
 
+  has_one :workshop
+
   def self.records(opts = {})
     super(opts).where(workshop_id: opts[:context][:params]['workshop_id'])
   end
