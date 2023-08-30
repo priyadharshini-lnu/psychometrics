09d83f14798690af277e6ef48fb1726ace35647a
Shuja Shabandri <shuja@shabandri.com>
2023-08-31 00:11:29 +0400

    change workshop banner (#3564)

--- app/frontend/modules/endUser/modules/campaigns/routes/Workshop/MeetingLink/MeetingLink.tsx
+++ app/frontend/modules/endUser/modules/campaigns/routes/Workshop/MeetingLink/MeetingLink.tsx
@@ -1,4 +1,4 @@
-import waves from './waves.png'
+import { Button } from 'antd'
 import styles from './styles.less'
 
 const { I18n } = window
@@ -9,11 +9,11 @@ interface Props {
 
 export const MeetingLink: React.FC<Props> = ({ meetingLink }) => (
   <div className={styles.meetingLinkContainer}>
-    <img className={styles.meetingLinkImg} src={waves} />
+    <div className={styles.meetingLinkBg} />
     <div className={styles.meetingLinkAnchor}>
-      <a href={meetingLink} target="_blank" rel="noreferrer" className={styles.meetingLink}>
+      <Button type="link" className={styles.meetingLink} href={meetingLink} target="_blank">
         {I18n.t('campaign.workshops.click_here')}
-      </a>
+      </Button>
     </div>
   </div>
 )
