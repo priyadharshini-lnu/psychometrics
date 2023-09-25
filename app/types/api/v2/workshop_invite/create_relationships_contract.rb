c28e4658484d36a9567eec8588291a9982280eb4
Eugene Volkovich <eugene@thetalententerprise.com>
2023-09-25 17:47:45 +0300

    LH-3031 WorkshopInvite add proper error message (#3584)
    * LH-3031 WorkshopInvite add proper error message
    
    * Add base presence check
    
    * Validate on backend

--- app/types/api/v2/workshop_invite/create_relationships_contract.rb
+++ app/types/api/v2/workshop_invite/create_relationships_contract.rb
@@ -11,6 +11,14 @@ module Api
         rule(:data) do
           workshop_invite = ::WorkshopInvite.find(_context[:params][:workshop_invite_id])
 
+          if value.count.zero?
+            key.failure(:filled?)
+          end
+
+          if workshop_invite.workshop_ids.intersection(result[:data].collect { |e| e[:id].to_i }).present?
+            key.failure(:already_invited)
+          end
+
           if workshop_invite.workshops.count + value.count > ::WorkshopInvite::RESTRICTED_ASSESSMENT_CENTERS
             key.failure(:exceeded_workshops_count, { count: ::WorkshopInvite::RESTRICTED_ASSESSMENT_CENTERS })
           end
