93831db0dccbd0da69c7b3be1bcfb25f4324a730
Rohan Pujari <rohanpujaris@gmail.com>
2023-10-06 12:02:11 +0400

    Load custom rubocop rule only when Rubocp is loaded (#3689)

--- lib/custom_rubocops/avoid_direct_use_of_mail_method.rb
+++ lib/custom_rubocops/avoid_direct_use_of_mail_method.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
-module CustomRubocops
-  class AvoidDirectUseOfMailMethod < RuboCop::Cop::Cop
-    MSG = "Use send_email method instead. It won't send emails to user who shouldn't receive communication"
-    RESTRICT_ON_SEND = %i[mail].freeze
+if Object.const_defined?('RuboCop::Cop::Cop')
+  module CustomRubocops
+    class AvoidDirectUseOfMailMethod < RuboCop::Cop::Cop
+      MSG = "Use send_email method instead. It won't send emails to user who shouldn't receive communication"
+      RESTRICT_ON_SEND = %i[mail].freeze
 
-    def on_send(node)
-      add_offense(node)
+      def on_send(node)
+        add_offense(node)
+      end
     end
   end
+else
+  # rubocop:disable Lint/EmptyClass
+  module CustomRubocops
+    class AvoidDirectUseOfMailMethod
+    end
+  end
+  # rubocop:enable Lint/EmptyClass
 end
