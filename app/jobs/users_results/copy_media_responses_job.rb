0a0cd95bd97ccfab87d247e910e2f1d915820945
Rohan Pujari <rohanpujaris@gmail.com>
2023-10-30 12:46:48 +0400

    Fix media response not getting copied while importing result (#3790)

--- app/jobs/users_results/copy_media_responses_job.rb
+++ app/jobs/users_results/copy_media_responses_job.rb
@@ -4,30 +4,8 @@ module UsersResults
   class CopyMediaResponsesJob < ApplicationJob
     def perform(from_user_result, to_user_result)
       from_user_result.media_responses.each do |media_response|
-        copy_single_media_response(media_response, to_user_result)
+        UsersResults::CopyMediaResponseJob.perform_later(media_response, to_user_result)
       end
-    end
-
-    def copy_single_media_response(media_response, to_user_result)
-      filename = "#{SecureRandom.uuid}/#{media_response.filename}"
-
-      begin
-        bucket = Rails.application.secrets.s3_compatible_storage[:private_bucket]
-        Aws::S3::Client.new.copy_object(
-          bucket: bucket,
-          copy_source: "#{bucket}/#{media_response.asset.path}",
-          key: "uploads/media_response/asset/#{filename}",
-          acl: media_response.asset.acl
-        )
-      rescue StandardError => e
-        Rails.logger.error("Unable to copy media_response with id #{media_response.id}", e.message)
-        return
-      end
-
-      new_media_response = media_response.dup
-      new_media_response.users_result_id = to_user_result.id
-      new_media_response.save!
-      new_media_response.update_column(:asset, filename)
     end
   end
 end
