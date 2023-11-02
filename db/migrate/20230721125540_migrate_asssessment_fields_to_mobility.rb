b4ea137ac3f655f81691c0a756d42853f434ebf3
Shuja Shabandri <shuja@shabandri.com>
2023-11-02 11:44:51 +0400

    convert assessment mobility migration to SQL (#3809)

--- db/migrate/20230721125540_migrate_asssessment_fields_to_mobility.rb
+++ db/migrate/20230721125540_migrate_asssessment_fields_to_mobility.rb
@@ -1,15 +1,26 @@
-class Assessment < ApplicationRecord
-  extend Mobility
-end
+# frozen_string_literal: true
 
 class MigrateAsssessmentFieldsToMobility < ActiveRecord::Migration[7.0]
-  def change
-    Assessment.find_each do |t|
-      attrs = t.attributes
-      t.name = attrs['name']
-      t.description = attrs['description']
-      t.timing = attrs['timing']
-      t.save!(validate: false)
+  def up
+    assessments = execute(%(
+      SELECT * FROM assessments
+    ))
+    execute('BEGIN')
+    assessments.each do |assessment|
+      exec_query(%(
+        INSERT INTO assessment_translations (name, description, timing, locale, assessment_id, created_at, updated_at)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+      ), 'SQL', [
+        assessment['name'], assessment['description'], assessment['timing'], 'en',
+        assessment['id'], Time.zone.now, Time.zone.now
+      ])
     end
+    execute('COMMIT')
+  end
+
+  def down
+    execute(%(
+      DELETE FROM assessment_translations
+    ))
   end
 end
