7f9abc0525f829f2c9fbca695fbfe58b73312b8e
Ayza Hamid <ayza@thetalententerprise.com>
2023-11-24 17:35:33 +0500

    LH-3243:Fixed short campaign schema (#3861)
    * Fixed short campaign schema
    
    * Handled data types for completion percentage

--- app/serializers/end_user/short_campaign_schema.rb
+++ app/serializers/end_user/short_campaign_schema.rb
@@ -10,11 +10,11 @@ module EndUser
         required(:name).filled(:string)
         required(:type).filled(:string)
         required(:status).filled(:string)
-        required(:completion_percentage).filled(:float)
+        required(:completion_percentage) { int? | float? }
         required(:progress_status).filled(:string)
         required(:user_reports_available).filled(:bool)
         required(:description).maybe(:string)
-        required(:timing).maybe(:string)
+        required(:timing).maybe(:int?)
         required(:scheduled_at).maybe(:string)
         required(:scheduled_in).maybe(:float)
       end
