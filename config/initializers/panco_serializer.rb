c7b24c970a3bc35e57da60ee205aa8e54be0df10
Rohan Pujari <rohanpujaris@gmail.com>
2023-12-21 18:26:22 +0400

    Auto delegate methods to model for panko serializer (#3923)
    * Auto delegate methods to model for panko serializer
    
    * Fix for places where conditional attributes are declared

--- config/initializers/panco_serializer.rb
+++ config/initializers/panco_serializer.rb
@@ -6,6 +6,25 @@ require 'panko_override/array_serializer'
 module Panko
   class Serializer
     include PankoOverride::Serializer
+
+    class << self
+      alias old_inherited inherited
+
+      def inherited(subclass)
+        old_inherited(subclass)
+
+        subclass.instance_eval do
+          alias :old_attributes :attributes
+
+          def attributes(*args)
+            old_attributes(*args)
+            declared_attributes = args.select { |a| a.is_a?(Symbol) || a.is_a?(String) }
+
+            delegate(*(declared_attributes - new.public_methods), to: :object)
+          end
+        end
+      end
+    end
   end
 
   class ArraySerializer
