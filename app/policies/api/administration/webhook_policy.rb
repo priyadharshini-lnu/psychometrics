fa325fd2f2a89062707c5c8ad1521183eb9ab6f6
Shuja Shabandri <shuja@shabandri.com>
2024-02-16 15:01:58 +0400

    Fix webhook permissions (#4110)

--- app/policies/api/administration/webhook_policy.rb
+++ app/policies/api/administration/webhook_policy.rb
@@ -30,11 +30,11 @@ module Api
           return scope if user.is?(:superadmin)
 
           permitted_client_admin_project_ids = @user.client_admin_project_ids.select do |project_id|
-            @user.has_permission?(:campaigns, :view, project_id: project_id)
+            @user.has_permission?(:project_settings, :webhooks, project_id: project_id)
           end
 
           permitted_project_admin_project_ids = @user.project_admin_client_ids.select do |project_id|
-            @user.has_permission?(:campaigns, :view, project_id: project_id)
+            @user.has_permission?(:project_settings, :webhooks, project_id: project_id)
           end
 
           scope.where(project_id: (permitted_client_admin_project_ids + permitted_project_admin_project_ids))
