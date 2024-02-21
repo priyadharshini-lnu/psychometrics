468717c01f57f3fff68ffeaf2ee5c3c2f2e7d093
Ayza Hamid <ayza@thetalententerprise.com>
2024-02-21 12:58:35 +0500

    LH 3381 - Missing key validation  (#4128)
    * Whitelist user result update schema
    
    * Updated key validation exception message
    
    * Fix rspec

--- app/serializers/base_schema.rb
+++ app/serializers/base_schema.rb
@@ -9,7 +9,7 @@ class BaseSchema
 
       unless existing_schema.config.validate_keys
         raise PankoOverride::Exceptions::KeyValidationMissing,
-              "Schema class '#{name}' does not have 'config.validate_keys' set to true"
+              "Schema class '#{name}' does not have 'config.validate_keys' set to true. Please set it to true or whitelist the schema in whitelisted_schemas method" # rubocop:disable Layout/LineLength
       end
       existing_schema
     end
@@ -43,6 +43,7 @@ class BaseSchema
      Assessments::Actions::Block::CreateByTemplate::QuestionSchema,
      Assessments::Actions::Block::CreateByTemplate::BlockSchema,
      Assessments::Actions::Question::CreateByTemplate::QuestionSchema,
-     Assessments::QuestionSchema, Assessments::AssessmentSchema, Assessments::BlockSchema, Assessments::FactorSchema]
+     Assessments::QuestionSchema, Assessments::AssessmentSchema, Assessments::BlockSchema, Assessments::FactorSchema,
+     UsersResultUpdateSchema]
   end
 end
