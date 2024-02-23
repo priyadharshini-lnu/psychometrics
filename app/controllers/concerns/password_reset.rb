8eff15fcd33d37f3d60a1ac62046358c0cd8d25f
Ayza Hamid <ayza@thetalententerprise.com>
2024-02-23 13:38:06 +0500

    Add flash message for invalid email (#4149)

--- app/controllers/concerns/password_reset.rb
+++ app/controllers/concerns/password_reset.rb
@@ -14,17 +14,21 @@ module PasswordReset
 
   def create
     @form = Users::PasswordResetForm.from_params(params[:user])
-    return render 'shared/password_reset' unless @form.valid?
 
-    project = GetProjectBySubdomain.call!(request.subdomain)
-    user = User.find_by(email: @form.email, project: project&.id)
-    if user
-      resource_class.send_reset_password_instructions(user)
-      audit! :request_change_password, user
+    if @form.valid?
+      project = GetProjectBySubdomain.call!(request.subdomain)
+      user = User.find_by(email: @form.email, project: project&.id)
+      if user
+        resource_class.send_reset_password_instructions(user)
+        audit! :request_change_password, user
+      end
+      set_flash_message! :notice, :send_instructions
+
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      set_flash_message! :alert, :invalid_email
+      redirect_to new_administration_password_path
     end
-    set_flash_message! :notice, :send_instructions
-
-    respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
   end
 
   private
