47a33dd5489b9c247903468ec01a620b4316f849
Rohan Pujari <rohanpujaris@gmail.com>
2024-02-27 16:06:37 +0400

    Fix Individual dashboard for user not working (#4178)
    * Fix Individual dashboard for user not working
    
    * Remove not required relationship

--- app/serializers/administration/individual_dashboard_serializer.rb
+++ app/serializers/administration/individual_dashboard_serializer.rb
@@ -2,10 +2,8 @@
 
 module Administration
   class IndividualDashboardSerializer < ::UserReportSerializer
-    has_one :user, method: :user_with_all_fields
-
-    def user_with_all_fields
-      UserWithAllFieldsSerializer.new(object.user)
+    def user
+      UserWithAllFieldsSerializer.new(object.user).to_h
     end
   end
 end
