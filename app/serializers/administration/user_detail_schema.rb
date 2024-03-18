b9f2e9b11948988ee5166608be8e6efb18b12682
Ayza Hamid <ayza@thetalententerprise.com>
2024-03-18 11:29:11 +0500

    Schema fixes (#4259)

--- app/serializers/administration/user_detail_schema.rb
+++ app/serializers/administration/user_detail_schema.rb
@@ -48,7 +48,7 @@ module Administration
               required(:proctoring_trial).filled(:bool?)
               required(:workshop_booking_requires_prework_completion).filled(:bool?)
               required(:campaign_scoring_variables).maybe(:str?)
-              optional(:proctoring_type).maybe(:int?)
+              optional(:proctoring_type).maybe(:str?)
             end
           end
         end
