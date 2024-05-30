abb3ec713caca16e750e2e7a41a57a131eab2141
Ayza Hamid <ayza@thetalententerprise.com>
2024-05-30 11:55:35 +0500

    LH-1940 Added validations on date fields (#4578)
    * Added validations on date fields
    
    * Added time range

--- app/frontend/modules/admin/modules/campaigns/routes/Campaign/routes/AssessmentsReports/routes/AssessmentsReports/BulkDownloadModal/BulkDownloadModal.tsx
+++ app/frontend/modules/admin/modules/campaigns/routes/Campaign/routes/AssessmentsReports/routes/AssessmentsReports/BulkDownloadModal/BulkDownloadModal.tsx
@@ -8,6 +8,8 @@ interface Props {
   onDownload: (startDate?: Date, endDate?: Date) => void;
 }
 
+const format = 'YYYY-MM-DD HH:mm'
+
 const BulkDownloadModal: React.FC<Props> = ({ visible, onCancel, onDownload }) => {
   const [startDate, setStartDate] = useState<Date | null>(null)
   const [endDate, setEndDate] = useState<Date | null>(null)
@@ -25,6 +27,20 @@ const BulkDownloadModal: React.FC<Props> = ({ visible, onCancel, onDownload }) =
   const resetDates = () => {
     setStartDate(null)
     setEndDate(null)
+  }
+
+  const disabledStartDate = (current: dayjs.Dayjs): boolean => {
+    const today = dayjs().endOf('day')
+    if (current > today) return true
+    if (endDate && current > dayjs(endDate)) return true
+    return false
+  }
+
+  const disabledEndDate = (current: dayjs.Dayjs): boolean => {
+    const today = dayjs().endOf('day')
+    if (current > today) return true
+    if (startDate && current < dayjs(startDate)) return true
+    return false
   }
 
   return (
@@ -46,12 +62,18 @@ const BulkDownloadModal: React.FC<Props> = ({ visible, onCancel, onDownload }) =
         onChange={date => setStartDate(date ? date.toDate() : null)}
         placeholder="Start Date"
         style={{ marginBottom: '16px', width: '100%' }}
+        disabledDate={disabledStartDate}
+        showTime
+        format={format}
       />
       <DatePicker
         value={endDate ? dayjs(endDate) : null}
         onChange={date => setEndDate(date ? date.toDate() : null)}
         placeholder="End Date"
         style={{ width: '100%' }}
+        disabledDate={disabledEndDate}
+        showTime
+        format={format}
       />
     </Modal>
   )
