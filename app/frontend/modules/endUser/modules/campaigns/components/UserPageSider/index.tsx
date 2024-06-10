0774de1d9e090d03c2b80605d41ab864d8f00050
Shuja Shabandri <shuja@shabandri.com>
2024-06-10 12:31:30 +0400

    reverting the branding (requested by PS)

--- app/frontend/modules/endUser/modules/campaigns/components/UserPageSider/index.tsx
+++ app/frontend/modules/endUser/modules/campaigns/components/UserPageSider/index.tsx
@@ -10,7 +10,7 @@ import {
 } from '@ant-design/icons'
 
 
-import lighthouseLogo from '~/assets/lighthouseLogoTall.png'
+import lighthouseLogo from '~/modules/endUser/assets/images/lighthouseLogoWide.png'
 import { history } from '~/modules/endUser/store'
 import { RootState } from '~/modules/endUser/core/rootReducers'
 import {
