85e4b2f5a09199e11cbc58d306e3f249f779effe
Fedor Tarasov <tarassovf@gmail.com>
2024-06-26 11:11:39 +0400

    LH-3583, LH-3671: add rack middleware, added check for threesixty and assessor (#4617)
    * LH-3583, LH-3671: add rack middleware, added check for threesixty and assessor
    
    * fix timeout
    
    * rename
    
    * fix lint
    
    * review updates

---
+++ app/frontend/hooks/useAvoidMultipleEvaluation.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+const useAvoidMultipleEvaluation = (assessmentId, result, validateSession) => {
+  const [showInvalidSession, setShowInvalidSession] = useState(false)
+
+  useEffect(() => {
+    if (!result?.id) { return }
+
+    const evaluationSessionIdCheckChannel = new BroadcastChannel('evaluationSessionIdCheck')
+    const evaluationSessionId = result?.evaluation_session_id
+
+    let interval
+    if (result.id && evaluationSessionId) {
+      interval = setInterval(() => {
+        validateSession(assessmentId, evaluationSessionId)
+          .then(({ response }: {response: { evaluationSessionExistis: string }}) => {
+            if (!response.evaluationSessionExistis) {
+              setShowInvalidSession(true)
+              clearInterval(interval)
+              evaluationSessionIdCheckChannel.close()
+            }
+          })
+      }, 30000)
+
+      evaluationSessionIdCheckChannel.addEventListener('message', (e) => {
+        if (e.data.type === 'DuplicateEvaluationSessionIdFound' && e.data.evaluationSessionId === evaluationSessionId) {
+          location.reload()
+        }
+
+        if (e.data.type === 'CheckDuplicateEvaluationSessionId') {
+          if (e.data.evaluationSessionId !== evaluationSessionId) return
+          evaluationSessionIdCheckChannel.postMessage({
+            type: 'DuplicateEvaluationSessionIdFound', evaluationSessionId,
+          })
+          setShowInvalidSession(true)
+          interval && clearInterval(interval)
+        }
+      })
+      evaluationSessionIdCheckChannel.postMessage({ type: 'CheckDuplicateEvaluationSessionId', evaluationSessionId })
+    }
+    return () => {
+      interval && clearInterval(interval)
+      evaluationSessionIdCheckChannel && evaluationSessionIdCheckChannel.close()
+    }
+  }, [result])
+
+  return [showInvalidSession, setShowInvalidSession] as [boolean, (val: boolean) => void]
+}
+
+export default useAvoidMultipleEvaluation
