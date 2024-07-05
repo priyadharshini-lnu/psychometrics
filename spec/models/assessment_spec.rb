493184ff2379666deaa2dcd462c03139c1591d59
Shaiju Edakulangara <162425744+shaiju-tte@users.noreply.github.com>
2024-07-05 17:15:24 +0530

    LH-3753: Rescue IIHT API error when trying to find external assessment name (#4718)
    * Rescue IIHT API error when trying to find external assessment name
    
    * Return empty array when IIHT API throws error
    
    * Add Faraday::Response::RaiseError

--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -19,4 +19,77 @@ RSpec.describe Assessment, type: :model do
       expect(assessment.saville?).to eq(false)
     end
   end
+
+  describe '#external_assessment_name' do
+    context 'when assessment type is common' do
+      it 'returns nil' do
+        assessment = build(:assessment)
+
+        expect(assessment.external_assessment_name).to be_nil
+      end
+    end
+
+    context 'when external_assessment_id is nil' do
+      it 'returns nil' do
+        assessment = build(:assessment, type: Assessment::TYPES[:saville])
+
+        expect(assessment.external_assessment_name).to be_nil
+      end
+    end
+
+    context 'when assessment type is hogan' do
+      before do
+        allow(Settings.providers.hogan.assessments).to receive(:find).and_return(Struct.new(:name).new(
+                                                                                   'HPI'
+                                                                                 ))
+      end
+
+      let(:hogan_assessment) { create(:hogan_assessment) }
+
+      it 'returns the assessment name' do
+        expect(hogan_assessment.external_assessment_name).to eq('HPI')
+      end
+    end
+
+    context 'when assessment type is saville' do
+      before do
+        allow(Settings.providers.saville.assessments).to receive(:find).
+          and_return(Struct.new(:name).new('Abstract Reasoning Aptitude-Rx (IA)'))
+      end
+
+      let(:saville_assessment) { create(:assessment, :saville) }
+
+      it 'returns the assessment name' do
+        expect(saville_assessment.external_assessment_name).to eq('Abstract Reasoning Aptitude-Rx (IA)')
+      end
+    end
+
+    context 'when assessment type is pearson' do
+      let(:pearson_assessment) { create(:assessment, :pearson) }
+
+      before do
+        create(:pearson_assessment, product_id: pearson_assessment.external_assessment_id, title: 'Pearson Assessment')
+      end
+
+      it 'returns the assessment title' do
+        expect(pearson_assessment.external_assessment_name).to eq('Pearson Assessment')
+      end
+    end
+
+    context 'when assessment type is iiht' do
+      let(:iiht_assessment) { create(:assessment, :iiht) }
+
+      before do
+        allow(Iiht::GetAssessments).to receive(:call!).
+          and_return([
+            { 'assessmentIdNumber' => iiht_assessment.external_assessment_id,
+              'name' => 'assessment_name' }
+          ])
+      end
+
+      it 'returns the assessment name' do
+        expect(iiht_assessment.external_assessment_name).to eq('assessment_name')
+      end
+    end
+  end
 end
