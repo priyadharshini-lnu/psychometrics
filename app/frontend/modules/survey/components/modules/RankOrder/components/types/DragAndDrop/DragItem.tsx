cec344d9e1bf632d8fe6c603a62e95806665faaf
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2024-07-11 17:11:08 +0530

    LH-3660: make rank order question keyboard accessible (#4738)

---
+++ app/frontend/modules/survey/components/modules/RankOrder/components/types/DragAndDrop/DragItem.tsx
@@ -0,0 +1,78 @@
+import React, {
+  FC, CSSProperties, RefObject,
+} from 'react'
+import { Button } from 'antd'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { DraggableSyntheticListeners } from '@dnd-kit/core'
+import styles from './DragAndDrop.less'
+
+import DescriptionPreview from '../../DescriptionPreview'
+
+type CommonProps = {
+  text: string,
+  number: number,
+  showDescription: boolean,
+  description: string,
+}
+
+export type Props = CommonProps & {
+  style: CSSProperties,
+  dragStyle: CSSProperties,
+  attributes: React.HTMLAttributes<HTMLElement>,
+  listeners: DraggableSyntheticListeners,
+}
+
+export const DragItem = React.forwardRef(
+  ({
+    showDescription, text, number, description, style, dragStyle, attributes, listeners,
+  }: Props, ref:RefObject<HTMLDivElement>) => (
+    <div ref={ref} className={styles.item} style={{ ...style, ...dragStyle }}>
+      <Button type="text" className={`fa fa-arrows-v ${styles.icon}`} {...attributes} {...listeners} />
+      <div className={styles.number}>{number}</div>
+      <div>
+        <div className={styles.text}>{text}</div>
+        {showDescription && <DescriptionPreview description={description} />}
+      </div>
+    </div>
+  ),
+)
+
+type FactorSortableProps = CommonProps & {
+  sortId: string
+}
+
+export const DragItemSortable:FC<FactorSortableProps> = ({
+  text, sortId, number, showDescription, description,
+}) => {
+  const {
+    attributes, listeners, setNodeRef, transform, transition, isDragging,
+  } = useSortable({
+    id: sortId,
+    data: {
+      type: 'container',
+    },
+  })
+
+  const dragStyle = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+    zIndex: isDragging ? 20 : undefined,
+  } as CSSProperties
+
+  const containerStyle: CSSProperties = { opacity: isDragging ? '0.5' : undefined }
+
+  return (
+    <DragItem
+      ref={setNodeRef}
+      attributes={attributes}
+      listeners={listeners}
+      dragStyle={dragStyle}
+      style={containerStyle}
+      text={text}
+      number={number}
+      showDescription={showDescription}
+      description={description}
+    />
+  )
+}
