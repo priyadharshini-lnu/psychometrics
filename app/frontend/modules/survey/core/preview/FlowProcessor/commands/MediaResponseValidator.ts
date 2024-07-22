236ade20da20a47b937c6a52e17eb88f6d200046
Fedor Tarasov <tarassovf@gmail.com>
2024-07-22 18:00:47 +0400

    Hotfix/file uploading validaton (#4777)
    * Hotfix: fix file uploadign validation
    
    * fix media responses

--- app/frontend/modules/survey/core/preview/FlowProcessor/commands/MediaResponseValidator.ts
+++ app/frontend/modules/survey/core/preview/FlowProcessor/commands/MediaResponseValidator.ts
@@ -8,9 +8,10 @@ const { I18n } = window
 const MediaResponseValidator = {
   run (question: Question, mediaResponses: MediaResponse[]) {
     if (isMandatory(question)) {
-      const mediaResponse = mediaResponses.length > 1
-        ? _.find(mediaResponses, mr => (mr.questionId === question.id && mr.userSelected))
-        : mediaResponses[0]
+      const questionResponses = mediaResponses.filter(mr => mr.questionId === question.id)
+      const mediaResponse = questionResponses.length > 1
+        ? _.find(questionResponses, mr => (mr.userSelected))
+        : questionResponses[0]
       if (!mediaResponse || !mediaResponse?.url) {
         const validationMessage = I18n.lookup(`${getValidationKey(question)}.required`)
           || I18n.t('validations.required')
