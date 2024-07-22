278f764ba19f0efca015a7be9928f3037ba231f6
Namrata Ukirde <namrata.ukirde@thetalententerprise.com>
2024-07-22 08:59:30 +0530

    Hotfix/fix 360 campaign admin result deletion (#4772)
    * fix 360 admin result deletion
    
    * fix action menu on evaluators screen
    
    * fix campaign admin evaluators actions menu permissions
    
    * fix subject list action menu

--- app/frontend/modules/admin/modules/threeSixtyCampaign/routes/Participants/Lists/SubjectList/SubjectList.jsx
+++ app/frontend/modules/admin/modules/threeSixtyCampaign/routes/Participants/Lists/SubjectList/SubjectList.jsx
@@ -3,12 +3,11 @@ import _ from 'lodash'
 import {
   Table, Row, Col, App,
 } from 'antd'
-import { UserOutlined, MoreOutlined } from '@ant-design/icons'
+import { UserOutlined } from '@ant-design/icons'
 import userPresenter from '~/presenters/user'
 import UserEditModal from '~/modules/admin/modules/threeSixtyCampaign/components/UserEditModal'
 import ResetSubjectModal from '~/modules/admin/modules/threeSixtyCampaign/components/ResetSubjectModal'
 import ConditionalDropdown from '~/components/ConditionalDropdown'
-import styles from './SubjectList.less'
 import { getActionsMenuProps } from './getActionsMenuProps'
 import ToolsDropdown from '../ToolsDropdown'
 import { Manage } from '../Manage'
@@ -152,11 +151,6 @@ export default function SubjectList ({
                       setShowResetSubjectModal,
                     })
                   }
-                  innerElement={(
-                    <div className={styles.actions}>
-                      <MoreOutlined />
-                    </div>
-                  )}
                 />
               )}
             />
