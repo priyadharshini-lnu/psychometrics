295768ce34ad80e8b6c04734da9f04055cbb0d88
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2024-07-26 19:02:12 +0530

    LH-3772: accessibility and responsivess changes to matrix type question (#4796)

--- app/frontend/modules/survey/components/modules/MatrixTable/components/types/Likert/SingleAnswerPreview.jsx
+++ app/frontend/modules/survey/components/modules/MatrixTable/components/types/Likert/SingleAnswerPreview.jsx
@@ -1,115 +1,178 @@
 import _ from 'lodash'
-import { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Radio, Checkbox } from 'antd'
+import { useContext } from 'react'
+import { Radio, Checkbox, Space } from 'antd'
+import cs from 'classnames'
+import { MediaQueryContext } from '~/glint'
+
 import styles from './Likert.less'
 
-export default class extends Component {
-  static propTypes = {
-    model: PropTypes.object.isRequired,
-  }
+const SingleAnswerPreview = ({ model, I18n, readOnly }) => {
+  const { isMobile } = useContext(MediaQueryContext)
+  const {
+    result, props, moduleConfig,
+  } = model
+  const { notApplicable, answersType } = props
+  const InputComponent = props.answersType === 'SingleAnswer' ? Radio : Checkbox
 
-  changeValue = (scale, choice, e) => {
-    const { model: { result } } = this.props
+  const changeValue = (scale, choice, e) => {
     if (result.notApplicable && result.notApplicable[choice]) {
       result.notApplicable[choice] = false
     }
     result.answer(scale, choice, e.target.checked)
-    this.forceUpdate()
   }
 
-  changeNotApplicable = (choice) => {
-    const { model: { result } } = this.props
+  const changeNotApplicable = (choice) => {
     _.remove(result.answers, { choice })
     result.notApplicable = result.notApplicable || {}
     result.notApplicable[choice] = true
     result.reduxAnswer()
-    this.forceUpdate()
   }
 
-  renderNotApplicableHeader () {
-    const { model, I18n } = this.props
-    const { notApplicable } = model.props
-    if (!notApplicable) { return null }
-    return (
-      <span className={styles.scalePointItem}>{I18n.tQuestion(model, 'notApplicableLabel')}</span>
-    )
-  }
+  const notApplicableHeader = notApplicable
+    ? (
+      <span
+        id={`notApplicable-${result.questionId}`}
+        className={styles.scalePointItem}
+      >
+        {I18n.tQuestion(model, 'notApplicableLabel')}
+      </span>
+    ) : null
 
-  renderNotApplicableCheckbox (choice) {
-    const { model, readOnly } = this.props
-    const { props: { notApplicable, answersType }, result } = model
-    const InputComponent = answersType === 'SingleAnswer' ? Radio : Checkbox
-    if (!notApplicable) { return null }
-    const checked = result.notApplicable && result.notApplicable[choice]
-    return (
-      <div>
-        <InputComponent
-          disabled={readOnly}
-          onChange={e => this.changeNotApplicable(choice, e)}
-          checked={checked || false}
-        />
-      </div>
-    )
-  }
 
-  render () {
-    const {
-      model, model: { props, result, moduleConfig }, readOnly, I18n,
-    } = this.props
-    const InputComponent = props.answersType === 'SingleAnswer' ? Radio : Checkbox
+  if (isMobile) {
     return (
-      <div className={styles.table}>
-        <div className={`${styles.row} ${styles.labels}`}>
-          <div className={styles.firstColumn} />
-          <div className={styles.labelItems}>
-            {_.times(props.labels, i => (
-              <div key={i}>
-                {props.labelsTexts[i] || moduleConfig.defaultLabelText(i + 1)}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={`${styles.row} ${styles.header}`}>
-          <div className={styles.firstColumn} />
-          <div className={styles.scalePoints}>
-            {_.times(props.scalePoints, i => (
-              <span key={i} className={styles.scalePointItem}>
-                {I18n.tQuestion(model, `scalePointsTexts${i + 1}`, { scale: i })
-                  || moduleConfig.defaultScalePointText(i + 1)}
-              </span>
-            ))}
-            {this.renderNotApplicableHeader()}
-          </div>
-        </div>
-
+      <Space size={10} direction="vertical" className="w-100">
         {_.times(props.choices, choice => (
-          <div key={choice} className={styles.row}>
-            <div className={styles.firstColumn}>
-              <div className={styles.item}>
-                <div key={choice}>
-                  {I18n.tQuestion(model, `choicesTexts${choice + 1}`, { choice })
-                    || moduleConfig.defaultChoiceText(choice + 1)}
-                </div>
-              </div>
+          <div key={choice} className={cs(styles.choiceSm, 'pt-1')}>
+            <div key={choice} id={`${result.questionId}-choice-${choice}`}>
+              {I18n.tQuestion(model, `choicesTexts${choice + 1}`, { choice })
+                  || moduleConfig.defaultChoiceText(choice + 1)}
             </div>
-            <div className={styles.inputs}>
+            <Space size={8} direction="vertical">
               {_.times(props.scalePoints, (scale) => {
                 const object = _.find(result.answers, { scale, choice }) || {}
                 return (
-                  <InputComponent
-                    disabled={readOnly}
-                    key={scale}
-                    checked={object.value || false}
-                    onChange={e => this.changeValue(scale, choice, e)}
-                  />
+                  <div key={scale}>
+                    {props.labels > 0
+                    && (
+                    <div id={`${result.questionId}-label-${scale}`} className={styles.labelItem}>
+                      {props.labelsTexts[scale] || moduleConfig.defaultLabelText(scale + 1)}
+                    </div>
+                    )}
+                    <Space size={0}>
+                      <InputComponent
+                        disabled={readOnly}
+                        checked={object.value || false}
+                        onChange={e => changeValue(scale, choice, e)}
+                        // eslint-disable-next-line max-len
+                        aria-describedby={`${result.questionId}-choice-${choice} ${result.questionId}-label-${scale} ${result.questionId}-scalepoint-${scale}`}
+                      />
+                      <span id={`${result.questionId}-scalepoint-${scale}`} className={styles.scalePointItem}>
+                        {I18n.tQuestion(model, `scalePointsTexts${scale + 1}`, { scale })
+                        || moduleConfig.defaultScalePointText(scale + 1)}
+                      </span>
+                    </Space>
+                  </div>
                 )
               })}
-              {this.renderNotApplicableCheckbox(choice)}
-            </div>
+              <Space size={0}>
+                <NotApplicableCheckbox
+                  result={result}
+                  choice={choice}
+                  answersType={answersType}
+                  notApplicable={notApplicable}
+                  readOnly={readOnly}
+                  changeNotApplicable={changeNotApplicable}
+                />
+                {notApplicableHeader}
+              </Space>
+            </Space>
           </div>
         ))}
-      </div>
+      </Space>
     )
   }
+
+  return (
+    <div className={styles.table}>
+      <div className={`${styles.row} ${styles.labels}`}>
+        <div className={styles.firstColumn} />
+        <div className={styles.labelItems}>
+          {_.times(props.labels, i => (
+            <div key={i} id={`${result.questionId}-label-${i}`}>
+              {props.labelsTexts[i] || moduleConfig.defaultLabelText(i + 1)}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={`${styles.row} ${styles.header}`}>
+        <div className={styles.firstColumn} />
+        <div className={styles.scalePoints}>
+          {_.times(props.scalePoints, i => (
+            <span key={i} className={styles.scalePointItem} id={`${result.questionId}-scalepoint-${i}`}>
+              {I18n.tQuestion(model, `scalePointsTexts${i + 1}`, { scale: i })
+                  || moduleConfig.defaultScalePointText(i + 1)}
+            </span>
+          ))}
+          {notApplicableHeader}
+        </div>
+      </div>
+
+      {_.times(props.choices, choice => (
+        <div key={choice} className={styles.row}>
+          <div className={styles.firstColumn}>
+            <div className={styles.item}>
+              <div key={choice} id={`${result.questionId}-choice-${choice}`}>
+                {I18n.tQuestion(model, `choicesTexts${choice + 1}`, { choice })
+                    || moduleConfig.defaultChoiceText(choice + 1)}
+              </div>
+            </div>
+          </div>
+          <div className={styles.inputs}>
+            {_.times(props.scalePoints, (scale) => {
+              const object = _.find(result.answers, { scale, choice }) || {}
+              return (
+                <InputComponent
+                  disabled={readOnly}
+                  key={scale}
+                  checked={object.value || false}
+                  onChange={e => changeValue(scale, choice, e)}
+                  // eslint-disable-next-line max-len
+                  aria-describedby={`${result.questionId}-choice-${choice} ${result.questionId}-label-${scale} ${result.questionId}-scalepoint-${scale}`}
+                />
+              )
+            })}
+            <NotApplicableCheckbox
+              result={result}
+              choice={choice}
+              answersType={answersType}
+              notApplicable={notApplicable}
+              readOnly={readOnly}
+              changeNotApplicable={changeNotApplicable}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+const NotApplicableCheckbox = ({
+  result, choice, answersType, notApplicable, readOnly, changeNotApplicable,
+}) => {
+  const InputComponent = answersType === 'SingleAnswer' ? Radio : Checkbox
+  if (!notApplicable) { return null }
+  const checked = result.notApplicable && result.notApplicable[choice]
+  return (
+    <div>
+      <InputComponent
+        disabled={readOnly}
+        onChange={e => changeNotApplicable(choice, e)}
+        checked={checked || false}
+        aria-describedby={`${result.questionId}-choice-${choice} notApplicable-${result.questionId}`}
+      />
+    </div>
+  )
+}
+
+export default SingleAnswerPreview
