0710fc21e64474b9a7ee84bd17b860d0aa167e88
Fedor Tarasov <tarassovf@gmail.com>
2024-08-02 11:28:51 +0400

    LH-3776: copy report fixes (#4821)
    * LH-3776: copy report fixes
    
    * fix filter

--- app/core/threesixty/reports_modules/remap_factor.rb
+++ app/core/threesixty/reports_modules/remap_factor.rb
@@ -20,10 +20,10 @@ module Threesixty
 
       def remap_multiple_factors_in_source
         report.modules.where("reports_modules.props -> 'source' ->>  'factors' IS NOT NULL").each do |m|
-          m.props['source']['factors'] = m.props['source']['factors'].map do |factor|
+          m.props['source']['factors'] = m.props['source']['factors'].filter_map do |factor|
             old_factor_id = factor['id']
             new_factor =  old_to_new_factor_mapping[old_factor_id]
-            next factor unless new_factor
+            next nil unless new_factor
 
             {
               id: new_factor.id,
