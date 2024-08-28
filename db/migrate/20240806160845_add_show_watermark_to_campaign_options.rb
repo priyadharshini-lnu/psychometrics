2fe61702a9f3e308b1df32d38e877f9951eff1d8
shivaraja-tte <shivaraja.nataraja@mercer.com>
2024-08-28 12:15:09 +0530

    revert watermark migration file change

--- db/migrate/20240806160845_add_show_watermark_to_campaign_options.rb
+++ db/migrate/20240806160845_add_show_watermark_to_campaign_options.rb
@@ -2,9 +2,7 @@
 
 class AddShowWatermarkToCampaignOptions < ActiveRecord::Migration[7.1]
   def change
-    change_table :campaign_options, bulk: true do |t|
-      t.boolean :show_watermark, default: false
-      t.string :watermark_content, default: ''
-    end
+    add_column :campaign_options, :show_watermark, :boolean, default: false
+    add_column :campaign_options, :watermark_content, :string, default: ''
   end
 end
