1024b5bde4346255da1092fe4c1cb8abf0c7ef1f
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2024-08-28 19:21:55 +0530

    Lh 3897 (#4942)
    * LH-3897: Remove number of takes property from video type question
    
    * Remove multiple takes UI form video recorder
    
    * remove unused translation keys
    
    * Remove unused translations

--- app/frontend/modules/survey/components/modules/VideoResponse/Properties.jsx
+++ app/frontend/modules/survey/components/modules/VideoResponse/Properties.jsx
@@ -2,7 +2,6 @@ import { Component } from 'react'
 import PropTypes from 'prop-types'
 import cs from 'classnames'
 import { Button, InputNumber } from 'antd'
-import _ from 'lodash'
 import { DefaultTrackerOptions } from '~/modules/survey/constants/DefaultProps'
 import styles from '~/modules/survey/views/PropertyPanel/components/PropertyPanel.less'
 import ValidationTypes from '~/modules/survey/components/ValidationTypes'
@@ -44,13 +43,6 @@ export class Properties extends Component {
     this.update()
   }
 
-  changeMaxTakes = (e) => {
-    const { model } = this.props
-    const maxTakes = parseInt(e.currentTarget.value, 10)
-    model.changeProps({ maxTakes })
-    this.update()
-  }
-
   updateFitInFrame = (e) => {
     const { model } = this.props
     const fitInFrame = e.currentTarget.value
@@ -88,19 +80,6 @@ export class Properties extends Component {
     )
   }
 
-  maxTakesFields () {
-    const { model } = this.props
-
-    return (
-      <div className={styles.fieldset} style={{ position: 'relative' }}>
-        <span className={styles.label}>Number of takes</span>
-        <select className="form-control" value={model.props.maxTakes} onChange={this.changeMaxTakes}>
-          <option value={null}>No Restriction</option>
-          {_.times(5, takeNo => (<option key={takeNo} value={takeNo + 1}>{takeNo + 1}</option>))}
-        </select>
-      </div>
-    )
-  }
 
   displayTrackerOptions () {
     const { model: { props: { fitInFrame, trackerOptions } } } = this.props
@@ -175,7 +154,6 @@ export class Properties extends Component {
     return (
       <div>
         { this.durationFields() }
-        {this.maxTakesFields() }
         { this.frameFields() }
       </div>
     )
