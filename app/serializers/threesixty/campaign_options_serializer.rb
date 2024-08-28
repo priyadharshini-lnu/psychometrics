7ea09528df3f7520f0956bd007532ea85596ef76
shivaraja-tte <shivaraja.nataraja@mercer.com>
2024-08-28 12:59:58 +0530

    fix watermark specs

--- app/serializers/threesixty/campaign_options_serializer.rb
+++ app/serializers/threesixty/campaign_options_serializer.rb
@@ -13,6 +13,8 @@ module Threesixty
     end
 
     def participants
+      return unless object&.participants
+
       watermark_content = object.participants.dig('global', 'watermark_content')
       show_watermark = object.participants.dig('global', 'show_watermark')
 
