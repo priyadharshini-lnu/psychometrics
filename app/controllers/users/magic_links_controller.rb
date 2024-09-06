42922f7bab9b42051fcdfaa00dc3b1ba0a0f3610
Ayza Hamid <ayza@thetalententerprise.com>
2024-09-06 01:22:25 -0700

    Lh 3943 | Added magic link with deep linking to assessments (#5026)
    * Added magic link with deep linking
    
    * Incorporated the feedback
    
    * refactored the params

--- app/controllers/users/magic_links_controller.rb
+++ app/controllers/users/magic_links_controller.rb
@@ -7,6 +7,21 @@ module Users
     def show
       super do
         session[:login_via_magic_link] = true
+
+        if valid_campaign_and_assessment?
+          user_assessment = current_user.user_assessments.find_by(assessment_id: assessment_id,
+                                                                  campaign_id: campaign_id)
+
+          if user_assessment
+            redirect_to user_assessment_path(user_assessment) and return
+          else
+            redirect_to root_path and return
+          end
+        elsif campaign_id.present?
+          redirect_to campaign_path(campaign_id) and return
+        else
+          redirect_to root_path and return
+        end
       end
     end
 
@@ -15,13 +30,29 @@ module Users
         :user_magic_link_url,
         id: params[:id],
         subdomain: @current_project.subdomain,
-        user: params[:user].permit(:email, :token, :remember_me)
+        user: params[:user].permit(:email, :token, :remember_me),
+        campaign_id: params[:campaign_id],
+        assessment_id: params[:assessment_id]
       )
       render :sign_in_link, layout: 'empty_end_user'
     end
 
     def auth_options
       { scope: :user }
+    end
+
+    private
+
+    def valid_campaign_and_assessment?
+      campaign_id.present? && assessment_id.present?
+    end
+
+    def campaign_id
+      params[:campaign_id]
+    end
+
+    def assessment_id
+      params[:assessment_id]
     end
   end
 end
---
+++ app/frontend/components/Editor/PipedTextModal/types/MagicLink.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import {
+  Button, Space, InputNumber, Form,
+} from 'antd'
+
+const { I18n } = window
+
+const MagicLink = ({ field: { getValue }, insert }) => {
+  const [open, setOpen] = useState(false)
+  const [form] = Form.useForm()
+
+  const onOk = () => {
+    form.validateFields().then(() => {
+      const { campaignId, assessmentId } = form.getFieldsValue()
+
+      let baseValue = getValue('', '')
+
+      if (campaignId && assessmentId) {
+        baseValue = getValue(campaignId, assessmentId)
+      } else if (campaignId) {
+        baseValue = getValue(campaignId, '')
+      } else if (assessmentId) {
+        baseValue = getValue('', assessmentId)
+      }
+
+      insert(baseValue)
+      setOpen(false)
+    })
+  }
+
+  const content = (
+    <Form form={form} labelCol={{ span: 8 }} wrapperCol={{ span: 16, style: { paddingLeft: '10px' } }}>
+      <Space direction="vertical">
+        <Form.Item
+          name="campaignId"
+          label={I18n.t('administration.piped_text_modal.campaign_id')}
+        >
+          <InputNumber style={{ width: 200 }} />
+        </Form.Item>
+        <Form.Item
+          name="assessmentId"
+          label={I18n.t('administration.piped_text_modal.assessment_id')}
+        >
+          <InputNumber style={{ width: 200 }} />
+        </Form.Item>
+        <Space>
+          <Button onClick={onOk} type="primary">
+            {I18n.t('common.actions.insert')}
+          </Button>
+          <Button onClick={() => setOpen(false)}>
+            {I18n.t('common.actions.cancel')}
+          </Button>
+        </Space>
+      </Space>
+    </Form>
+  )
+
+  return open ? content : (
+    <Button type="link" onClick={() => setOpen(true)}>
+      {I18n.t('administration.piped_text_modal.magic_link')}
+    </Button>
+  )
+}
+
+export default MagicLink
