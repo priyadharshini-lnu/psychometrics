153b127f647cb29fd15b9bbbd639997207a858ff
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2024-09-30 14:48:43 +0530

    LH-4011: Add option to choose owner while copying assessment and reports (#5180)
    * LH-4011: Add option to choose owner to choose while copying assessment and report
    
    * add spec and feedback
    
    * fix spec

--- app/frontend/modules/admin/modules/Reports/routes/ReportList/routes/ReportList/CopyReportFormModal.tsx
+++ app/frontend/modules/admin/modules/Reports/routes/ReportList/routes/ReportList/CopyReportFormModal.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import {
   Form, Input, App,
+  Select,
+  Spin,
 } from 'antd'
 
 import { useResourceContext } from '~/modules/admin/components/Resource'
 import { Report, ReportTR } from '~/modules/admin/modules/client/core/reports'
 
 import ResourceFormModal from '~/components/ResourceFormModal'
+import { useResources } from '~/hooks/useResources'
+import { Client } from '~/modules/admin/modules/client/core/clients'
 
 const { I18n } = window
+
+
+type OptionsType = {
+  id: string
+  name: string
+}
 
 interface Props {
   report: Report
@@ -38,6 +48,18 @@ const CopyReportFormModal: React.FC<Props> = ({
     message.success(I18n.t('reports.actions.copy.success_message', { name: response.name }))
   })
 
+  const {
+    data: clients, fetch: fetchClients, isLoading: isClientsLoading,
+  } = useResources<Client>('clients')
+
+  const getClients = (): OptionsType[] => {
+    if (!report || !report.owner || clients.find(d => report?.owner?.id === d.id)) {
+      return clients
+    }
+
+    return [...clients, report.owner]
+  }
+
   return (
     <ResourceFormModal
       resourceName="assessments"
@@ -57,6 +79,27 @@ const CopyReportFormModal: React.FC<Props> = ({
             rules={[{ required: true, transform: value => value.trim() }]}
           >
             <Input name="report_name" />
+          </Form.Item>
+          <Form.Item
+            name="ownerId"
+            label={I18n.t('common.column.owner')}
+            initialValue={report?.owner?.id || null}
+          >
+            <Select
+              showSearch
+              onSearch={(value) => {
+                fetchClients({
+                  apiConfig: { filter: { filterable_fields: value }, fields: { clients: ['name'] } },
+                })
+              }}
+              notFoundContent={isClientsLoading('fetch') ? <Spin size="small" /> : null}
+              filterOption={false}
+            >
+              <Select.Option>TTE</Select.Option>
+              {getClients().map(({ id, name }) => (
+                <Select.Option key={id} value={id}>{name}</Select.Option>
+              ))}
+            </Select>
           </Form.Item>
         </>
       )}
