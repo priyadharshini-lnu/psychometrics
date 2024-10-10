0739a7bf94dfe7be4ad0fafe4bd1d4910bc4c855
Namrata Ukirde <namrata.ukirde@thetalententerprise.com>
2024-10-10 10:49:46 +0530

    LH-4022 fix sentry related to campaign report bulk download (#5225)

--- app/core/admin_jobs/bulk_download_reports.rb
+++ app/core/admin_jobs/bulk_download_reports.rb
@@ -11,7 +11,9 @@ module AdminJobs
 
       bulk_report = result[:ok]
 
-      return broadcast :ok, { error_messages: bulk_report[:error_messages] } if bulk_report[:error_messages].present?
+      if bulk_report&.dig(:error_messages)&.present?
+        return broadcast :ok, { error_messages: bulk_report[:error_messages] }
+      end
 
       if bulk_report
         content = [
