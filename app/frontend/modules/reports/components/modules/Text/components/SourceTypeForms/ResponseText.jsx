e322bd58b130d2f4b1b7deee865a0ead5408f9ed
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2024-10-17 12:23:49 +0530

    feat/LH-4114: response text changes in campaign factor feedback question (#5265)

--- app/frontend/modules/reports/components/modules/Text/components/SourceTypeForms/ResponseText.jsx
+++ app/frontend/modules/reports/components/modules/Text/components/SourceTypeForms/ResponseText.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/destructuring-assignment */
 import _ from 'lodash'
 import { Component } from 'react'
-import { Space } from 'antd'
+import { Space, InputNumber } from 'antd'
 import PropTypes from 'prop-types'
 import Select from 'react-select'
 import * as QuestionPresenter from '~/modules/reports/presenters/QuestionPresenter'
@@ -20,17 +20,29 @@ const FORMATS = [{
   value: 'NumberedList',
 }]
 
+const CAMPAIGN_FACTOR_RESULT_TYPES = [
+  { value: 'name', label: 'Name' },
+  { value: 'feedback', label: 'Feedback' },
+]
+
 export default class ResponseText extends Component {
   static propTypes = {
     model: PropTypes.object.isRequired,
     onChangeModelIn: PropTypes.func.isRequired,
   }
 
+  // TODO combine all the onChange handlers into one
   onChangeQuestion = ({ value }) => this.props.onChangeModelIn(['props', 'question'], value)
 
   onChangeFormat = ({ currentTarget: { value } }) => this.props.onChangeModelIn(['props', 'format'], value)
 
-  onChangeTextEntryFormAnswerIndex = e => this.props.onChangeModelIn(['props', 'answerIndex'], e.currentTarget.value)
+  onChangeTextEntryFormAnswerIndex = value => this.props.onChangeModelIn(['props', 'answerIndex'], value)
+
+  handleOnAnswerIndexCodeChange = value => this.props.onChangeModelIn(['props', 'answerIndexCode'], value)
+
+  onChangeCampaignFactorResultType = value => this.props.onChangeModelIn(
+    ['props', 'campaignFactorResultType'], value,
+  )
 
   onChangeCheckbox = ({ currentTarget: { checked } }, name) => {
     this.props.onChangeModelIn(['props', name], checked)
@@ -126,7 +138,11 @@ export default class ResponseText extends Component {
     return (
       <div>
         <div>TextEntry form field</div>
-        <select className="form-control" value={answerIndex} onChange={this.onChangeTextEntryFormAnswerIndex}>
+        <select
+          className="form-control"
+          value={answerIndex}
+          onChange={e => this.onChangeTextEntryFormAnswerIndex(e.currentTarget.value)}
+        >
           {_.times(choices, i => (<option key={i} value={i}>{choicesTexts[i]}</option>))}
         </select>
       </div>
@@ -134,17 +150,63 @@ export default class ResponseText extends Component {
   }
 
   renderCampaignFactorFeedbackAnswerIndex () {
-    const { props: { question, answerIndex }, campaignFactorsList } = this.props.model
+    const {
+      props: {
+        question, answerIndex, answerIndexCode, campaignFactorResultType,
+      },
+      campaignFactorsList,
+    } = this.props.model
     const currentQuestion = this.lookupQuestionById(question)
     if (!currentQuestion || currentQuestion.type !== 'CampaignFactorFeedback') { return null }
+
+    const showCodeSelectionFields = campaignFactorResultType && campaignFactorResultType === 'code'
+    const answerIndexNumber = isNaN(answerIndex) ? null : answerIndex
+
     return (
-      <div>
+      <Space direction="vertical" className="w-100">
         <div>Campaign factor feedback</div>
-        <select className="form-control" value={answerIndex || ''} onChange={this.onChangeTextEntryFormAnswerIndex}>
-          {!answerIndex ? <option value="" /> : null}
-          {campaignFactorsList.map(({ code }) => (<option key={code} value={code}>{code}</option>))}
-        </select>
-      </div>
+        <label className="font-normal">
+          <input
+            checked={campaignFactorResultType === 'code'}
+            type="checkbox"
+            onChange={e => this.onChangeCampaignFactorResultType(e.currentTarget.checked ? 'code' : '')}
+          />
+          {' '}
+          Code based
+        </label>
+        {showCodeSelectionFields ? (
+          <select
+            className="form-control"
+            value={answerIndexCode || ''}
+            onChange={e => this.handleOnAnswerIndexCodeChange(e.currentTarget.value)}
+          >
+            {!answerIndexCode ? <option value="" /> : null}
+            {campaignFactorsList.map(({ code }) => (<option key={code} value={code}>{code}</option>))}
+          </select>
+        )
+          : (
+            <>
+              <InputNumber
+                min={1}
+                value={answerIndexNumber}
+                onChange={this.onChangeTextEntryFormAnswerIndex}
+              />
+              {answerIndexNumber ? (
+                <select
+                  className="form-control"
+                  value={campaignFactorResultType || ''}
+                  onChange={e => this.onChangeCampaignFactorResultType(e.currentTarget.value)}
+                >
+                  {!campaignFactorResultType ? <option value="" /> : null}
+                  {CAMPAIGN_FACTOR_RESULT_TYPES.map((
+                    { value, label },
+                  ) => (<option key={value} value={value}>{label}</option>))}
+                </select>
+              ) : null}
+            </>
+          )
+        }
+      </Space>
     )
   }
 
