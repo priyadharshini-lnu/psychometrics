e4b19a3a73bb2f6f00fce9dc7500a346a9fcfdc3
namrataukirde <namrata.ukirde@thetalententerprise.com>
2024-10-24 14:36:55 +0530

    fix rubocop and modify error msg

--- app/models/concerns/active_storage_attachable.rb
+++ app/models/concerns/active_storage_attachable.rb
@@ -34,16 +34,16 @@ module ActiveStorageAttachable
     end
 
     def copy_and_upload(attachment, attribute)
-      begin
-        file_content = attachment.download
-        send(attribute).attach(
-          io: StringIO.new(file_content),
-          filename: attachment.filename,
-          content_type: attachment.content_type
-        )
-      rescue ActiveStorage::FileNotFoundError => e
-        Rails.logger.error("ActiveStorage Error: #{e.message}")
-      end
+      file_content = attachment.download
+      send(attribute).attach(
+        io: StringIO.new(file_content),
+        filename: attachment.filename,
+        content_type: attachment.content_type
+      )
+    rescue ActiveStorage::FileNotFoundError => e
+      Rails.logger.error(
+        "ActiveStorage Error: #{e.message} attachment_type: #{attachment.class.name} record_id: #{attachment.id}"
+      )
     end
 
     after_create do
