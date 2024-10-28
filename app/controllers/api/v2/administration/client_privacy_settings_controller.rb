17860a3c3e6a63b55e35fed8a0d0409f4c72f475
vijay2136 <imvijay2136@gmail.com>
2024-10-28 15:52:56 +0530

    Code fix

--- app/controllers/api/v2/administration/client_privacy_settings_controller.rb
+++ app/controllers/api/v2/administration/client_privacy_settings_controller.rb
@@ -2,5 +2,7 @@
 
 module Api
   class V2::Administration::ClientPrivacySettingsController < Api::V2::Administration::BaseController
+    validates_request_schema :update, Api::V2::ClientPrivacySettings::UpdateContract.new
+    validate_crud_requests Api::V2::ClientPrivacySettings::Schema
   end
 end
