a599abda74665238a92da66a97f11e8d9c0840e4
vijay2136 <imvijay2136@gmail.com>
2024-11-04 15:04:07 +0530

    Added timestamp columns

--- db/migrate/20241025070422_create_client_privacy_settings.rb
+++ db/migrate/20241025070422_create_client_privacy_settings.rb
@@ -2,18 +2,21 @@
 
 class CreateClientPrivacySettings < ActiveRecord::Migration[7.1]
   def change
-    create_table :client_privacy_settings do |t| # rubocop:disable Rails/CreateTableWithTimestamps
+    create_table :client_privacy_settings do |t|
       t.references :client, foreign_key: { on_delete: :cascade }, null: false
       t.boolean :disable_data_processing, default: false
+      t.timestamps
     end
 
     reversible do |dir|
       dir.up do
         sql = <<~SQL.squish
-          INSERT INTO client_privacy_settings (client_id, disable_data_processing)
-          SELECT clients.id, false FROM clients
+          INSERT INTO client_privacy_settings (client_id, disable_data_processing, created_at, updated_at)
+          SELECT clients.id, false, NOW(), NOW()
+          FROM clients
           WHERE clients.ancestry_depth = 0
         SQL
+
         execute(sql)
       end
     end
