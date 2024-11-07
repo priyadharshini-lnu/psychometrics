aeb3d22ec2bf13ce937a1425c2b2f29210b9bad0
Namrata Ukirde <namrata.ukirde@thetalententerprise.com>
2024-11-07 19:21:22 +0530

    hotfix - fix agile result import (#5381)
    * hotfix - fix agile result import
    
    * fix rubocop

--- db/migrate/20241105093139_reimport_saville_factors.rb
+++ db/migrate/20241105093139_reimport_saville_factors.rb
@@ -2,6 +2,8 @@
 
 class ReimportSavilleFactors < ActiveRecord::Migration[7.1]
   def change
+    # rubocop:disable CustomRubocops/AvoidActiveRecordInMigrations, Rails/FilePath
     Saville::ImportFactors.call!("#{Rails.root}/public/source/saville-factors.csv")
+    # rubocop:enable CustomRubocops/AvoidActiveRecordInMigrations, Rails/FilePath
   end
 end
