1126f74b8cdfe1095bc2239dd4dc6478e2d271bd
Fedor Tarasov <tarassovf@gmail.com>
2024-11-07 19:16:00 +0400

    fix global styles (#5383)

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,9 @@ export default defineConfig({
         const path = id.split('?').slice(0, 1).join('')
         if (path.endsWith('/ant.less')) { return false }
         if (path.endsWith('/globals.less')) { return false }
+        if (path.endsWith('/styles/common.less')) { return false }
+        if (path.endsWith('/styles/utils.less')) { return false }
+
         if (path.endsWith('less') && !path.includes('node_modules')) {
           return true
         }
