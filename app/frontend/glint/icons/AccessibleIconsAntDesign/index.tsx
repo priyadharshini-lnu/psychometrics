ee35e1d060c2ef70b8dbb6d2505738d6a43d3a55
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2024-12-05 22:26:50 +0530

    fix/LH-4142: accessibility fixes in end user pages (#5473)
    - Fix heading hierarchy on Campaign listing and Task listing page.
    - Fix menu item text cutting off in page sider when letter spacing is increased.
    - Accessible icon replacement.
    - Fix color contrast of pageheader back icon focus ring.
    - Make Show more/less instructions button by adding proper aria labels. Also make Instructions content available for screenreaders/keyboard only when you see full content.
    - Profile form changes to keep focus on first form input with error in case of failed submit.

--- app/frontend/glint/icons/AccessibleIconsAntDesign/index.tsx
+++ app/frontend/glint/icons/AccessibleIconsAntDesign/index.tsx
@@ -34,6 +34,7 @@ import Icon, {
   UploadOutlined as AntdUploadOutlined,
   EyeTwoTone as AntdEyeTwoTone,
   EyeInvisibleOutlined as AntdEyeInvisibleOutlined,
+  EditOutlined as AntdEditOutlined,
 } from '@ant-design/icons'
 import type { IconComponentProps } from '@ant-design/icons/lib/components/Icon'
 
@@ -77,3 +78,4 @@ export const CloseCircleFilled = withAccessibilityProps(AntdCloseCircleFilled)
 export const UploadOutlined = withAccessibilityProps(AntdUploadOutlined)
 export const EyeTwoTone = withAccessibilityProps(AntdEyeTwoTone)
 export const EyeInvisibleOutlined = withAccessibilityProps(AntdEyeInvisibleOutlined)
+export const EditOutlined = withAccessibilityProps(AntdEditOutlined)
