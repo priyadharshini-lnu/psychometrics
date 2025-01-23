ba89356a29b11f95d0397dbea6b177ced1acc6e7
Fedor Tarasov <tarassovf@gmail.com>
2025-01-23 22:46:15 +0400

    Hotfix: fix assessment default language (#5764)

--- app/controllers/concerns/threesixty/set_assessment_locale.rb
+++ app/controllers/concerns/threesixty/set_assessment_locale.rb
@@ -4,9 +4,10 @@ module Threesixty::SetAssessmentLocale
   extend ActiveSupport::Concern
 
   def set_locale_for_user_assessment(user_assessment)
-    fallback_locale = user_assessment.selected_locale || @current_user.locale || user_locale
+    fallback_locale = user_assessment.selected_locale || @current_user.locale ||
+                      user_assessment.assessment.default_language || user_locale
     @available_translations = ::Translation.available_translation_for_assessment(user_assessment.assessment_id)
-    valid_locales = @available_translations + [I18n.default_locale.to_s]
+    valid_locales = @available_translations + [user_assessment.assessment.default_language]
 
     if params[:lang] && valid_locales.include?(params[:lang])
       @selected_locale = params[:lang]
