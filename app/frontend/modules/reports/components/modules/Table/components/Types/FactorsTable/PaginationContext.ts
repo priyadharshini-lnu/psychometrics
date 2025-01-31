b317156676c1374a86733d83f4e107bb23a66ce3
Fedor Tarasov <tarassovf@gmail.com>
2025-01-31 10:21:17 +0400

    Hotfix: fix second page height calculation (#5802)
    * Hotfix: fix second page height calculation
    
    * update data attr

--- app/frontend/modules/reports/components/modules/Table/components/Types/FactorsTable/PaginationContext.ts
+++ app/frontend/modules/reports/components/modules/Table/components/Types/FactorsTable/PaginationContext.ts
@@ -28,10 +28,11 @@ export class PaginationContext {
   splitInPages () {
     const containerHeight = this.module.props.position.height
     const headerheight = this.root.querySelector('[data-table-header]')?.getBoundingClientRect()?.height || 0
+    const legendHeight = this.root.querySelector('[data-legend]')?.getBoundingClientRect()?.height || 0
 
-    const secondPageHeight = (this.module.props.pagination?.position?.height || 1000) - headerheight
+    const secondPageHeight = (this.module.props.pagination?.position?.height || 1000) - headerheight - legendHeight
 
-    const contentHeight = containerHeight - headerheight
+    const contentHeight = containerHeight - headerheight - legendHeight
 
     const rootRect = this.root.getBoundingClientRect()
 
@@ -39,7 +40,7 @@ export class PaginationContext {
       return
     }
 
-    const firstLevel = this.root.querySelectorAll('[data-paginatable="1"]')
+    const rows = this.root.querySelectorAll('[data-row]')
     let page:PageData = {
       factorIds: [],
       index: this.pages.length,
@@ -47,19 +48,20 @@ export class PaginationContext {
     this.pages.push(page)
 
     let currentPageHeight = 0
-    firstLevel.forEach((filterElement) => {
+    rows.forEach((filterElement) => {
       const factorRow = filterElement.getBoundingClientRect()
       const factorId = filterElement.getAttribute('data-factor-id')
       if (!factorId) { return }
 
       const maxHeight = this.pages.length > 1 ? secondPageHeight : contentHeight
-      if (currentPageHeight + factorRow.height > maxHeight) {
+      if (page.factorIds.length && (currentPageHeight + factorRow.height > maxHeight)) {
         this.needPagination = true
         page = {
           factorIds: [factorId],
           index: this.pages.length,
         }
         this.pages.push(page)
+        currentPageHeight = factorRow.height
       } else {
         page.factorIds.push(factorId)
         currentPageHeight += factorRow.height
