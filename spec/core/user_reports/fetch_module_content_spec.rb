bead798c848feec7548ce7d673fc5e43d245d3b4
Vijay Vinod <48003440+vijay2136@users.noreply.github.com>
2025-02-28 15:04:40 +0530

    Task/1192861: Added module content column in the qc logs (#5932)
    * Added module content column in the qc logs
    
    * Used private_attr_reader in fetch_module_content class

---
+++ spec/core/user_reports/fetch_module_content_spec.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UserReports::FetchModuleContent, type: :service do
+  let(:superadmin) { create(:superadmin) }
+  let(:module1) { create(:module, name: 'Module 1', props: { 'text' => 'Module 1 Content' }) }
+  let(:report) { create(:report) }
+  let(:page) { create(:page, modules: [module1], report: report) }
+  let(:user_report) { create(:user_report, user: superadmin, report: report) }
+  let(:event_created_at) { Time.current }
+  let(:service) { described_class.new(user_report_event) }
+
+  describe '#call' do
+    subject { service.call }
+
+    context 'when module_id is nil' do
+      let(:user_report_event) do
+        create(
+          :user_report_event,
+          event_type: 'status_changed',
+          initiator: superadmin,
+          user_report: user_report,
+          details: { 'module' => nil },
+          created_at: event_created_at
+        )
+      end
+
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+
+    context 'when event_type is "removed_text"' do
+      let(:user_report_event) do
+        double(
+          UserReportEvent,
+          event_type: 'removed_text',
+          initiator: superadmin,
+          user_report: user_report,
+          user_report_id: user_report.id,
+          details: { 'module' => module1.id.to_s, 'content' => 'removed_content' },
+          created_at: event_created_at,
+          module_content: module1.props['text']
+        )
+      end
+
+      it 'returns the original module content without HTML tags' do
+        expect(subject).to eq(module1.props['text'])
+      end
+    end
+
+    context 'when event has content' do
+      let(:user_report_event) do
+        double(
+          UserReportEvent,
+          event_type: 'added_text',
+          initiator: superadmin,
+          user_report: user_report,
+          user_report_id: user_report.id,
+          details: { 'module' => module1.id.to_s, 'content' => '<p>Event content</p>' },
+          created_at: event_created_at,
+          module_content: module1.props['text']
+        )
+      end
+
+      it 'returns the event content without HTML tags' do
+        expect(subject).to eq('Event content')
+      end
+    end
+
+    context 'when event does not have content' do
+      let(:user_report_event) do
+        double(
+          UserReportEvent,
+          event_type: 'comment_added',
+          initiator: superadmin,
+          user_report: user_report,
+          user_report_id: user_report.id,
+          details: { 'module' => module1.id.to_s },
+          created_at: event_created_at,
+          module_content: module1.props['text']
+        )
+      end
+
+      context 'and there is a latest event with content' do
+        before do
+          latest_event = double(
+            UserReportEvent,
+            event_type: 'created_text',
+            details: { 'content' => '<p>Latest event content</p>' }
+          )
+
+          allow_any_instance_of(described_class).to receive(:fetch_latest_event_with_content).and_return(latest_event)
+        end
+
+        it 'returns the latest event content without HTML tags' do
+          expect(subject).to eq('Latest event content')
+        end
+      end
+
+      context 'and there is no latest event with content' do
+        before do
+          allow_any_instance_of(described_class).to receive(:fetch_latest_event_with_content).and_return(nil)
+        end
+
+        it 'returns the original module content without HTML tags' do
+          expect(subject).to eq(module1.props['text'])
+        end
+      end
+    end
+  end
+end
