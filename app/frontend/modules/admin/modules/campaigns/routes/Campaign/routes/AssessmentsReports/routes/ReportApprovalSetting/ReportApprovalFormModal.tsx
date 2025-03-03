b186c0ae02c2ef61aa5639f828abd443791ee4dd
Fedor Tarasov <fiodar.tarasov@mercer.com>
2025-03-03 11:54:13 +0400

    fix i18n

--- app/frontend/modules/admin/modules/campaigns/routes/Campaign/routes/AssessmentsReports/routes/ReportApprovalSetting/ReportApprovalFormModal.tsx
+++ app/frontend/modules/admin/modules/campaigns/routes/Campaign/routes/AssessmentsReports/routes/ReportApprovalSetting/ReportApprovalFormModal.tsx
@@ -12,6 +12,7 @@ import { AdditionRelationshipAttribute } from '~/libs/jsonApi/interfaces'
 import { ReportApprovalSettings } from '~/modules/admin/modules/campaigns/core/reportApprovalSettings'
 
 const { Option } = Select
+const { I18n } = window
 
 interface Props {
   reportApprovalSettings: AdditionRelationshipAttribute<ReportApprovalSettings>
@@ -137,7 +138,7 @@ export const ReportApprovalFormModal: React.FC<Props> = ({
                       <Switch />
                     </Form.Item>
                     <div className="weight-600">
-                      Approvers Not Required
+                      {I18n.t('administration.campaigns.assessment_reports.report_approval.approvers_not_required')}
                     </div>
                   </Space>
                   {!form.getFieldValue('approversNotRequired') && (
@@ -151,7 +152,7 @@ export const ReportApprovalFormModal: React.FC<Props> = ({
                           <Switch />
                         </Form.Item>
                         <div className="weight-600">
-                          Approvers Can Edit
+                          {I18n.t('administration.campaigns.assessment_reports.report_approval.approvers_can_edit')}
                         </div>
                       </Space>
                     </>
@@ -165,7 +166,7 @@ export const ReportApprovalFormModal: React.FC<Props> = ({
                       <Switch />
                     </Form.Item>
                     <div className="weight-600">
-                      Do Not Send Notifications
+                      {I18n.t('administration.campaigns.assessment_reports.report_approval.do_not_send_notifications')}
                     </div>
                   </Space>
                 </Space>),
