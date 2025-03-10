3feedbd9e475e230f7eb5e8a40407fbb2b172eb8
Rohith R <rohith.r@mercer.com>
2025-03-10 10:02:17 +0100

    TASK/1193040: Query fix for empty user_ids (#6009)
    * ADO-1193040 query fix for empty user_ids
    
    * ADO-1193040 query fix for empty user_ids

--- app/core/campaign_users/change_campaign_score_finalized.rb
+++ app/core/campaign_users/change_campaign_score_finalized.rb
@@ -13,9 +13,13 @@ module CampaignUsers
     end
 
     def call
-      campaign_users = ::CampaignUser.
-                       where(campaign_id: campaign.id).
-                       where(exclude ? ['user_id NOT IN (?)', user_ids] : { user_id: user_ids })
+      campaign_users = ::CampaignUser.where(campaign_id: campaign.id)
+      campaign_users = if exclude
+                         user_ids.empty? ? campaign_users : campaign_users.where.not(user_id: user_ids)
+                       else
+                         user_ids.empty? ? campaign_users : campaign_users.where(user_id: user_ids)
+                       end
+
       campaign_users.update_all(
         campaign_scores_finalized: campaign_score_finalized,
         campaign_scores_finalized_date: campaign_score_finalized ? Time.current : nil
@@ -32,10 +36,15 @@ module CampaignUsers
     private
 
     def campaign_factor_dependent_user_reports
-      UserReport.joins(:report).
-        merge(Report.campaign_factor_dependable).
-        where(campaign_id: campaign.id).
-        where(exclude ? ['user_id NOT IN (?)', user_ids] : { user_id: user_ids })
+      user_reports = UserReport.joins(:report).
+                     merge(Report.campaign_factor_dependable).
+                     where(campaign_id: campaign.id)
+
+      if exclude
+        user_ids.empty? ? user_reports : user_reports.where.not(user_id: user_ids)
+      else
+        user_ids.empty? ? user_reports : user_reports.where(user_id: user_ids)
+      end
     end
   end
 end
