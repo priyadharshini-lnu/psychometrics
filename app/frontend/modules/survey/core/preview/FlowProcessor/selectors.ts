4574baaf10d2b78f2b18db9ce09bc07833473e10
shivaraja-tte <shivaraja.nataraja@mercer.com>
2025-03-13 13:08:54 +0530

    allow all media responses like audio and file upload

--- app/frontend/modules/survey/core/preview/FlowProcessor/selectors.ts
+++ app/frontend/modules/survey/core/preview/FlowProcessor/selectors.ts
@@ -31,13 +31,9 @@ export const getAllAnsweredQuestions = (state): Question[] => {
   return _.reduce(
     pages, (res, block) => {
       const questions = _.flatten(_.map(
-        block, page => getQuestions(state, page.questions).filter((question) => {
-          const resultAvailableForQuestion = !!state.results[question.id]
-          if (!resultAvailableForQuestion && question.type === 'VideoResponse') {
-            return !!getMediaResponseByQuestionId(state, question.id)
-          }
-          return resultAvailableForQuestion
-        }),
+        block, page => getQuestions(state, page.questions).filter(
+          question => state.results[question.id] || getMediaResponseByQuestionId(state, question.id),
+        ),
       ))
 
       return [...res, ...questions]
