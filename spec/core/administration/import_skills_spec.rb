8b470307bb8e02adb64dde2998695bbaa66787ef
Namrata Ukirde <namrata.ukirde@thetalententerprise.com>
2025-04-29 15:16:53 +0400

    add model level validation for skills name (#6286)

--- spec/core/administration/import_skills_spec.rb
+++ spec/core/administration/import_skills_spec.rb
@@ -81,7 +81,7 @@ RSpec.describe Administration::ImportSkills do
 
       it 'returns error for duplicate skill name' do
         result = described_class.new(file_url, project.id).call
-        expect(result).to include("Line 2: Skill with name 'Programming' already exists in project.")
+        expect(result).to include("Line 2: Failed to save skill 'Programming': Name has already been taken")
       end
 
       it 'does not create or update any skills when there is a duplicate' do
