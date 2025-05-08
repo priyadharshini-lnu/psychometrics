01bbc5dad3351dbd5b9d95ca75bdb670ef8146ac
Fedor Tarasov <tarassovf@gmail.com>
2025-05-08 17:55:21 +0400

    Fix to fail rubocop migration on CI  (#6354)
    * use branch cache only
    
    * debug
    
    * add dummy posgress url
    
    * update config
    
    * ignore existing migrations
    
    * move db config to rubocop
    
    * rollback cache key

--- db/migrate/20250430135151_add_columns_to_user_idp_plan.rb
+++ db/migrate/20250430135151_add_columns_to_user_idp_plan.rb
@@ -2,7 +2,7 @@
 
 class AddColumnsToUserIdpPlan < ActiveRecord::Migration[7.1]
   def change
-    add_column :user_idp_plans, :completed_at, :datetime
+    add_column :user_idp_plans, :completed_at, :datetime # rubocop:disable Rails/BulkChangeTable
     add_column :user_idp_plans, :started_at, :datetime
   end
 end
