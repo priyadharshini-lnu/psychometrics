b750fad9fe0019b87ba0683537b9f01129eae53a
Shaiju Edakulangara <shaiju@thetalententerprise.com>
2025-05-21 13:15:39 +0530

    Fix rubocop issue

--- db/migrate/20250429125102_add_api_key_fields_to_webhook_subscriptions.rb
+++ db/migrate/20250429125102_add_api_key_fields_to_webhook_subscriptions.rb
@@ -2,8 +2,10 @@
 
 class AddApiKeyFieldsToWebhookSubscriptions < ActiveRecord::Migration[7.1]
   def change
+    # rubocop:disable Rails/BulkChangeTable
     add_column :webhook_subscriptions, :encrypted_api_key, :string
     add_column :webhook_subscriptions, :encrypted_api_key_iv, :string
     add_column :webhook_subscriptions, :api_key_header, :string
+    # rubocop:enable Rails/BulkChangeTable
   end
 end
--- db/migrate/20250430135151_add_columns_to_user_idp_plan.rb
+++ db/migrate/20250430135151_add_columns_to_user_idp_plan.rb
@@ -2,7 +2,9 @@
 
 class AddColumnsToUserIdpPlan < ActiveRecord::Migration[7.1]
   def change
+    # rubocop:disable Rails/BulkChangeTable
     add_column :user_idp_plans, :completed_at, :datetime
     add_column :user_idp_plans, :started_at, :datetime
+    # rubocop:enable Rails/BulkChangeTable
   end
 end
