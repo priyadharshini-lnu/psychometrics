b65928f24de76bcbe58d65777a9adf171a694803
ruthvik-mandapati <mandapati.ruthvik@mercer.com>
2025-05-21 12:06:13 +0530

    fix rubocop issues

--- db/migrate/20250430135151_add_columns_to_user_idp_plan.rb
+++ db/migrate/20250430135151_add_columns_to_user_idp_plan.rb
@@ -2,9 +2,7 @@
 
 class AddColumnsToUserIdpPlan < ActiveRecord::Migration[7.1]
   def change
-    # rubocop:disable Rails/BulkChangeTable
     add_column :user_idp_plans, :completed_at, :datetime
     add_column :user_idp_plans, :started_at, :datetime
-    # rubocop:enable Rails/BulkChangeTable
   end
 end
