c95b12f91757242fdb2d83f3e2ed9cb9578da55f
Fedor Tarasov <tarassovf@gmail.com>
2025-05-27 12:52:12 +0400

    Hotfix: fix campaign factor form to search in client scope (#6445)
    * Hotfix: fix campaign factor form to search in client scope
    
    * use params as source of campaign ids

--- app/types/api/v2/data_report/types_forms/campaign_score_form.rb
+++ app/types/api/v2/data_report/types_forms/campaign_score_form.rb
@@ -9,7 +9,11 @@ module Api::V2::DataReport
       validate :factor_is_related_to_client
 
       def factor_is_related_to_client
-        factor = CampaignFactor.find_by(code: factor_code)
+        campaign_ids = context[:campaign_ids] ||
+                       Project.where(id: context[:project_ids]).map(&:project_campaign_ids).flatten
+
+        factor = CampaignFactor.find_by(code: factor_code, campaign_id: campaign_ids)
+
         return errors.add(:factor_code, I18n.t('administration.data_reports.errors.not_found')) if factor.nil?
 
         if factor.campaign.client.id != context[:client_id]
