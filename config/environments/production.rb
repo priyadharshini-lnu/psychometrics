ae99f03aec8b46d943559551d81f0bf7d823cf33
Rohan Pujari <rohanpujari1992@gmail.com>
2025-07-21 10:27:38 +0400

    Hotfix/1710342 improve server boot time (#6834)
    * Improve server boot time (#6826)
    
    * Improve server boot time
    
    * Fix rubocop
    
    * Decrease rediness and liveness probe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  Rails.autoloaders.main.do_not_eager_load(config.root.join('app/types'))
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = Logger::Formatter.new
