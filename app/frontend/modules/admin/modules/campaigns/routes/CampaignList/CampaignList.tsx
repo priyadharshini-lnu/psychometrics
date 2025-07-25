c2001644c9c5b4e2868e82145b2e51471dbd6446
ruthvik-mandapati <mandapati.ruthvik@mercer.com>
2025-07-25 19:35:19 +0530

    Hotfix: Missing translations on table columns (#6878)
    * Update CampaignList.tsx
    
    * localise client type in clients table

--- app/frontend/modules/admin/modules/campaigns/routes/CampaignList/CampaignList.tsx
+++ app/frontend/modules/admin/modules/campaigns/routes/CampaignList/CampaignList.tsx
@@ -12,7 +12,6 @@ import {
   Space,
 } from 'antd'
 import { MoreOutlined } from '@ant-design/icons'
-import capitalize from 'lodash/capitalize'
 import map from 'lodash/map'
 import { MenuItem } from '~/interfaces/Antd'
 import dayjs from '~/utils/dayjs'
@@ -191,7 +190,7 @@ const CampaignListComponent: React.FC<Props> = ({
             <Column
               title={I18n.t('administration.campaigns.listing.status')}
               key="status"
-              render={({ status }) => capitalize(status)}
+              render={({ status }) => I18n.t(`administration.campaigns.filters.${status}`)}
               filterMultiple={false}
               filters={map(STATUSES, status => ({
                 text: I18n.t(`administration.campaigns.filters.${status}`),
