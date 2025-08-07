106c666a320491f93aeb073f23faa46b9fdb2619
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2025-08-07 16:15:44 +0530

    user-story/1671895: Default participants options settings for 360 campaign (#6950)

--- app/models/threesixty/option.rb
+++ app/models/threesixty/option.rb
@@ -4,7 +4,8 @@ module Threesixty
   class Option < ApplicationRecord
     audited
 
-    DEFAULT_PARTICIPANTS = { manager: {}, subject: {}, evaluator: {}, global: {} }.freeze
+    DEFAULT_PARTICIPANTS = { manager: {}, subject: { can_evaluate_self: true },
+                             evaluator: {}, global: { can_not_edit_evaluation: true } }.freeze
     DEFAULT_REPORTS = { access: {}, approval: {}, availability: { conditions: [] } }.freeze
 
     belongs_to :threesixty_campaign, class_name: 'Threesixty::Campaign'
