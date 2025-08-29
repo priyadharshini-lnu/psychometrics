cb122a1f5cf971c8c85ccfa0716afb2f8cf1900e
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2025-08-29 12:36:46 +0530

    chore: apply RuboCop auto-corrections to migration file

--- db/migrate/20250804045044_remove_dependencies_columns_from_campaign_ai_artifacts.rb
+++ db/migrate/20250804045044_remove_dependencies_columns_from_campaign_ai_artifacts.rb
@@ -3,10 +3,8 @@
 class RemoveDependenciesColumnsFromCampaignAIArtifacts < ActiveRecord::Migration[7.1]
   def change
     # Bulk doesn't allow adding column types making t.remove (without type) is not reversible
-    # rubocop:disable Rails/BulkChangeTable
     remove_column :campaign_ai_artifacts, :assessments, :jsonb, default: []
     remove_column :campaign_ai_artifacts, :datasheet_columns, :jsonb, default: []
     remove_column :campaign_ai_artifacts, :campaign_factors, :jsonb, default: []
-    # rubocop:enable Rails/BulkChangeTable
   end
 end
