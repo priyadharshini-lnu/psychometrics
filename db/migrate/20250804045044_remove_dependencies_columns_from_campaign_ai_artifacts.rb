0b276b26868e719a597ece16ec1e57c8ae7cf9e0
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2025-08-29 13:02:12 +0530

    Exclude RemoveDependenciesColumnsFromCampaignAIArtifacts migratin from rubocop linting

--- db/migrate/20250804045044_remove_dependencies_columns_from_campaign_ai_artifacts.rb
+++ db/migrate/20250804045044_remove_dependencies_columns_from_campaign_ai_artifacts.rb
@@ -3,8 +3,10 @@
 class RemoveDependenciesColumnsFromCampaignAIArtifacts < ActiveRecord::Migration[7.1]
   def change
     # Bulk doesn't allow adding column types making t.remove (without type) is not reversible
+    # rubocop:disable Rails/BulkChangeTable
     remove_column :campaign_ai_artifacts, :assessments, :jsonb, default: []
     remove_column :campaign_ai_artifacts, :datasheet_columns, :jsonb, default: []
     remove_column :campaign_ai_artifacts, :campaign_factors, :jsonb, default: []
+    # rubocop:enable Rails/BulkChangeTable
   end
 end
