5cfef2a1a8e308fc5c4d2a9cfc9691e252ed7d73
Melvin Thomas <melvin.thomas@mercer.com>
2025-09-24 21:04:09 +0530

    Rubocop

--- db/migrate/20250924140546_add_digest_delivery_mode_to_report_approval_settings.rb
+++ db/migrate/20250924140546_add_digest_delivery_mode_to_report_approval_settings.rb
@@ -2,8 +2,8 @@
 
 class AddDigestDeliveryModeToReportApprovalSettings < ActiveRecord::Migration[7.1]
   def change
-    change_table :report_approval_settings, bulk:true do |t|
-      t.string  :digest_delivery_mode, default: 'immediate', null: true
+    change_table :report_approval_settings, bulk: true do |t|
+      t.string :digest_delivery_mode, default: 'immediate', null: true
       t.datetime :last_digest_sent_at, null: true
     end
   end
