3f479c43a759234ca7954c0adc15f2eb2acd50b6
Sharat T Oommen <sharatoommen94@gmail.com>
2025-09-25 20:17:57 +0530

    fix rtl for default local (#7221)

--- spec/mailers/communication_email_mailer_spec.rb
+++ spec/mailers/communication_email_mailer_spec.rb
@@ -26,4 +26,66 @@ RSpec.describe CommunicationEmailMailer do
       expect(mail.body.encoded).to include('Bienvenido')
     end
   end
+
+  describe 'RTL logic' do
+    let(:user) { create(:user, :with_project_membership) }
+    let(:communication) { create(:communication) }
+    let(:communication_email) { create(:communication_email, user: user, communication: communication) }
+
+    context 'when user locale is Arabic but template is not' do
+      before do
+        user.user_profile.update!(locale: 'ar')
+        Mobility.with_locale(:en) do
+          communication.update!(subject: 'Hello', body: '<p>Welcome</p>')
+        end
+      end
+
+      it 'does not apply RTL styling' do
+        mail = described_class.create(communication_email.id)
+        expect(mail.body.encoded).not_to include('dir="rtl"')
+      end
+    end
+
+    context 'when communication template is Arabic but user locale is not' do
+      before do
+        user.user_profile.update!(locale: 'en')
+        Mobility.with_locale(:ar) do
+          communication.update!(subject: 'مرحبا', body: '<p>أهلا وسهلا</p>')
+        end
+      end
+
+      it 'does not apply RTL styling' do
+        mail = described_class.create(communication_email.id)
+        expect(mail.body.encoded).not_to include('dir="rtl"')
+      end
+    end
+
+    context 'when both user locale and template are Arabic' do
+      before do
+        user.user_profile.update!(locale: 'ar')
+        Mobility.with_locale(:ar) do
+          communication.update!(subject: 'مرحبا', body: '<p>أهلا وسهلا</p>')
+        end
+      end
+
+      it 'applies RTL styling' do
+        mail = described_class.create(communication_email.id)
+        expect(mail.body.encoded).to include('dir="rtl"')
+      end
+    end
+
+    context 'when neither user locale nor template is Arabic' do
+      before do
+        user.user_profile.update!(locale: 'en')
+        Mobility.with_locale(:en) do
+          communication.update!(subject: 'Hello', body: '<p>Welcome</p>')
+        end
+      end
+
+      it 'does not apply RTL styling' do
+        mail = described_class.create(communication_email.id)
+        expect(mail.body.encoded).not_to include('dir="rtl"')
+      end
+    end
+  end
 end
