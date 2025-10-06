5f2563f7bda84cf7d2d508d61fb8bdeef32120e1
Melvin Thomas <melvin.thomas@mercer.com>
2025-10-06 19:31:36 +0530

    WIP

---
+++ app/frontend/modules/admin/modules/client/routes/Client/routes/Project/routes/LicenseList/LicenseTable.tsx
@@ -0,0 +1,151 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect, ConnectedProps } from 'react-redux'
+import { MenuProps, Switch } from 'antd'
+import { RootState } from '~/modules/admin/core/rootReducers'
+import { Resource, useResourceContext } from '~/modules/admin/components/Resource'
+import { License } from '~/modules/admin/modules/client/core/licenses'
+import { openModal } from '~/modules/admin/core/ui/modals'
+import { get as getCurrentUser, isSuperAdmin } from '~/core/currentUser'
+import { UpdateResource } from '~/hooks/useResources/interfaces'
+import ConditionalDropdown from '~/components/ConditionalDropdown'
+
+const { I18n } = window
+
+const connecter = connect(
+  (state: RootState) => ({
+    currentUser: getCurrentUser(state),
+  }),
+  {
+    openModal,
+  },
+)
+
+type PropsFromRedux = ConnectedProps<typeof connecter>
+type Props = PropsFromRedux
+
+const ClientLicensesTableComponent: React.FC<Props> = ({
+  currentUser,
+  openModal,
+}) => {
+  const { resource } = useResourceContext<License>()
+
+  return (
+    <Resource.Table pagination>
+      <Resource.Column<License>
+        title={I18n.t('common.column.id')}
+        id="id"
+        dataIndex="id"
+        width={30}
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.enabled')}
+        id="disabledStatus"
+        dataIndex="disabled"
+        width={30}
+        render={(_, { id, enabled }) => (
+          <Switch checked={enabled} onChange={value => resource.updateResource({ id, enabled: value })} />
+        )
+        }
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.report_family')}
+        id="report_family_id"
+        dataIndex={['reportFamily', 'name']}
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.type')}
+        id="type"
+        dataIndex="type"
+        render={(_, { type }) => I18n.t(`licenses.types.${type}`)}
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.used_number')}
+        id="used_number"
+        dataIndex="usedNumber"
+        render={(_, { usedNumber, number }) => I18n.t('licenses.used_out_of', {
+          used: usedNumber - usedOveruseNumber(usedNumber, number),
+          total: number,
+        })}
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.overuse_number')}
+        id="overuse_number"
+        dataIndex="overuseNumber"
+        render={(_, { overuseNumber, usedNumber, number }) => I18n.t('licenses.used_out_of', {
+          used: usedOveruseNumber(usedNumber, number),
+          total: overuseNumber,
+        })}
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.start_date')}
+        id="start_date"
+        dataIndex="startDate"
+        sorter
+      />
+      <Resource.Column<License>
+        title={I18n.t('licenses.end_date')}
+        id="end_date"
+        dataIndex="endDate"
+        sorter
+      />
+      {isSuperAdmin(currentUser)
+          && (
+            <Resource.Column<License>
+              title={I18n.t('common.column.action')}
+              id="actions"
+              key="actions"
+              render={license => (
+                <ConditionalDropdown
+                  menu={
+                    getActionsMenuProps({
+                      license,
+                      openModal,
+                      updateResource: resource.updateResource,
+                    })
+                  }
+                />
+              )}
+            />
+          )}
+    </Resource.Table>
+  )
+}
+
+interface ActionMenuData {
+  license: License
+  updateResource: UpdateResource<License>
+  openModal: (modalName: string, modalProps: unknown) => void
+}
+
+const getActionsMenuProps = ({
+  license, updateResource, openModal,
+}: ActionMenuData): MenuProps => {
+  const menuItems = [
+    { key: 'edit', label: I18n.t('common.actions.edit') },
+    {
+      key: 'show',
+      label: (
+        <Link to={`${license.id}/license_usages`}>
+          {I18n.t('license_usage.usage_overview')}
+        </Link>
+      ),
+    },
+  ]
+
+  const handleMenuClick = ({ key }) => {
+    if (key === 'edit') {
+      return openModal('LicenseFormModal', {
+        updateLicense: updateResource, license,
+      })
+    }
+  }
+
+  return ({ items: menuItems, onClick: handleMenuClick })
+}
+
+function usedOveruseNumber (used: number, total: number): number {
+  return total >= used ? 0 : used - total
+}
+
+export const ClientLicensesTable = connecter(ClientLicensesTableComponent)
---
+++ app/frontend/modules/admin/modules/client/routes/Client/routes/Project/routes/LicenseList/LicensesList.tsx
@@ -0,0 +1,78 @@
+
+import React from 'react'
+import { connect, ConnectedProps } from 'react-redux'
+import {
+  Button,
+} from 'antd'
+// import { PlusOutlined } from '@ant-design/icons'
+import { useParams } from 'react-router-dom'
+import { Resource } from '~/modules/admin/components/Resource'
+import { LicenseTR } from '~/modules/admin/modules/client/core/licenses'
+import { RootState } from '~/modules/admin/core/rootReducers'
+import Modals from '~/modules/admin/components/Modals'
+import { openModal } from '~/modules/admin/core/ui/modals'
+import { get as getCurrentUser, isSuperAdmin } from '~/core/currentUser'
+// import { LicenseFormModal } from './LicenseFormModal'
+import { ClientLicensesTable } from './LicenseTable'
+import { UserOutlined } from '@ant-design/icons'
+
+const { I18n } = window
+
+// const MODALS = {
+//   LicenseFormModal,
+// }
+
+const connecter = connect(
+  (state: RootState) => ({
+    currentUser: getCurrentUser(state),
+  }),
+  {
+    openModal,
+  },
+)
+
+export type PropsFromRedux = ConnectedProps<typeof connecter>
+type Props = PropsFromRedux
+
+const LicenseList: React.FC<Props> = ({
+  currentUser, openModal,
+}) => {
+  const { clientId } = useParams() as { clientId: string }
+  const config = {
+    trackUrl: true,
+    responseType: LicenseTR,
+    basePath: `clients/${clientId}`,
+    apiConfig: {
+      include: ['report_family'],
+      include_meta: ['permissions'],
+      fields: { report_families: ['id', 'name'] },
+    },
+  }
+
+  return (
+    <>
+      <Resource config={config} name="licenses">
+        {/* <Resource.Filter placeholder={I18n.t('common.actions.search')} name="report_family_name_cont">
+          {isSuperAdmin(currentUser)
+              && (
+                <Button
+                  type="primary"
+                  disabled={false}
+                  onClick={() => {
+                    openModal('LicenseFormModal')
+                  }}
+                >
+                  <PlusOutlined />
+                  {I18n.t('frontend.clients.actions.create.create_license')}
+                </Button>
+              )}
+        </Resource.Filter> */}
+        <ClientLicensesTable />
+
+        {/* <Modals modals={MODALS} /> */}
+      </Resource>
+    </>
+  )
+}
+
+export default connecter(LicenseList)
