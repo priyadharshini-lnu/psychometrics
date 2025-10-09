3f6815c86963c35207ba711ffde4ad234607d23d
farhad-bharucha <farhad.k.khurshed@mercer.com>
2025-10-09 15:48:22 +0530

    Task/1860952 [BE] API changes to support advanced filter options on stats page (#7199)
    * task/1859412 ui changes advanced filter
    
    * task/1860952 - API changes to support advanced filter options on stats page
    
    * task/1860952 - API changes to support advanced filter options on stats page
    
    * task/1860952 - API changes to support advanced filter options on stats page
    
    * task/1860952 minor change to support null values as possible option for data sheet filters
    
    * frontend updates
    
    * ui updates for Advanced filter options on stats page
    
    * change to campaign_users_active_in
    
    * Changing stats controller index to a post request
    
    * update the Ui with post call
    
    ---------
    
    Co-authored-by: mohit dutt <mohit.dutt@mercer.com>

--- app/core/campaigns/status_timeseries_query.rb
+++ app/core/campaigns/status_timeseries_query.rb
@@ -2,13 +2,32 @@
 
 module Campaigns
   class StatusTimeseriesQuery < Rectify::Query
-    private_attr_reader :campaign, :date_from, :date_to, :campaign_users_active_in
+    private_attr_reader :campaign, :date_from, :date_to, :campaign_users_active_in, :datasheet_filters,
+                        :filtered_user_ids
 
-    def initialize(campaign, date_from, date_to, campaign_users_active_in)
+    def initialize(campaign, date_from, date_to, campaign_users_active_in, datasheet_filters)
       @campaign = campaign
       @date_from = date_from
       @date_to = date_to
       @campaign_users_active_in = campaign_users_active_in
+      @datasheet_filters = datasheet_filters
+      @filtered_user_ids = get_filtered_user_ids
+    end
+
+    def get_filtered_user_ids
+      campaign_user_data = CampaignUser.joins(:user).
+                           where(campaign_id: campaign.id, active: campaign_users_active_in).
+                           select('campaign_users.id, users.email')
+
+      valid_users_ids = campaign_user_data.map(&:id)
+
+      if @datasheet_filters.present?
+        datasheet_rows = campaign.project_datasheet.rows.map { |r| SheetRows::GetData.call!(r).except(:id) }
+        valid_users_ids = campaign_user_data.select do |cu|
+          campaign_user_passes_datasheet_filter?(cu, datasheet_rows)
+        end.map(&:id)
+      end
+      valid_users_ids
     end
 
     def sql
@@ -29,7 +48,7 @@ module Campaigns
         FROM
           campaign_users
         WHERE
-          campaign_id = :id and active in (:active)
+          campaign_id = :id and active in (:active) and id IN (:filtered_user_ids)
         GROUP BY
           1
       ),
@@ -41,7 +60,7 @@ module Campaigns
         FROM
           campaign_users
         WHERE
-          campaign_id = :id and active in (:active)
+          campaign_id = :id and active in (:active) and id IN (:filtered_user_ids)
         GROUP BY
           1
       )
@@ -61,12 +80,35 @@ module Campaigns
     end
 
     def params
-      { id: campaign.id, date_from: date_from, date_to: date_to, active: campaign_users_active_in }
+      { id: campaign.id,
+        date_from: date_from,
+        date_to: date_to,
+        active: campaign_users_active_in,
+        filtered_user_ids: filtered_user_ids }
     end
 
     def period
       days = (date_to.to_datetime - date_from.to_datetime).to_i
       @period ||= days >= 1 ? 'days' : 'hours'
+    end
+
+    def get_datasheet_row_for_user(campaign_user_data, datasheet_rows)
+      datasheet_rows.find do |row|
+        %w[Email email].any? { |key| row[key].to_s.strip.casecmp?(campaign_user_data.email.to_s.strip) }
+      end
+    end
+
+    def campaign_user_passes_datasheet_filter?(filtered_campaign_user, datasheet_rows)
+      return true if @datasheet_filters.blank?
+
+      user_datasheet_row = get_datasheet_row_for_user(filtered_campaign_user, datasheet_rows)
+      user_datasheet_row = {} if user_datasheet_row.blank?
+
+      @datasheet_filters.each do |field, valid_options|
+        row_value = user_datasheet_row[field.to_s]
+        return false if valid_options.exclude?(row_value)
+      end
+      true
     end
   end
 end
