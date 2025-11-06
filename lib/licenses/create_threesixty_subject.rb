ada914e22ae99622be710ec56dd72fb35c67f89f
Melvin Thomas <melvin.thomas@mercer.com>
2025-11-06 12:20:23 +0530

    License check for 360 licenses

--- lib/licenses/create_threesixty_subject.rb
+++ lib/licenses/create_threesixty_subject.rb
@@ -2,7 +2,7 @@
 
 module Licenses
   class CreateThreesixtySubject < BaseCommand
-    attr_reader :client, :user, :campaign
+    attr_reader :client, :user, :campaign, :project_license
 
     def initialize(context)
       @client       = context[:campaign].client
@@ -23,12 +23,25 @@ module Licenses
         raise Errors::LicenseError.new(client, nil, user, message)
       end
 
+      if license.is_project_specific?
+        project_license = fetch_project_license?(license)
+        unless project_license&.enough_licenses?
+          raise Licenses::NotEnoughError,
+                I18n.t('licenses.project_limit_reached', license_name: 'ThreeSixty License')
+        end
+      end
+
       extras = { subject_name: user.decorate.full_name, subject_email: user.email, campaign_name: campaign.name }
-      license.license_usages.create!(client: client, user: user, campaign: campaign, extras: extras)
+      license.license_usages.create!(client: client, user: user, campaign: campaign, project: campaign.project,
+                                     project_license: project_license, extras: extras)
     end
 
     def fetch_available_license
       client.licenses.available.where(type: :threesixty).order(end_date: :asc).first
+    end
+
+    def fetch_project_license?(license)
+      ProjectLicense.enabled.find_by(license_id: license.id, project_id: campaign.project.id)
     end
   end
 end
