2b92fa88f7c7791605ad8c45487f088b3dc6a6b0
Sritabh Priyadarshi <43126956+sritabh@users.noreply.github.com>
2025-11-12 12:29:58 +0530

    Hotfix/xxxxxxx: Fixed pgvector hnsw indexing not available migration failure (#7465)
    * Hotfix/XXXXXXX-Fixed-pgvector-hnsw-indexing-not-available
    
    * Skip creating different indexing

--- db/migrate/20251015075724_create_vector_embeddings.rb
+++ db/migrate/20251015075724_create_vector_embeddings.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Migration has been modified to deal with different pgvector versions for index creation.
 class CreateVectorEmbeddings < ActiveRecord::Migration[7.1]
   def change
     create_table :vector_embeddings do |t|
@@ -8,6 +9,13 @@ class CreateVectorEmbeddings < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
-    add_index :vector_embeddings, :embedding, using: :hnsw, opclass: :vector_cosine_ops
+    begin
+      # HNSW with cosine ops (pgvector >= 0.5.0)
+      add_index :vector_embeddings, :embedding, using: :hnsw, opclass: :vector_cosine_ops
+    rescue ActiveRecord::StatementInvalid => e
+      # rubocop:disable CustomRubocops/AvoidActiveRecordInMigrations
+      Rails.logger.warn { "Could not create HNSW index with cosine ops. Error: #{e.message}" }
+      # rubocop:enable CustomRubocops/AvoidActiveRecordInMigrations
+    end
   end
 end
