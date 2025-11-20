c220e4073156d3344d2fe5b50508b8806ddabe66
Melvin Thomas <melvin.thomas@mercer.com>
2025-11-20 07:30:51 +0530

    Fixes ransack filtering for add project license modal

--- app/models/license.rb
+++ app/models/license.rb
@@ -60,7 +60,7 @@ class License < ApplicationRecord
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[id]
+    %w[id is_project_specific]
   end
 
   def self.ransackable_associations(_auth_object = nil)
