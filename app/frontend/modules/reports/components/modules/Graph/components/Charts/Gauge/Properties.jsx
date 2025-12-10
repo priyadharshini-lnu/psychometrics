92fb92753a4584472f27554e5acb0ea253514783
shivaraja-tte <88094975+shivaraja-tte@users.noreply.github.com>
2025-12-10 16:00:16 +0530

    hotfix/2033172: fix gauge chart in rtl (#7623)
    * hotfix/2033172: fix gauge chart in rtl
    
    * remove startAngle and endAngle from chartoptions file

--- app/frontend/modules/reports/components/modules/Graph/components/Charts/Gauge/Properties.jsx
+++ app/frontend/modules/reports/components/modules/Graph/components/Charts/Gauge/Properties.jsx
@@ -16,12 +16,6 @@ const Properties = ({ modules }) => {
     })
   }
 
-  const changeLabelPosition = (val) => {
-    updateAll((item) => {
-      item.props.labelVerticalPosition = val
-    })
-  }
-
   const changeSize = (val) => {
     updateAll((item) => {
       item.props.speedometerSize = `${val}%`
@@ -148,107 +142,6 @@ const Properties = ({ modules }) => {
       <div className={styles.block}>
         Speedometer Size
         <ChoicesInput value={parseInt(speedometerSize, 10)} onChange={changeSize} minValue={40} maxValue={200} />
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.block}>
-        Max Value
-        <ChoicesInput
-          value={parseInt(maxValue, 10) || 6}
-          onChange={changeMaxValue}
-          minValue={0}
-          maxValue={1000}
-        />
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.block}>
-        Width
-        <ChoicesInput
-          value={gaugeWidth}
-          onChange={changeWidthValue}
-          minValue={0}
-          maxValue={100}
-        />
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.block}>
-        Border
-        <ChoicesInput
-          value={gaugeBorder}
-          onChange={changeBorderValue}
-          minValue={0}
-          maxValue={1000}
-        />
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.block}>
-        Background Color
-        <ColorPicker
-          getValueInHexFormat
-          value={speedometerBackgroundColor}
-          onChange={color => changeColorProperty('speedometerBackgroundColor', color)}
-        />
-      </div>
-      <div className={styles.block}>
-        Border Color
-        <ColorPicker
-          getValueInHexFormat
-          value={borderColor}
-          onChange={color => changeColorProperty('borderColor', color)}
-        />
-      </div>
-      <hr className={styles.divider} />
-      <div className="margin-top-10">
-        <HintCheckbox
-          label="Hide Label"
-          checked={model.props.hideLabel}
-          onChange={() => handleCheckControl('hideLabel')}
-          hints={['It will hide label on the top of chart', 'It will show label on the top of chart']}
-        />
-      </div>
-      <div className="margin-top-10">
-        <HintCheckbox
-          label="Hide Markers"
-          checked={model.props.hideMarkers}
-          onChange={() => handleCheckControl('hideMarkers')}
-          hints={['It will hide markers from the chart', 'It will show markers on the chart']}
-        />
-      </div>
-      <div className="margin-top-10">
-        <HintCheckbox
-          label="Percentage"
-          checked={model.props.gaugePercentage}
-          onChange={() => handleCheckControl('gaugePercentage')}
-          hints={['It will show value as score', 'It will show value as percentage score']}
-        />
-      </div>
-      <div className="margin-top-10">
-        <HintCheckbox
-          label="Rounded"
-          checked={model.props.rounded}
-          onChange={() => handleCheckControl('rounded')}
-          hints={['It will show rounded corners', 'It will show normal corners']}
-        />
-      </div>
-      <hr className={styles.divider} />
-
-      <div className={styles.block}>
-        Label Vertical Position
-        <ChoicesInput
-          value={labelVerticalPosition}
-          onChange={changeLabelPosition}
-          minValue={-200}
-          maxValue={200}
-        />
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.block}>
-        Speedometer Size
-        <ChoicesInput
-          value={parseInt(speedometerSize, 10)}
-          onChange={changeSize}
-          minValue={40}
-          maxValue={200}
-        />
       </div>
       <hr className={styles.divider} />
       <div className={styles.block}>
