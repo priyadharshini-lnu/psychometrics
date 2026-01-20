c87ca89765ddf47fd8b3b4ee3f4bbab3bbc3c399
Rohan Pujari <rohanpujari1992@gmail.com>
2026-01-20 15:24:00 +0400

    Fix user search related sentry (#7870)

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class UserSerializer < Panko::Serializer
+  # NOTE: object is not always a User here, sometimes it is a SheetRow.
+  # Use safe navigation whenever you want to access any user properties.
+
   attributes :id, :first_name, :last_name, :email, :photo, :gender, :age
 
   delegate :user_profile, to: :object
-  delegate :gender, :age, to: :user_profile
+  delegate :gender, :age, to: :user_profile, allow_nil: true
 
   def photo
-    object.user_profile.photo&.url
+    object.user_profile&.photo&.url
   end
 end
