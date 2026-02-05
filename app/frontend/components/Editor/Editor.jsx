324d02bd076e06ae6303d7fd6377b60ee6ba182d
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2026-02-05 17:51:30 +0530

    Merge pull request #7999 from TheTalentEnterprise/hotfix/2238449_froala-editor
    hotfix/2238449: Upgrade froala editor to latest to fix color issue

--- app/frontend/components/Editor/Editor.jsx
+++ app/frontend/components/Editor/Editor.jsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useState } from 'react'
+import { useRef, useEffect } from 'react'
 import 'codemirror/lib/codemirror.css'
 import CodeMirror from 'codemirror'
 import 'codemirror/mode/xml/xml'
@@ -108,13 +108,11 @@ function Editor ({
     direction,
   }
 
-  const [isInitialized, setIsInitialized] = useState(false)
-
   config = { ...config, ...configOverrides }
 
   withPipedText && config.toolbarButtons.unshift('pipedText')
 
-  const ref = createRef()
+  const ref = useRef(null)
 
   useEffect(() => {
     if (ref.current?.editor?.opts) {
@@ -122,21 +120,9 @@ function Editor ({
     }
   }, [type, details])
 
-  useEffect(() => {
-    async function initPlugins () {
-      // Refer: https://github.com/froala/react-froala-wysiwyg/issues/410#issuecomment-2627465406
-      // Import  Froala Editor plugin lazily;
-      await import('froala-editor/js/plugins.pkgd.min')
-      setIsInitialized(true)
-    }
-    if (!isInitialized) {
-      initPlugins()
-    }
-  })
-
   return (
     <div className={className}>
-      {isInitialized && <FroalaEditor ref={ref} config={config} model={content} onModelChange={handleContentChange} />}
+      <FroalaEditor ref={ref} config={config} model={content} onModelChange={handleContentChange} />
     </div>
   )
 }
