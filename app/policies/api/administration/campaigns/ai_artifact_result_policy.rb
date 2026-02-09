da2ce95aea12563b0e8e9ce2f663c44792e9b7bb
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2026-02-09 18:44:55 +0530

    hotfix: 2246450 : Allow admin user with AI artifacts permission to see and manage ai artifacts (#8014)
    * Allow admin user with AI artifacts permission to see and manage ai artifacts

--- app/policies/api/administration/campaigns/ai_artifact_result_policy.rb
+++ app/policies/api/administration/campaigns/ai_artifact_result_policy.rb
@@ -5,33 +5,67 @@ module Api
     module Campaigns
       class AIArtifactResultPolicy < ::Administration::UserPolicy
         def index?
-          @user.is?(:superadmin)
+          feature_enabled? && can_view?
         end
 
         def update?
-          @user.is?(:superadmin)
+          feature_enabled? && can_manage?
         end
 
         def show?
-          @user.is?(:superadmin)
+          feature_enabled? && can_view?
         end
 
         def generate?
-          @user.is?(:superadmin)
+          feature_enabled? && can_manage?
         end
 
         def test_generate?
-          @user.is?(:superadmin)
+          feature_enabled? && can_manage?
         end
 
         def bulk_generate?
-          @user.is?(:superadmin)
+          feature_enabled? && can_manage?
         end
 
         class Scope < Administration::BasePolicy::Scope
           def resolve
             scope.where(campaign_id: campaign_id)
           end
+        end
+
+        private
+
+        def feature_enabled?
+          campaign&.project&.client&.feature_enabled?(:ai_assistants) &&
+            campaign&.project&.project_feature_enabled?(:ai_assistants)
+        end
+
+        def can_view?
+          superadmin? || permission_granted?('view')
+        end
+
+        def can_manage?
+          superadmin? || permission_granted?('manage')
+        end
+
+        def permission_granted?(permission)
+          has_permission?(
+            'ai_artifacts',
+            permission,
+            project_id: campaign&.project_id,
+            campaign_id: campaign&.id
+          )
+        end
+
+        def campaign
+          @campaign ||= if campaign_id.present?
+                          Campaign.find_by(id: campaign_id)
+                        end
+        end
+
+        def superadmin?
+          @user.is?(:superadmin)
         end
       end
     end
