86f0e81ae5dccdd270cb8f81cf4b7da86883acf5
Shaiju Edakulangara <162425744+shaiju-tte@users.noreply.github.com>
2026-02-20 23:53:57 +0530

    Save assessment completion after media is saved (#8102)

--- lib/imports/assessments/result_import_user_result.rb
+++ lib/imports/assessments/result_import_user_result.rb
@@ -31,9 +31,9 @@ module Imports
 
         if errors.blank?
           user_results.each do |user_result|
-            user_result.user_assessment.save!
             user_result.save!
             if user_result.completed?
+              user_result.user_assessment.save!
               ::UsersResults::SaveScoringWithCallbacksJob.perform_later(user_result, user_result.user)
             end
           end
