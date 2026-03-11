a3335423d9b5724ccd7eab6c9b60ad7f80e0d913
Shaiju Edakulangara <162425744+shaiju-tte@users.noreply.github.com>
2026-03-11 11:56:41 +0530

    Revert "Bugfix/2256320: Assessment Centre date not getting populated in the r…" (#8190)
    This reverts commit bae70e425e4eaba9473890c0fd05bb3d0586eebf.

--- app/core/threesixty/piped_text/branches/date_time_fields/assessment_center.rb
+++ app/core/threesixty/piped_text/branches/date_time_fields/assessment_center.rb
@@ -12,16 +12,11 @@ module Threesixty
           private
 
           def attended_date
-            query = WorkshopSubject.joins(:workshop).
-                    where(user_id: subject.id,
-                          campaign_id: context[:campaign]&.id).
-                    where(attendance_status: %i[on_time late])
-
-            if campaign_assessment_group_id.present?
-              query = query.where(workshop: { campaign_assessment_group_id: campaign_assessment_group_id })
-            end
-
-            workshop_subject = query.order(:created_at).last
+            workshop_subject = WorkshopSubject.joins(:workshop).
+                               where(user_id: subject.id,
+                                     campaign_id: context[:campaign]&.id).
+                               where(attendance_status: %i[on_time late]).
+                               order(:created_at).last
 
             workshop_subject&.workshop&.start_time&.strftime(params['f'])
           end
@@ -32,10 +27,6 @@ module Threesixty
 
           def campaign
             context[:campaign]
-          end
-
-          def campaign_assessment_group_id
-            context[:users_result]&.user_assessment&.campaign_assessment&.campaign_assessment_group_id
           end
         end
       end
