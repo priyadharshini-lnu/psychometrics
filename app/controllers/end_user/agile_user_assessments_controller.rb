21ca7a72b9ec041bff93ec7f447ca097d61c78ac
Aashish Saini <aashish.saini@mercer.com>
2026-03-25 15:04:59 +0530

    Bugfix/Allow fullscreen mode (#8259)
    * allow fullscreen mode
    
    * rubocop fixeds
    
    * changed approach

--- app/controllers/end_user/agile_user_assessments_controller.rb
+++ app/controllers/end_user/agile_user_assessments_controller.rb
@@ -4,12 +4,17 @@ class EndUser::AgileUserAssessmentsController < ApplicationController
   include ::Threesixty::InitialState
   include AgileUserResult
 
+  prepend_before_action :set_fullscreen_permissions_policy
   prepend_before_action :authenticate_anonymous_user!
   before_action :set_user_result
   before_action :can_start_based_on_sequencing, only: %i[show update events]
   initial_state_for :show
 
   private
+
+  def set_fullscreen_permissions_policy
+    response.set_header('Permissions-Policy', 'fullscreen=(self)')
+  end
 
   def can_start_based_on_sequencing
     return if UserAssessments::CanStartBasedOnSequencing.call!(@user_assessment)
