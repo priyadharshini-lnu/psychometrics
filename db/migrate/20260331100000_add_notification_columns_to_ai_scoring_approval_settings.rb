efc3572a606f4173e2bcb0362351ebc76d40d78d
Vijay Vinod <48003440+vijay2136@users.noreply.github.com>
2026-04-10 11:20:03 +0530

    Fixed approval settings migration (#8402)

--- db/migrate/20260331100000_add_notification_columns_to_ai_scoring_approval_settings.rb
+++ db/migrate/20260331100000_add_notification_columns_to_ai_scoring_approval_settings.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddNotificationColumnsToAiScoringApprovalSettings < ActiveRecord::Migration[6.0]
+class AddNotificationColumnsToAIScoringApprovalSettings < ActiveRecord::Migration[8.0]
   def change
     change_table :ai_scoring_approval_settings, bulk: true do |t|
       t.bigint :approval_notification_user_ids, array: true, default: [], null: false
