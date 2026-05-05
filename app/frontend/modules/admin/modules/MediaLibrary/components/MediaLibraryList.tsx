2fb386c46d1fe32b6adf55ec1253dd5bcf27041b
Aashish Saini <aashish.saini@mercer.com>
2026-05-05 14:10:55 +0530

    Fix bug 2476015 page title (#8553)

--- app/frontend/modules/admin/modules/MediaLibrary/components/MediaLibraryList.tsx
+++ app/frontend/modules/admin/modules/MediaLibrary/components/MediaLibraryList.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Resource } from '~/modules/admin/components/Resource'
+import { useDocumentTitle } from '~/hooks/useDocumentTitle'
 import MediaLibraryTable from './MediaLibraryTable'
 
+const { I18n } = window
+
 const MediaLibraryList: React.FC = () => {
+  useDocumentTitle(I18n.t('admin.media_library_page_title'))
   const baseApiConfig = {
     trackUrl: true,
     apiConfig: {
