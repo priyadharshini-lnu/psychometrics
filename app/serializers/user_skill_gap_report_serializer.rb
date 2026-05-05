bbbd7937a497c5fd1268ef51dc5fd5825a4043a9
Fedor Tarasov <tarassovf@gmail.com>
2026-05-05 12:37:53 +0400

    Hotfix: fix skill gap report results serializing (#8559)
    * Hotfix: fix skill gap report results serializing
    
    * remove debug

--- app/serializers/user_skill_gap_report_serializer.rb
+++ app/serializers/user_skill_gap_report_serializer.rb
@@ -2,7 +2,7 @@
 
 class UserSkillGapReportSerializer < Panko::Serializer
   attributes :id, :status, :campaign_id, :pdf, :is_self, :results, :user, :report_url, :report_data,
-             :campaign_ai_artifact_results
+             :campaign_ai_artifact_results, :module_overrides, :campaign_factor_results
 
   has_one :report, serializer: ReportSerializer
 
@@ -28,6 +28,19 @@ class UserSkillGapReportSerializer < Panko::Serializer
     object.pdf_download_url
   end
 
+  def campaign_factor_results
+    object.campaign.campaign_factor_values.where(
+      user_id: object.user_id, campaign_factors: { public_visibility: true }
+    ).includes(:campaign_factor).map do |cfv|
+      {
+        code: cfv.campaign_factor.code,
+        value: cfv.value,
+        name: cfv.campaign_factor.name,
+        description: cfv.campaign_factor.description
+      }
+    end
+  end
+
   def campaign_ai_artifact_results
     results = Campaigns::AIArtifactResultsQuery.new(
       object.campaign_id, object.user_id
@@ -39,21 +52,31 @@ class UserSkillGapReportSerializer < Panko::Serializer
     ).to_a
   end
 
+  def module_overrides
+    Panko::ArraySerializer.new(
+      raw_module_overrides,
+      each_serializer: TextModuleOverrideSerializer
+    ).to_a
+  end
+
   private
 
   def current_user
     context[:current_user]
   end
 
-  def module_overrides
+  def raw_module_overrides
     TextModuleOverride.where(user_report_id: object.id)
   end
 
   def report
     ReportSerializer.new(
       context: {
-        module_overrides: module_overrides,
-        include: '**'
+        module_overrides: raw_module_overrides,
+        user_results: context[:user_results],
+        piped_text_context: context[:piped_text_context],
+        campaign: context[:campaign],
+        lang: context[:lang]
       }
     ).serialize(context[:report])
   end
