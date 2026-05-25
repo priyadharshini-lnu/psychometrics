c8b741962cd4a6c095f05260ad77a9190a884531
Prasanth Chaduvula <chaduvulaprasanth1998@gmail.com>
2026-05-25 16:51:21 +0530

    Hotfix/2573514: Fix Saville::SaveResultsAndReportsJob ArgumentError (#8711)
    * Fix Saville::SaveResultsAndReportsJob ArgumentError
    
    * Fix Saville::SaveResultsAndReportsJob ArgumentError spec

--- spec/controllers/webhooks/saville_controller_spec.rb
+++ spec/controllers/webhooks/saville_controller_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Webhooks::SavilleController, type: :controller do
 
     post :results, body: webhook_data
   end
+
+  it 'passes a String to SaveResultsAndReportsJob even when raw_post returns an IO::Stream::StringBuffer' do
+    xml_string = '<AssessmentResult xmlns="http://ns.hr-xml.org/2007-04-15"></AssessmentResult>'
+    io_stream_buffer = double('IO::Stream::StringBuffer', to_s: xml_string)
+
+    allow_any_instance_of(ActionDispatch::Request).to receive(:raw_post).and_return(io_stream_buffer)
+
+    expect(Saville::SaveResultsAndReportsJob).to receive(:perform_later).with(xml_string)
+
+    post :results
+  end
 end
