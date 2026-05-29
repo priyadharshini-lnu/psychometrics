4dcb9207b81ddbe83c8172c747a9017d651c3e2c
priyadharshini-lnu <priyadharshini.lnu@mercer.com>
2026-05-29 16:04:29 +0530

    Bugfix/2562343: Redirect URL session timeout fix (#8740)
    * bugfix/2562343-redirect-signin-fix
    
    * bugfix/2562343-reverted the devise failure app fix
    
    * bugfix/2562343-cookie-fix

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -41,7 +41,6 @@ Warden::Manager.before_logout do |_user, auth, opts|
   scope = opts[:scope]
   request = auth.request
 
-  auth.env['sso_session'] = request.session[:sso].present?
   request.session["#{scope}.id"] = nil
 end
 
