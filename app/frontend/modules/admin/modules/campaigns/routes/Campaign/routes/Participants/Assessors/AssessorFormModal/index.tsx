769bb66fab456af7225917bf61d8dfccf0c8a382
ruthvik-mandapati <mandapati.ruthvik@mercer.com>
2026-06-19 12:45:47 +0530

    add missing translation in add assessor modal (#8888)

--- app/frontend/modules/admin/modules/campaigns/routes/Campaign/routes/Participants/Assessors/AssessorFormModal/index.tsx
+++ app/frontend/modules/admin/modules/campaigns/routes/Campaign/routes/Participants/Assessors/AssessorFormModal/index.tsx
@@ -27,32 +27,32 @@ interface Props {
 }
 
 const { I18n } = window
-const localI18n = (code, params = {}) => I18n.t(`admin.assessor_modals_create_assessor_${code}`, params)
+const localI18n = (code, params = {}) => I18n.t(`admin.${code}`, params)
 
 const tableFields = [
   {
-    name: localI18n('sheet_subject_email'),
+    name: localI18n('subject_email'),
     key: 'subjectEmail',
   },
   {
-    name: localI18n('sheet_assessor_email'),
+    name: localI18n('assessor_email'),
     key: 'assessorEmail',
   },
   {
-    name: localI18n('sheet_assessor_first_name'),
+    name: localI18n('assessor_first_name'),
     key: 'assessorFirstName',
   },
   {
-    name: localI18n('sheet_assessor_last_name'),
+    name: localI18n('assessor_last_name'),
     key: 'assessorLastName',
   },
   {
-    name: localI18n('sheet_assessments'),
+    name: localI18n('assessments'),
     key: 'assessmentIds',
     type: 'MultiSelect',
     multiple: true,
     styles: { width: '200px' },
-    placeholder: localI18n('sheet_assessments_placeholder'),
+    placeholder: localI18n('assessments_placeholder'),
     values: ({ assessments }) => assessments.map(a => ({ key: a.id, value: a.name })),
   },
 ]
@@ -119,7 +119,7 @@ const AssessorFormModal: React.FC<Props & PropsFromRedux> = ({
   return (
     <Modal
       width={900}
-      title={localI18n('title')}
+      title={localI18n('assessor_create_assessor')}
       open
       onCancel={close}
       footer={[
