0578d3c8448f72d9e753c4c60710798bc779a574
Aashish Saini <aashish.saini@mercer.com>
2026-06-22 19:57:12 +0530

    Task/Fix Sentry 2670028, NoMethodError undefined method 'check_geo_restriction!' for nil (#8910)
    * Fix Sentry 2670028, NoMethodError
    
    * use tenant context

--- spec/api/v2/user_report_events_controller_spec.rb
+++ spec/api/v2/user_report_events_controller_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe Api::V2::Administration::UserReportEventsController, type: :reque
   let!(:superadmin) { create(:superadmin) }
   let!(:normal_user) { create(:user) }
   let!(:campaign) { create(:campaign) }
+  let!(:api_user) { create(:application_user) }
+  let!(:api_key) { create(:api_key, user: api_user) }
+  let(:authorization) { "Basic #{Base64.strict_encode64("#{api_key.key}:#{api_key.token}")}" }
+  let(:api_json_headers) do
+    {
+      'Authorization' => authorization,
+      'Accept' => 'application/vnd.api+json'
+    }
+  end
+
   before { sign_in(superadmin) }
 
   describe 'GET /api/v2/administration/user_report_events/export' do
@@ -45,6 +55,34 @@ RSpec.describe Api::V2::Administration::UserReportEventsController, type: :reque
            }
 
       expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  describe 'GET /api/v2/administration/user_report_events' do
+    it 'returns forbidden for invalid project_id string in query' do
+      get '/api/v2/administration/user_report_events',
+          params: {
+            query: {
+              project_id: 'ping -n 11 127.0.0.1'
+            }
+          },
+          headers: api_json_headers
+
+      expect(response).to have_http_status(:forbidden)
+      expect(JSON.parse(response.body)['error']).to be_present
+    end
+
+    it 'returns forbidden for invalid campaign_id string in query' do
+      get '/api/v2/administration/user_report_events',
+          params: {
+            query: {
+              campaign_id: 'bad-campaign-id'
+            }
+          },
+          headers: api_json_headers
+
+      expect(response).to have_http_status(:forbidden)
+      expect(JSON.parse(response.body)['error']).to be_present
     end
   end
 end
