994109a307cd962d8bb867f7ce9541be665b99cf
Vijay Vinod <48003440+vijay2136@users.noreply.github.com>
2026-07-01 11:07:10 +0530

    Task/2680213: Add pending email for scoring approval (#8947)
    * Added ability to send pending qc notification
    
    * Added specs
    
    * Show auto_approve status in the frontend
    
    * Minor fix
    
    * Spec fix

---
+++ app/mailers/scoring_approving/assessor_notification_mailer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module ScoringApproving
+  class AssessorNotificationMailer < ApplicationMailer
+    layout 'admin_email'
+
+    def notify(score_approval, user)
+      @score_approval = score_approval
+      @user = user
+      @project = @score_approval.project
+      @setting = @score_approval.setting
+      @review_role = @setting&.one_level_approve? ? 'approver' : 'assessor'
+      @review_stage = @setting&.one_level_approve? ? 'approval' : 'QC'
+      @url = administration_ai_scoring_approvals_all_url("#{@score_approval.id}/review")
+
+      send_email(
+        user,
+        subject: "Scoring ready for #{@review_stage}",
+        template_path: 'mailer/scoring_approving',
+        template_name: 'assessor_notification',
+        **admin_sender_attributes(@score_approval.project&.client)
+      )
+    end
+  end
+end
