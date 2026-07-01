688fa1d63c0afd691ed95b7ee55fc271264f98a3
farhad-bharucha <farhad.k.khurshed@mercer.com>
2026-07-01 13:32:46 +0530

    task/2656281 Simplifying user report download policy (#8872)
    * task/2656281 Removing scope from user_report_downloads_policy.rb
    
    * task/2656281 spec changes

--- app/policies/user_report_download_policy.rb
+++ app/policies/user_report_download_policy.rb
@@ -20,6 +20,8 @@ class UserReportDownloadPolicy < BasePolicy
     check_threesixty_user_report
   end
 
+  private
+
   def assessor_can_access_report?
     return false unless @current_user.is?(:assessor)
 
@@ -36,41 +38,6 @@ class UserReportDownloadPolicy < BasePolicy
       relationship: Relationship.assessor_relationship
     )
   end
-
-  class Scope < BasePolicy::Scope
-    def resolve
-      return scope.all if user.is?(:superadmin)
-
-      own_report_ids = scope.where(user: user, user_access: true).select(:id)
-      permitted_report_ids = user.accessible_records(UserReport, 'results.view_report').select(:id)
-      threesixty_report_ids = threesixty_reports_for_user.select(:id)
-
-      scope.where(id: own_report_ids).
-        or(scope.where(id: permitted_report_ids)).
-        or(scope.where(id: threesixty_report_ids))
-    end
-
-    private
-
-    def threesixty_reports_for_user
-      report_ids = user_threesixty_campaigns.flat_map do |threesixty_campaign|
-        managed_subjects = Threesixty::Evaluators::GetManagedSubjectsQuery.new(threesixty_campaign, user).query
-        ::Threesixty::UsersReportsQuery.new(threesixty_campaign, managed_subjects, user).query.pluck(:id)
-      end
-
-      scope.where(id: report_ids)
-    end
-
-    def user_threesixty_campaigns
-      campaign_ids = Threesixty::Participant.where(evaluator_id: user.id).
-                     or(Threesixty::Participant.where(subject_id: user.id)).
-                     select(:campaign_id)
-
-      Threesixty::Campaign.where(campaign_id: campaign_ids)
-    end
-  end
-
-  private
 
   def check_threesixty_user_report
     UserReportPolicy.new({ current_user: @current_user }, @record).send(:check_user_report)
