7ffe274a15ba42190d2aa892c70a89fc104f5a76
Aashish Saini <aashish.saini@mercer.com>
2026-08-04 12:58:15 +0530

    Task-2646782/Fix User gets stuck on profile screen (#9198)
    * Fix User gets stuck on profile screen
    
    * some improvements

--- app/core/users/profile_completion.rb
+++ app/core/users/profile_completion.rb
@@ -2,6 +2,9 @@
 
 module Users
   class ProfileCompletion < BaseCommand
+    # Fields stored on `User` (not `UserProfile`) that always count towads completion percentage.
+    USER_FIELDS = %i[first_name last_name].freeze
+
     attr_reader :user
 
     def initialize(user)
@@ -9,14 +12,12 @@ module Users
     end
 
     def call
-      user_fields = %i[first_name last_name]
-
       filled = 0
-      total = (user_fields.length + required_default_fields.length + required_custom_fields.length).to_f
+      total = (USER_FIELDS.length + required_default_fields.length + required_custom_fields.length).to_f
       required_default_fields.each do |field|
         filled += 1 if user.user_profile.respond_to?(field) && user.user_profile.send(field).present?
       end
-      user_fields.each do |field|
+      USER_FIELDS.each do |field|
         filled += 1 if user.send(field).present?
       end
 
@@ -33,7 +34,11 @@ module Users
     def required_default_fields
       return [] unless user.project
 
-      @required_default_fields ||= user.project.profile_setting.required_default_fields.select { |_, v| v }.keys
+      # `first_name`/`last_name` live on `User` and are already counted via USER_FIELDS.
+      # Excluding them here prevents double-counting in the denominator, which otherwise
+      # caps completion below 100% whenever they are marked required in profile settings.
+      @required_default_fields ||=
+        user.project.profile_setting.required_default_fields.select { |_, v| v }.keys.map(&:to_sym) - USER_FIELDS
     end
 
     def required_custom_fields
