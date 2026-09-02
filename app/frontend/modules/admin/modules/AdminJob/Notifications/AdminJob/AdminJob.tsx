a57945d545a9a1b68f22d71f4001b67652b1e87b
priyadharshini-lnu <priyadharshini.lnu@mercer.com>
2026-09-02 14:34:03 +0530

    bugfix/2871911 addressed review changes

--- app/frontend/modules/admin/modules/AdminJob/Notifications/AdminJob/AdminJob.tsx
+++ app/frontend/modules/admin/modules/AdminJob/Notifications/AdminJob/AdminJob.tsx
@@ -45,7 +45,7 @@ const AdminJob: React.FC<{ job: AdminJobI, read: (id: number) => void }> = ({ jo
         <Typography.Text type="secondary">{dayjs(job.createdAt).fromNow()}</Typography.Text>
       </Typography.Text>
       {job.isValid ? (
-        <Typography.Link href={job.titleLink.href}>
+        <Typography.Link href={job.titleLink.href} target="_blank" rel="noopener noreferrer">
           {job.titleLink.label}
         </Typography.Link>
       ) : (
