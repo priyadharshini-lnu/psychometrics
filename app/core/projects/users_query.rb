ea4db43d66d4c0aad1f8fc594cfafd9dab677d51
Aashish Saini <aashish.saini@mercer.com>
2026-09-15 17:20:19 +0530

    Task/2934761, 2934786: Uat users enhancements (#9485)
    * task 2934761 changes
    
    * task 2934786 changes

--- app/core/projects/users_query.rb
+++ app/core/projects/users_query.rb
@@ -17,12 +17,14 @@ module Projects
 
     def sql
       <<-SQL.squish
-        SELECT users.id, users.email, users.first_name, users.last_name, 'users' as source, user_profiles.locale
+        SELECT users.id, users.email, users.first_name, users.last_name, 'users' as source, user_profiles.locale,
+               users.is_uat
         FROM users
         JOIN user_profiles ON users.id = user_profiles.user_id
         WHERE project_id = :project_id AND (email LIKE :query OR first_name LIKE :query OR last_name LIKE :query)
         UNION
-        SELECT sheet_rows.id, sheet_rows.email, null as first_name, null as last_name, 'sheets' as source, null as locale
+        SELECT sheet_rows.id, sheet_rows.email, null as first_name, null as last_name, 'sheets' as source, null as locale,
+               false as is_uat
         FROM sheet_rows
         JOIN sheets on sheets.id = sheet_rows.sheet_id and sheets.project_id = :project_id
         WHERE sheets.type = 'Datasheet' AND sheet_rows.email LIKE :query
